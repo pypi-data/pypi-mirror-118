757232d8cf27706303b97a8028c3a75cd9ca1933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:30 +0000

    Add package dwhutils/0.1.1/dwhutils-0.1.1.tar.gz

---
+++ packages/dwhutils/dwhutils-0.1.1.tar.gz/dwhutils-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from pygments.lexer import include
+from setuptools import setup, find_packages
+
+setup(
+    name='dwhutils',
+    version='0.1.1',
+    author='PoeticDrunkenCat',
+    author_email='poeticdrunkencat@gmail.com',
+    packages=find_packages(include=['dwhutils']),
+    include_package_data=True,
+)
