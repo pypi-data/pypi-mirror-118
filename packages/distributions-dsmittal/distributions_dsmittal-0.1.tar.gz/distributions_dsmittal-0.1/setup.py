3e3d13881a1788118dbc92ecf55a622fdc782c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:39 +0000

    Add package distributions-dsmittal/0.1/distributions_dsmittal-0.1.tar.gz

---
+++ packages/distributions-dsmittal/distributions_dsmittal-0.1.tar.gz/distributions_dsmittal-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_dsmittal',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['distributions_dsmittal'],
+      author='Devanshi Mittal',
+      author_emmail='mittal.devanshi1996@gmail.com',
+      zip_safe=False)
