9a1b450025e2384dffda3a680a96ab84a9e41add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:53 +0000

    Add package mmmnnn-distributions/99.2/mmmnnn_distributions-99.2.tar.gz

---
+++ packages/mmmnnn-distributions/mmmnnn_distributions-99.2.tar.gz/mmmnnn_distributions-99.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mmmnnn_distributions',
+      version='99.02',
+      description='Gaussian distributions',
+      packages=['mmmnnn_distributions'],
+      author='Weerachai Srisingchai',
+      author_email='No@mal.com',
+      zip_safe=False)
