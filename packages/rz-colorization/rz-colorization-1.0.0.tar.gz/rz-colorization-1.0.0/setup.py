f3f09c0b8308ad0b80c8c8bd76b84bdec92a0922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:00 +0000

    Add package rz-colorization/1.0.0/rz-colorization-1.0.0.tar.gz

---
+++ packages/rz-colorization/rz-colorization-1.0.0.tar.gz/rz-colorization-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="rz-colorization",
+    version="1.0.0",
+    description="Rich Zhang's colorization model in the form of an easy to use python package.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arnavg115/colorization",
+    author="Arnav G.",
+    license="MIT",
+    packages=["colorizers"],
+    include_package_data=True,
+    install_requires=["torch","scikit-image","numpy","matplotlib","Pillow"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ]
+)
+
