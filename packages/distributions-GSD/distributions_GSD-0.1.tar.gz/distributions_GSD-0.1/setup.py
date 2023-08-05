053ffe73bd2abff3bb32fb4a088426a88519a35e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:13 +0000

    Add package distributions-GSD/0.1/distributions_GSD-0.1.tar.gz

---
+++ packages/distributions-GSD/distributions_GSD-0.1.tar.gz/distributions_GSD-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_GSD',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_GSD'],
+      author='Deepa B',
+      zip_safe=False)
