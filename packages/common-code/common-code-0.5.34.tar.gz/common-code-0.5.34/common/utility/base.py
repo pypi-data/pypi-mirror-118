0a4358121b9563399cd3dc540154895d22f322d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:41 +0000

    Add package common-code/0.5.34/common-code-0.5.34.tar.gz

---
+++ packages/common-code/common-code-0.5.34.tar.gz/common-code-0.5.34/common/utility/base.py
@@ -0,0 +1,23 @@
+import time
+import uuid
+from datetime import datetime
+
+
+def dt_time():
+    nnn = time.time() - 8 * 60 * 60
+    dat = datetime.fromtimestamp(nnn)
+    ttt = dat.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    return ttt
+
+
+def dt_now_time():
+    nnn = time.time()
+    dat = datetime.fromtimestamp(nnn)
+    ttt = dat.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    return ttt
+
+
+def get_uuid():
+    uid = str(uuid.uuid4())
+    suid = "".join(uid.split("-"))
+    return suid
