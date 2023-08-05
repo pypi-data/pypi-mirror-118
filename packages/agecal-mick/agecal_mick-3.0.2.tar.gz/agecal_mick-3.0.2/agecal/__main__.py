183c69f0179541fdffafb703d4f45766c94b2fcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:54 +0000

    Add package agecal-mick/3.0.2/agecal_mick-3.0.2.tar.gz

---
+++ packages/agecal-mick/agecal_mick-3.0.2.tar.gz/agecal_mick-3.0.2/agecal/__main__.py
@@ -0,0 +1,11 @@
+from agecal import greeting
+from agecal import compareage
+
+def main():
+    greeting.sayHi()
+
+    compareage.compare_my_age()
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/agecal-mick/agecal_mick-3.0.2.tar.gz/agecal_mick-3.0.2/setup.py
@@ -0,0 +1,30 @@
+import os.path
+from setuptools import setup
+
+
+# The directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+setup(
+    name="agecal_mick",
+    version="3.0.2",
+    description="Compare age with mickzaa return older or younger",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Mickzaa",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["agecal"],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={"console_scripts": ["realpython=agecal.__main__:main"]},
+)
