04eca38d9a6f97d9fd717edd5fe6c9289a0c260c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:16 +0000

    Add package vigenpy/0.2.1/vigenpy-0.2.1.tar.gz

---
+++ packages/vigenpy/vigenpy-0.2.1.tar.gz/vigenpy-0.2.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import _install_setup_requires, setup, find_packages
+
+with open('readme.md','r') as f:
+	long_desc = f.read()
+
+with open('documents/short_desc.txt','r') as f:
+	short_desc = f.read()
+
+with open('requirements.txt','r') as f:
+	requirements = f.read()
+
+requirements = requirements.split('\n')
+
+setup(
+	name='vigenpy',
+	version='0.2.1',
+	description=short_desc,
+	license='MIT',
+	long_description=long_desc,
+	long_description_content_type='text/markdown',
+	author='Vinay Verma',
+	maintainer='Vinay Verma',
+	maintainer_email='vermavinay982@gmail.com',
+	author_email='vermavinay982@gmail.com',
+	platforms=['Windows','Linux','Mac OS-X','Unix'],
+	url='https://github.com/vermavinay982/vigenpy',
+	python_requires='>=3.7',
+	packages= [
+		'vigenpy',
+		'vigenpy/video',
+	],
+	install_requires=requirements,
+)
