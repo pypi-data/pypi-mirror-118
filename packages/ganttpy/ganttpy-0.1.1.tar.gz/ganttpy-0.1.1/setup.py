3ecd25fcd4c442b4e6dc5c2a315da23d5131dcfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:54 +0000

    Add package ganttpy/0.1.1/ganttpy-0.1.1.tar.gz

---
+++ packages/ganttpy/ganttpy-0.1.1.tar.gz/ganttpy-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ganttpy',
+    version='0.1.1',
+    packages=[''],
+    url='https://github.com/FernandoJSM/ganttpy',
+    license='MIT',
+    author='Fernando Magalhaes',
+    author_email='fernandomagalhaes@yahoo.com.br',
+    description='Gantt chart creation in python'
+)
