3b21b9d5c84d178c3644a9d4bca87e522517be6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:09 +0000

    Add package smurf-imputation/1.0.6/smurf-imputation-1.0.6.tar.gz

---
+++ packages/smurf-imputation/smurf-imputation-1.0.6.tar.gz/smurf-imputation-1.0.6/setup.py
@@ -0,0 +1,32 @@
+# _*_ coding: utf-8 _*_
+"""
+Time:     2021/7/22 18:37
+Author:   WANG Bingchen
+Version:  V 0.1
+File:     setup.py
+Describe: 
+"""
+
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="smurf-imputation",
+    version="1.0.6",
+    author="Bingchen Wang",
+    author_email="wangbingchen@buaa.edu.cn",
+    description="SMURF : A matrix factorization method for single-cell",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/deepomicslab/SMURF",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
