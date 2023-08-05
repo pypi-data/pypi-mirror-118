6b33499a595dd2404c63fbba5085b6c2f9396275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:52 +0000

    Add package pyFlarum/1.0.9.3b0/pyFlarum-1.0.9.3b0.tar.gz

---
+++ packages/pyFlarum/pyFlarum-1.0.9.3b0.tar.gz/pyFlarum-1.0.9.3b0/pyflarum/datetime_conversions.py
@@ -0,0 +1,43 @@
+from typing import Optional
+
+from datetime import datetime
+
+
+def flarum_to_datetime(flarum_dt: Optional['str | datetime']=None):
+    """
+        Converts Flarum's datetime string to Python's datetime object.
+        Doesn't convert if the parameter is already a datetime object.
+        
+        Flarum's datetime format is `%Y-%m-%dT%H:%M:%S%z`
+    """
+
+    if not flarum_dt:
+        return None
+
+    elif isinstance(flarum_dt, datetime):
+        return flarum_dt
+
+    else:
+        strpped = datetime.strptime(flarum_dt, r'%Y-%m-%dT%H:%M:%S%z') # type: datetime
+
+        return strpped
+
+
+def datetime_to_flarum(dt: Optional['datetime | str']=None):
+    """
+        Converts Python's datetime object to Flarum's datetime string.
+        Doesn't convert if the parameter is already a string.
+        
+        Flarum's datetime format is `%Y-%m-%dT%H:%M:%S%z`
+    """
+
+    if not dt:
+        return None
+
+    elif isinstance(dt, str):
+        return dt
+
+    else:
+        strfed = dt.strftime(r'%Y-%m-%dT%H:%M:%S%z') # type: str
+
+        return strfed
---
+++ packages/pyFlarum/pyFlarum-1.0.9.3b0.tar.gz/pyFlarum-1.0.9.3b0/pyflarum/extensions/flarum/FoF_UsernameRequest.py
@@ -0,0 +1,47 @@
+from typing import Optional
+
+from datetime import datetime
+
+from .. import ExtensionMixin
+from ...flarum.core.users import UserFromBulk
+
+
+AUTHOR = 'fof'
+NAME = 'username-request'
+ID = f"{AUTHOR}-{NAME}"
+
+
+SOFT_DEPENDENCIES = []
+HARD_DEPENCENDIES = []
+
+
+
+class UsernameRequestUserMixin(UserFromBulk):
+    @property
+    def usernameHistory(self) -> Optional[list[dict[str, datetime]]]:
+        raw_list = self.attributes.get("usernameHistory", None) # type: list[dict[str, int]]
+        formatted_list = [] # type: list[dict[str, datetime]]
+
+        if raw_list and isinstance(raw_list[0], dict):
+            for raw_dict in raw_list:
+                for username, unix in raw_dict.items():
+                    dt = datetime.fromtimestamp(unix)
+
+                    formatted_list.append({ username: dt})
+
+            return formatted_list
+
+        return None
+
+
+
+class UsernameRequestExtension(ExtensionMixin):
+    def get_dependencies(self):
+        return {
+            "soft": SOFT_DEPENDENCIES,
+            "hard": HARD_DEPENCENDIES
+        }
+
+
+    def mixin(self):
+        super().mixin(self, UserFromBulk, UsernameRequestUserMixin)
