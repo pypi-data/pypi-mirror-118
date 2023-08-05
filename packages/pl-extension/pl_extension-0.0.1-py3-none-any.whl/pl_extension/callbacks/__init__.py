a9db45f101bde3b13ce407be4de4c155d87b292e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:44 +0000

    Add package pl-extension/0.0.1/pl_extension-0.0.1-py3-none-any.whl

---
+++ packages/pl-extension/pl_extension-0.0.1-py3-none-any.whl/pl_extension/callbacks/__init__.py
@@ -0,0 +1,5 @@
+from .evaluation import Evaluation
+from .linear_scaling_rule import LinearScalingRule
+from .lr_scheduler import LRScheduler
+from .model_checkpoint import ModelCheckpoint
+from .speed import Speed
