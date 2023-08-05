42632f2244259b3a44cd3160165162ba0b784167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:35 +0000

    Add package qvapay/0.0.1/qvapay-0.0.1-py3-none-any.whl

---
+++ packages/qvapay/qvapay-0.0.1-py3-none-any.whl/qvapay/__init__.py
@@ -0,0 +1,20 @@
+"""
+   ___             ____             
+  / _ \__   ____ _|  _ \ __ _ _   _ 
+ | | | \ \ / / _` | |_) / _` | | | |
+ | |_| |\ V / (_| |  __/ (_| | |_| |
+  \__\_\ \_/ \__,_|_|   \__,_|\__, |
+                              |___/ ✔️✔️
+
+   Python SDK for the QvaPay API
+"""
+
+from __future__ import absolute_import
+from qvapay.resources import *
+from qvapay.client import Client
+
+__version__ = '0.0.1'
+__author__ = 'Carlos Lugones <contact@lugodev.com>'
+__all__ = []
+
+api_url = 'https://qvapay.com/api/v'
