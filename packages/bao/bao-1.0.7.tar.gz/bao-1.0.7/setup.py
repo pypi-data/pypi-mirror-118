62eb1780be52494d7994b67418e6b4277e539a6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:15 +0000

    Add package bao/1.0.7/bao-1.0.7.tar.gz

---
+++ packages/bao/bao-1.0.7.tar.gz/bao-1.0.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+'''
+    必要元素，name，version，description，packages等。
+    常用元素，author，pymoudles,long_description,author_email,install_requires
+'''
+def rf(filename):
+    with open(filename,encoding="utf-8") as ff:
+        return ff.read()
+    
+
+
+
+#print (readme_file())
+setup(
+    name = "bao",
+    version = "1.0.7",
+    description = "this is my first test",
+    packages = ["bao"],
+    py_modules = ["tool"],
+    author = "xushibo",
+    author_email = "937207118@qq.com",
+    install_requires = ["bibtexparser"],#待测试
+    long_description = rf("C:/Users/86135/Desktop/baobao/README.rst"),
+    url = "https://www.bilibili.com/"
+    
+
+)
