627bc7b27920da858c7d3c25ae781e0b99add336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:59 +0000

    Add package ConversationAgent/0.3.8/ConversationAgent-0.3.8.tar.gz

---
+++ packages/ConversationAgent/ConversationAgent-0.3.8.tar.gz/ConversationAgent-0.3.8/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+with open("./ConversationAgent/README.md", "r", encoding="utf-8") as fh:
+  long_description = fh.read()
+setuptools.setup(
+    name='ConversationAgent',
+    version='0.3.8',
+    description='ConversationAgent',
+    author='Theta',
+    license='',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    package_data={
+        "": ["*.txt", "*.py", "*.md"]
+    },
+    zip_safe=False
+)
+
