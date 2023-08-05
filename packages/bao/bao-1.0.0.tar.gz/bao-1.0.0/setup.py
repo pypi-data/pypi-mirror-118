a3ff419ab062ccc1eda4a021e78e16007f9f05d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:55 +0000

    Add package bao/1.0.0/bao-1.0.0.tar.gz

---
+++ packages/bao/bao-1.0.0.tar.gz/bao-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "bao",
+    version = "1.0.0",
+    description = "this is my first test",
+    packages = ["bao"],
+    author = "xushibo",
+
+
+)
