c1fe5d38406a72a017d3fbd6b68995863a649062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:44 +0000

    Add package gideon/0.0.4/gideon-0.0.4.tar.gz

---
+++ packages/gideon/gideon-0.0.4.tar.gz/gideon-0.0.4/gideon/__init__.py
@@ -0,0 +1,61 @@
+
+from typing import Mapping
+from neuralintents import GenericAssistant
+import speech_recognition
+import pyttsx3 as tts
+import os,sys,time
+import random
+from pyttsx3 import *
+from datetime import *
+
+recognizer = speech_recognition.Recognizer()
+
+speaker = tts.init()
+speaker.setProperty('rate', 150)
+
+
+
+# def timetraffel():
+#     global recognizer
+
+#     speaker.say("to what year do you want to go captain?")
+#     speaker.runAndWait()
+
+#     done = False
+#     while not done:
+#         try:
+#             with speech_recognition.Microphone() as mic:
+#                 recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+#                 audio = recognizer.listen(mic)
+
+#                 year = recognizer.recognize_google(audio)
+#                 year = year.lower()
+#         except speech_recognition.UnknownValueError:
+#             recognizer = speech_recognition.Recognizer()
+#             speaker.say("i did not get that captain")
+#             speaker.runAndWait()
+
+def voices():
+    for voice in speaker.getProperty('voices'):
+        print(voice)
+
+
+def assistant(file):
+    recognizer = speech_recognition.Recognizer()
+    assistant = GenericAssistant(file)
+    assistant.train_model()
+    # assistant.request("gideon")
+    os.system("cls")
+    while True:
+        try:
+            with speech_recognition.Microphone() as mic:
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
+
+                message = recognizer.recognize_google(audio)
+                message = message.lower()
+
+                assistant.request(message)
+
+        except speech_recognition.UnknownValueError:
+            recognizer = speech_recognition.Recognizer()
