8e9c2b20fb99eb7b89690411c4d54ae784e508d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:19 +0000

    Add package as-project/0.1.0/as_project-0.1.0.tar.gz

---
+++ packages/as-project/as_project-0.1.0.tar.gz/as_project-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='as_project',
+    version='0.1.0',    
+    description='Enlighten coding project--alphabet soup',
+    author='Charlene DiMiceli',
+    author_email='cdimicel@umd.edu',
+    packages=find_packages(),
+    install_requires=[
+                      'numpy',
+                      ],
+
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
