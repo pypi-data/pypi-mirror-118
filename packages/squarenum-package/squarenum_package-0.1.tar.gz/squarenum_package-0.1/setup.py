a2a4efc75371f398f8195580a0d5632b18dc8dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:15 +0000

    Add package squarenum-package/0.1/squarenum_package-0.1.tar.gz

---
+++ packages/squarenum-package/squarenum_package-0.1.tar.gz/squarenum_package-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='squarenum_package',
+      version='0.1',
+      description='Square of a number',
+      author='Salomon Marquez',
+      author_email='phd.smarquez@gmail.com',
+      packages=['squarenum_package'],
+      zip_safe=False)
---
+++ packages/squarenum-package/squarenum_package-0.1.tar.gz/squarenum_package-0.1/squarenum_package/squarenum.py
@@ -0,0 +1,13 @@
+def calculate_square(num):
+	"""
+	Function that returns the square of a number
+
+	Args:
+		num(float): a number
+
+	Returns:
+		float: square of the number
+
+	"""	
+	
+	return num**2
