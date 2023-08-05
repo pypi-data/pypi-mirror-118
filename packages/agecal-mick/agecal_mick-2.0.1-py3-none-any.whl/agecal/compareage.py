cef78524c18e2f630f8f251d2e80e23098a55302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:52 +0000

    Add package agecal-mick/2.0.1/agecal_mick-2.0.1-py3-none-any.whl

---
+++ packages/agecal-mick/agecal_mick-2.0.1-py3-none-any.whl/agecal/compareage.py
@@ -0,0 +1,15 @@
+import datetime
+
+def compare_my_age():
+    my_birth_date = datetime.datetime(2000, 8, 18)
+    present_date = datetime.datetime.now()
+
+    duration = (present_date - my_birth_date)
+    duration_in_s = duration.total_seconds()
+    my_age = divmod(duration_in_s, 31536000)[0]
+
+    
+    your_age = int(input('Enter Your Age\n'))
+
+    statement = 'You are a little baby' if my_age >= your_age else 'You are older'
+    print(statement)
