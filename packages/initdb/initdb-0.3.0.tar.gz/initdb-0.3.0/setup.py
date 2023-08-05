aa6d2b4118bccddd07a73a5a2bfa3a2dadcb1bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:08 +0000

    Add package initdb/0.3.0/initdb-0.3.0.tar.gz

---
+++ packages/initdb/initdb-0.3.0.tar.gz/initdb-0.3.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="initdb",
+    version="0.3.0",
+    author='Eugen Ciur',
+    author_email='eugen@papermerge.com',
+    url='https://github.com/papermerge/initdb',
+    description="Small package that creates a user"
+    " and database owned by that user.",
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'psycopg2 == 2.9.1',
+    ],
+)
---
+++ packages/initdb/initdb-0.3.0.tar.gz/initdb-0.3.0/src/initdb/utils.py
@@ -0,0 +1,82 @@
+import time
+import psycopg2
+
+from .sql_query import SqlQuery
+
+
+def connect(
+    host,
+    user,
+    port=5432,
+    database=None,
+    password=None
+):
+    try:
+        conn = psycopg2.connect(
+            database=database,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+        )
+    except Exception as e:
+        print(e)
+        raise
+
+    return conn
+
+
+def db_is_ready(conf):
+    """
+    Is database up and running ?
+
+    This is blocking function, which means that
+    it will block entire script execution until database
+    will be available - i.e. it will be able to connect
+    with initial_user + initial_password + host to the database
+    """
+    while(True):
+        try:
+            conn = connect(
+                user=conf.initial_user,
+                password=conf.initial_password,
+                host=conf.host,
+                port=conf.port
+            )
+            conn.close()
+            print("DB is READY!")
+            break
+        except psycopg2.Error as e:
+            print("DB not ready yet")
+            print(e)
+            time.sleep(1)
+
+    return True
+
+
+def create_user(conf):
+    connection = connect(
+        user=conf.initial_user,
+        password=conf.initial_password,
+        host=conf.host,
+        port=conf.port
+    )
+    sql_query = SqlQuery(
+        connection=connection,
+        config=conf
+    )
+    sql_query.create_user()
+
+
+def create_db(conf):
+    connection = connect(
+        user=conf.initial_user,
+        password=conf.initial_password,
+        host=conf.host,
+        port=conf.port
+    )
+    sql_query = SqlQuery(
+        connection=connection,
+        config=conf
+    )
+    sql_query.create_db()
