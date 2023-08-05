85b6b8c20bbb61bb1a25754cafb7a04cb207c3fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:28 +0000

    Add package sockslib/1.6.3/sockslib-1.6.3.tar.gz

---
+++ packages/sockslib/sockslib-1.6.3.tar.gz/sockslib-1.6.3/sockslib/socksauth.py
@@ -0,0 +1,20 @@
+from sockslib.socks import AuthenticationMethod
+from sockslib.socks import Socks
+import struct
+
+class UserPassAuth(AuthenticationMethod):
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def getId(self):
+        return 0x02
+
+    def forP(self):
+        return Socks.SOCKS5
+
+    def authenticate(self, socket):
+        socket.sendall(b"\x01" + struct.pack("B", len(self.username)) + self.username.encode() + struct.pack("B", len(self.password)) + self.password.encode())
+        ver, status = socket.recv(2)
+
+        return status == 0x00
