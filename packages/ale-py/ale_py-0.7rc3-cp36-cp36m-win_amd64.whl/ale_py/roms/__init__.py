46a2e5b1018e6c1afc16e215c8a00714c5e0aaa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:07 +0000

    Add package ale-py/0.7rc3/ale_py-0.7rc3-cp36-cp36m-win_amd64.whl

---
+++ packages/ale-py/ale_py-0.7rc3-cp36-cp36m-win_amd64.whl/ale_py/roms/__init__.py
@@ -0,0 +1,51 @@
+import os
+import warnings
+
+from ale_py.roms.utils import (
+    SupportedPackage as _SupportedPackage,
+    SupportedPlugin as _SupportedPlugin,
+)
+
+
+# Precedence is as follows:
+#  1. Internal ROMs
+#  2. External ROMs
+#  3. ROMs from atari-py.roms
+#  4. ROMs from atari-py-roms.roms
+_SUPPORTED_PACKAGES = [
+    _SupportedPackage("ale_py.roms"),
+    _SupportedPlugin("ale_py.roms"),
+    _SupportedPackage("atari_py.atari_roms"),
+    _SupportedPackage("atari_py_roms.atari_roms"),
+]
+
+
+def _resolve_roms():
+    roms = {}
+    for package in _SUPPORTED_PACKAGES:
+
+        try:
+            supported, unsupported = package.resolve()
+
+            if isinstance(package, _SupportedPackage) and package.package.startswith(
+                "atari_py"
+            ):
+                warnings.warn(
+                    "Importing atari-py roms won't be supported in future releases of ale-py.",
+                    category=DeprecationWarning,
+                    stacklevel=3,
+                )
+            if len(unsupported) > 0:
+                parent = unsupported[0].parent
+                names = ", ".join(map(lambda path: path.name, unsupported))
+                warnings.warn(
+                    f"{package} contains unsupported ROMs: {parent}{os.sep}{{{names}}}"
+                )
+
+            roms.update(supported)
+        except ModuleNotFoundError:
+            pass
+    globals().update(roms)
+
+
+_resolve_roms()
