61c1d11dd18381f161712ab2560b247fbddd096f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:52 +0000

    Add package dash-utils-test/0.0.1/dash_utils_test-0.0.1.tar.gz

---
+++ packages/dash-utils-test/dash_utils_test-0.0.1.tar.gz/dash_utils_test-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+    
+setup(
+    name="dash_utils_test",
+    version="0.0.1",
+    author="Andrew Dircks",
+    author_email="abd93@cornell.edu",
+    description="Test recursive pipreqs.",
+    url="https://github.com/andrewdircks/dash_utils_test",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    packages=['dash_utils_test'],
+    python_requires=">=3.7",
+)
