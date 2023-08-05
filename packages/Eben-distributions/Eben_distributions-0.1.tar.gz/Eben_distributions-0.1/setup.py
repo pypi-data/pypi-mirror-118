28cc4c258cb59e11f437ac07e1002fd5e324e1b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:37 +0000

    Add package Eben-distributions/0.1/Eben_distributions-0.1.tar.gz

---
+++ packages/Eben-distributions/Eben_distributions-0.1.tar.gz/Eben_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'Eben_distributions',
+    version ='0.1',
+    description = 'Gaussian distributions',
+    package = ['distributions'],
+    author = 'Ebenezer Olubayode',
+    author_email = 'Olubayodeeben@yahoo.com',
+    zip_safe = False) 
