738b27c64b9ecc083c53d312c7975d304624e5d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:24 +0000

    Add package vedo/2021.0.5/vedo-2021.0.5.tar.gz

---
+++ packages/vedo/vedo-2021.0.5.tar.gz/vedo-2021.0.5/examples/volumetric/numpy_imread.py
@@ -0,0 +1,18 @@
+"""Create a Volume
+from a numpy object using imread"""
+from vedo import *
+from skimage.io import imread
+
+
+f = dataurl+'embryo.tif'
+
+voriginal = load(f)
+printc('voxel size is', voriginal.spacing(), c='cyan')
+
+raw = imread(f)
+
+vraw = Volume(raw, spacing=(104,104,104))
+
+# Compare loading the volume directly with the numpy volume:
+# they should be the same
+show([(voriginal,__doc__), (vraw,"From imread")], N=2, axes=1)
