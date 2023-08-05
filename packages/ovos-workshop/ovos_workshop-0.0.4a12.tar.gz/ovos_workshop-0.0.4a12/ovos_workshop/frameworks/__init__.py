a8d2e284d023549c19f84df67b9b4e205ac8b934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:57 +0000

    Add package ovos-workshop/0.0.4a12/ovos_workshop-0.0.4a12.tar.gz

---
+++ packages/ovos-workshop/ovos_workshop-0.0.4a12.tar.gz/ovos_workshop-0.0.4a12/ovos_workshop/frameworks/__init__.py
@@ -0,0 +1,2 @@
+from ovos_workshop.frameworks.playback import CommonPlayPlaybackType, CommonPlayMatchConfidence, \
+    CommonPlayStatus, CommonPlayMediaType, MycroftCommonPlayInterface, OVOSCommonPlaybackInterface
---
+++ packages/ovos-workshop/ovos_workshop-0.0.4a12.tar.gz/ovos_workshop-0.0.4a12/ovos_workshop/frameworks/playback/__init__.py
@@ -0,0 +1,2 @@
+from ovos_workshop.frameworks.playback.status import *
+from ovos_workshop.frameworks.playback.interface import *
