13d738548b1af56b2e5c90078178c07a4d7679f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:22 +0000

    Add package 3DefficientNet/1.1.0/3DefficientNet-1.1.0-py3-none-any.whl

---
+++ packages/3DefficientNet/3DefficientNet-1.1.0-py3-none-any.whl/src/__init__.py
@@ -0,0 +1,13 @@
+from ._extended_model import *
+
+__version__ = "1.1.0"
+
+
+EfficientNetB0 = efficient_net_b0
+EfficientNetB1 = efficient_net_b1
+EfficientNetB2 = efficient_net_b2
+EfficientNetB3 = efficient_net_b3
+EfficientNetB4 = efficient_net_b4
+EfficientNetB5 = efficient_net_b5
+EfficientNetB6 = efficient_net_b6
+EfficientNetB7 = efficient_net_b7
