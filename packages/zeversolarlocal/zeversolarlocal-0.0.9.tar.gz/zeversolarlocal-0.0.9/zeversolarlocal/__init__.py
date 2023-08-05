84c3d4da633f6376144a4fe64567c6e35d054133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:23 +0000

    Add package zeversolarlocal/0.0.9/zeversolarlocal-0.0.9.tar.gz

---
+++ packages/zeversolarlocal/zeversolarlocal-0.0.9.tar.gz/zeversolarlocal-0.0.9/zeversolarlocal/__init__.py
@@ -0,0 +1,12 @@
+"""zeversolarlocal"""
+
+
+__version__ = "0.0.9"
+
+from zeversolarlocal.api import (  # noqa
+    ZeverError,
+    ZeverTimeout,
+    default_url,
+    inverter_id,
+    solardata,
+)
