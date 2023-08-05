b21dff3c813c93fc1b917b5783f33b5aa84e99f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:44 +0000

    Add package qsilver/0.2/qsilver-0.2-py3-none-any.whl

---
+++ packages/qsilver/qsilver-0.2-py3-none-any.whl/qsilver/tcp_server.py
@@ -0,0 +1,24 @@
+from socket import *
+from typing import Coroutine
+from .async_socket import AsyncSocket
+from .api import add_coroutine
+
+__all__ = (
+    'BasicTCPServer',
+)
+
+class BasicTCPServer:
+    def __init__(self, address, port, queue_size):
+        self.socket = AsyncSocket.create(AF_INET, SOCK_STREAM)
+        self.socket.socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.socket.socket.bind((address, port))
+        self.queue_size = queue_size
+    async def listener(self, client_handler: Coroutine):
+        self.socket.socket.listen(self.queue_size)
+        try:
+            while True:
+                client, address = await self.socket.accept()
+                add_coroutine(client_handler(client, address))
+        except KeyboardInterrupt:
+            pass
+        self.socket.socket.close()
