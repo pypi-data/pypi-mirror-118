79c71e07dce48544bf83f7a400d971e9094ae96b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:54 +0000

    Add package vansulich/2021.8.0/vansulich-2021.8.0.tar.gz

---
+++ packages/vansulich/vansulich-2021.8.0.tar.gz/vansulich-2021.8.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+require_twill = ['requests>=2.25,<3']
+setuptools.setup(
+    name="vansulich",
+    version="2021.8.0",
+    author="@trumxuquang",
+    author_email="trumxuquang@gmail.com",
+    description="https://vnhass.blogspot.com/",
+    long_description="https://vnhass.blogspot.com/",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # checked by 'pip install'. See instead 'python_requires' below.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    install_requires=require_twill,
+)
