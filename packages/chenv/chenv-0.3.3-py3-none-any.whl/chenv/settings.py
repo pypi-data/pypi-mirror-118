7b926327fa97ed697ec0b51cc7df99f0eb61440b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:03 +0000

    Add package chenv/0.3.3/chenv-0.3.3-py3-none-any.whl

---
+++ packages/chenv/chenv-0.3.3-py3-none-any.whl/chenv/settings.py
@@ -0,0 +1,23 @@
+"""general settings for the application."""
+from dotenv import load_dotenv
+
+from chenv import fs
+
+DOTENV = ".env"
+PREFIX = f"{DOTENV}."
+ENVMERGE = ".envmerge"
+ENVIGNORE = ".envignore"
+
+
+def _dotenv_path() -> str:
+    return fs.local_file_path(DOTENV)
+
+
+def mount() -> None:
+    """Mount (load and set in current process) application variables from the local `.env` file."""
+    load_dotenv(_dotenv_path())
+
+
+def add(key: str, value: str) -> None:
+    """Adds a new env-variable to the application's local `.env` file."""
+    fs.dump(_dotenv_path(), {key: value}, overwrite=False)
