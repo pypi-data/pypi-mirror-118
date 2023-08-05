4b8e557c0c70e164a9caff757f47c36204440473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:42 +0000

    Add package bibcopy/0.0.2/bibcopy-0.0.2.tar.gz

---
+++ packages/bibcopy/bibcopy-0.0.2.tar.gz/bibcopy-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+'''
+    必要元素，name，version，description，packages等。
+    常用元素，author，pymoudles,long_description,author_email,install_requires
+'''
+
+
+
+#print (readme_file())
+setup(
+    name = "bibcopy",
+    version = "0.0.2",
+    py_module = ["bibcopy"],
+    description = "this is my first test",
+    author = "chenkun",
+    author_email = "qbei113043@163.com",
+    install_requires = ["bibtexparser","mysqlclient"],#待测试
+    #install_requires = ["bibtexparser","mysqlclient" ],#待测试
+    long_description = ("这是我们的尝试啊")
+
+)
