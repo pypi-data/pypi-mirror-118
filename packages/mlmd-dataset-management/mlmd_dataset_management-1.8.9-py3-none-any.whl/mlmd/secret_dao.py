f1e5a03bd9bcad1441b092552b3b13f6fd4457db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:46 +0000

    Add package mlmd-dataset-management/1.8.9/mlmd_dataset_management-1.8.9-py3-none-any.whl

---
+++ packages/mlmd-dataset-management/mlmd_dataset_management-1.8.9-py3-none-any.whl/mlmd/secret_dao.py
@@ -0,0 +1,18 @@
+
+import json
+from google.cloud import secretmanager
+import os
+
+def get_secret(secret_name, secret_version=None, project_id="mlops-315607", json_decode=True):
+	if secret_version is None:
+		secret_version = os.environ["SECRET_VERSION"]
+	if secret_version is None:
+		print("Configuration version is not defined (by using env var SECRET_VERSION) and is set to default value 1")
+		secret_version = 1
+
+	client = secretmanager.SecretManagerServiceClient()
+	name = f"projects/{project_id}/secrets/{secret_name}/versions/{secret_version}"
+	res = client.access_secret_version(request={"name":name})
+	if json_decode:
+		return json.loads(res.payload.data.decode("UTF-8"))
+	return res.payload.data.decode("UTF-8")
