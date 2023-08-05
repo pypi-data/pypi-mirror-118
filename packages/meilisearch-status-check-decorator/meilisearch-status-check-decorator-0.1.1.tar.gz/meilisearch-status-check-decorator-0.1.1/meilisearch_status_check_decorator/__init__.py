c8156193b2a62dca4a7ea687a5823626b4ece84b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:29 +0000

    Add package meilisearch-status-check-decorator/0.1.1/meilisearch-status-check-decorator-0.1.1.tar.gz

---
+++ packages/meilisearch-status-check-decorator/meilisearch-status-check-decorator-0.1.1.tar.gz/meilisearch-status-check-decorator-0.1.1/meilisearch_status_check_decorator/__init__.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from functools import wraps
+from typing import Any, Callable
+
+from meilisearch.errors import MeiliSearchApiError
+from meilisearch.index import Index
+
+
+def status_check(index: Index) -> Callable:
+    def decorator_status_check(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            try:
+                initial_status_count = len(index.get_all_update_status())
+            except MeiliSearchApiError:
+                initial_status_count = 0
+
+            result = func(*args, **kwargs)
+
+            errors = False
+            all_status = index.get_all_update_status()
+            if len(all_status) == initial_status_count + 1:
+                status = all_status[-1:]
+                if status[0]["status"] == "failed":
+                    errors = True
+            else:
+                status = all_status[initial_status_count:]
+                for s in status:
+                    if s["status"] == "failed":
+                        errors = True
+
+            if errors:
+                print(f"FAILED: {status}")  # noqa: T001
+
+            return result
+
+        return wrapper
+
+    return decorator_status_check
