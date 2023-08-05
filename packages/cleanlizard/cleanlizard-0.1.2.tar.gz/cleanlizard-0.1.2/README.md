ed79353aa23efc381c29e927c938e5a3b225cf83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:38 +0000

    Add package cleanlizard/0.1.2/cleanlizard-0.1.2.tar.gz

---
+++ packages/cleanlizard/cleanlizard-0.1.2.tar.gz/cleanlizard-0.1.2/README.md
@@ -0,0 +1,2 @@
+# <i>liz_clean </i>package 
+current version = 0.1.2
---
+++ packages/cleanlizard/cleanlizard-0.1.2.tar.gz/cleanlizard-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='cleanlizard',
+    version='0.1.2',
+    author='Christopher Agard',
+    author_email='cragard@gmail.com',
+    description="Python library for cleaning CC lizard data",
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
