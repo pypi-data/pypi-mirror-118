6b4fd253bab9f9effaa316366cf400aa0e98982d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:27 +0000

    Add package utils-s/1.6.3/utils-S-1.6.3.tar.gz

---
+++ packages/utils-s/utils-S-1.6.3.tar.gz/utils-S-1.6.3/setup.py
@@ -0,0 +1,22 @@
+import os
+import setuptools
+requiredPackages = [
+    'requests'
+]
+
+for p in requiredPackages:
+    os.system('pip3 install {} -q'.format(p))
+
+
+import utils
+setuptools.setup(
+    name='utils-S',
+    version=utils.version,
+    author="Sal Faris",
+    description="Utility functions",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    author_email='salmanfaris2005@hotmail.com',
+    url='https://github.com/The-Sal/utils/',
+    download_url='https://github.com/The-Sal/utils/archive/refs/tags/v1.6.3.tar.gz'
+)
