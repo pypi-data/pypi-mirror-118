12dfc0f95202ee82027567c1d0d04b1c08b29b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:28 +0000

    Add package fractal-toolkit/0.2.6/fractal-toolkit-0.2.6.tar.gz

---
+++ packages/fractal-toolkit/fractal-toolkit-0.2.6.tar.gz/fractal-toolkit-0.2.6/fractal/contrib/fastapi/routers/default.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter
+
+from fractal.contrib.fastapi.routers import Routes
+from fractal.contrib.fastapi.routers.domain.models import AdapterInfo, Info
+from fractal.core.utils.application_context import ApplicationContext
+
+
+def inject_default_routes(context: ApplicationContext):
+    router = APIRouter()
+
+    @router.get(Routes.ROOT)
+    def read_root():
+        return {"FastAPI": "Example"}
+
+    @router.get(Routes.INFO, responses={200: {"model": Info}})
+    def info():
+        adapters = list(context.adapters())
+        data = [
+            AdapterInfo(
+                adapter=adapter.__class__.__name__,
+                status=False,
+            )
+            for adapter in adapters
+        ]
+        for i in range(len(data)):
+            try:
+                data[i].status_ok = adapters[i].is_healthy()
+            except Exception as e:
+                context.logger.error(e)
+                data[i].status_ok = False
+        return data
+
+    @router.get(Routes.HEALTHZ)
+    def healthz():
+        return "ok"
+
+    return router
