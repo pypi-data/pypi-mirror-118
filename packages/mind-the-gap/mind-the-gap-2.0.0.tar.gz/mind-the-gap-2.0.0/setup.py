8e6648aa3a96f4215d0b479acb1f869102797c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:22 +0000

    Add package mind-the-gap/2.0.0/mind-the-gap-2.0.0.tar.gz

---
+++ packages/mind-the-gap/mind-the-gap-2.0.0.tar.gz/mind-the-gap-2.0.0/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tfl',
+ 'tfl.api',
+ 'tfl.api.line',
+ 'tfl.api.line.meta',
+ 'tfl.api.presentation',
+ 'tfl.api.presentation.entities',
+ 'tfl.cli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.26.0,<3.0.0']
+
+extras_require = \
+{'cli': ['typer>=0.3.2,<0.4.0']}
+
+entry_points = \
+{'console_scripts': ['tfl = tfl.__main__:app']}
+
+setup_kwargs = {
+    'name': 'mind-the-gap',
+    'version': '2.0.0',
+    'description': 'TFL',
+    'long_description': None,
+    'author': 'Antonio Feregrino',
+    'author_email': 'antonio.feregrino@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
