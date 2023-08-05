508701cfacf7c5ca0789a5e46ef59b1a3a85f760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:44 +0000

    Add package discord-markdown-ast-parser/1.0.5/discord_markdown_ast_parser-1.0.5-py3-none-any.whl

---
+++ packages/discord-markdown-ast-parser/discord_markdown_ast_parser-1.0.5-py3-none-any.whl/discord_markdown_ast_parser/__init__.py
@@ -0,0 +1,31 @@
+from pprint import pprint
+from typing import Any, Dict, List
+
+from discord_markdown_ast_parser.lexer import lex
+from discord_markdown_ast_parser.parser import Node, parse_tokens
+
+
+def parse(text) -> List[Node]:
+    """
+    Parses the text and returns an AST, using this package's internal Node
+    representation.
+    See parse_to_dict for a more generic string representation.
+    """
+    tokens = list(lex(text))
+    return parse_tokens(tokens)
+
+
+def parse_to_dict(text) -> List[Dict[str, Any]]:
+    """
+    Parses the text and returns an AST, represented as a dict.
+    See the README for information on the structure of this dict.
+    """
+    node_ast = parse(text)
+    return [node.to_dict() for node in node_ast]
+
+
+if __name__ == "__main__":
+    with open("shit.txt", "r") as f:
+        shit = f.read()
+
+    pprint(parse_to_dict(shit))
