3220de85492fd0d5c518cb44fc09afad7f67fdf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:21 +0000

    Add package Fred-Frechet/1.8.1/Fred-Frechet-1.8.1.tar.gz

---
+++ packages/Fred-Frechet/Fred-Frechet-1.8.1.tar.gz/Fred-Frechet-1.8.1/src/dynamic_time_warping.cpp
@@ -0,0 +1,50 @@
+/*
+Copyright 2021 Dennis Rohde
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include <vector>
+#include <limits>
+#include <chrono>
+
+#include "dynamic_time_warping.hpp"
+
+namespace Dynamic_Time_Warping {
+
+namespace Discrete {
+    
+std::string Distance::repr() const {
+    std::stringstream ss;
+    ss << value;
+    return ss.str();
+}
+    
+Distance distance(const Curve &curve1, const Curve &curve2) {
+    Distance result;
+    auto start = std::chrono::high_resolution_clock::now();
+    std::vector<std::vector<distance_t>> a(curve1.complexity() + 1, std::vector<distance_t>(curve2.complexity() + 1, std::numeric_limits<distance_t>::infinity()));
+    a[0][0] = 0;
+    #pragma omp parallel for ordered collapse(2)
+    for (curve_size_t i = 1; i <= curve1.complexity(); ++i) {
+        for (curve_size_t j = 1; j <= curve2.complexity(); ++j) {
+            a[i][j] = std::sqrt(curve1[i-1].dist_sqr(curve2[j-1]));
+            #pragma omp ordered
+            a[i][j] += std::min(std::min(a[i-1][j], a[i][j-1]), a[i-1][j-1]);
+        }
+    }
+    auto value = a[curve1.complexity()][curve2.complexity()];
+    auto end = std::chrono::high_resolution_clock::now();
+    result.time = std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
+    result.value = value;
+    return result;
+    
+}
+
+} // end namespace Discrete
+
+} // end namespace Dynamic Time Warping
---
+++ packages/Fred-Frechet/Fred-Frechet-1.8.1.tar.gz/Fred-Frechet-1.8.1/src/fred_python_wrapper.cpp
@@ -0,0 +1,203 @@
+/*
+Copyright 2020-2021 Dennis Rohde
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+
+#include "curve.hpp"
+#include "point.hpp"
+#include "frechet.hpp"
+#include "jl_transform.hpp"
+#include "clustering.hpp"
+#include "coreset.hpp"
+#include "grid.hpp"
+#include "simplification.hpp"
+#include "dynamic_time_warping.hpp"
+
+namespace py = pybind11;
+
+namespace fc = Frechet::Continuous;
+namespace fd = Frechet::Discrete;
+namespace ddtw = Dynamic_Time_Warping::Discrete;
+
+const distance_t default_error = 1;
+
+void set_frechet_error(const distance_t error) {
+    fc::error = error;
+}
+
+void set_frechet_rounding(const bool round) {
+    fc::round = round;
+}
+
+distance_t get_frechet_error() {
+    return fc::error;
+}
+
+bool get_frechet_rounding() {
+    return fc::round;
+}
+
+Curve minimum_error_simplification(const Curve &curve, const curve_size_t l) {
+    Simplification::Subcurve_Shortcut_Graph graph(const_cast<Curve&>(curve));
+    auto scurve = graph.minimum_error_simplification(l);
+    scurve.set_name("Simplification of " + curve.get_name());
+    return scurve;
+}
+
+Curve approximate_minimum_link_simplification(const Curve &curve, const distance_t epsilon) {
+    auto scurve = Simplification::approximate_minimum_link_simplification(curve, epsilon);
+    scurve.set_name("Simplification of " + curve.get_name());
+    return scurve;
+}
+
+Curve approximate_minimum_error_simplification(const Curve &curve, const curve_size_t ell) {
+    auto scurve = Simplification::approximate_minimum_error_simplification(curve, ell);
+    scurve.set_name("Simplification of " + curve.get_name());
+    return scurve;
+}
+
+void set_number_threads(std::uint64_t number) {
+    omp_set_dynamic(0);
+    omp_set_num_threads(number);
+}
+
+PYBIND11_MODULE(backend, m) {
+    
+    m.attr("default_error_continuous_frechet") = default_error;
+    
+    py::class_<Point>(m, "Point")
+        .def(py::init<dimensions_t>())
+        .def("__len__", &Point::dimensions)
+        .def("__getitem__", &Point::get)
+        .def("__setitem__", &Point::set)
+        .def("__str__", &Point::str)
+        .def("__iter__", [](Point &v) { return py::make_iterator(v.begin(), v.end()); }, py::keep_alive<0, 1>())
+        .def("__repr__", &Point::repr)
+        .def_property_readonly("values", &Point::as_ndarray)
+    ;
+    
+    py::class_<Points>(m, "Points")
+        .def(py::init<dimensions_t>())
+        .def("__len__", &Points::number)
+        .def("__getitem__", &Points::get, py::return_value_policy::reference)
+        .def("__str__", &Points::str)
+        .def("__iter__", [](Points &v) { return py::make_iterator(v.begin(), v.end()); }, py::keep_alive<0, 1>())
+        .def("__repr__", &Points::repr)
+        .def("add", &Points::add)
+        .def_property_readonly("values", &Points::as_ndarray)
+        .def_property_readonly("centroid", &Points::centroid)
+    ;
+    
+    py::class_<Curve>(m, "Curve")
+        .def(py::init<py::array_t<coordinate_t>>())
+        .def(py::init<py::array_t<coordinate_t>, std::string>())
+        .def_property_readonly("dimensions", &Curve::dimensions)
+        .def_property_readonly("complexity", &Curve::complexity)
+        .def_property("name", &Curve::get_name, &Curve::set_name)
+        .def_property_readonly("values", &Curve::as_ndarray)
+        .def_property_readonly("centroid", &Curve::centroid)
+        .def("__getitem__", &Curve::get, py::return_value_policy::reference)
+        .def("__len__", &Curve::complexity)
+        .def("__str__", &Curve::str)
+        .def("__iter__", [](Curve &v) { return py::make_iterator(v.begin(), v.end()); }, py::keep_alive<0, 1>())
+        .def("__repr__", &Curve::repr)
+    ;
+        
+    py::class_<Curves>(m, "Curves")
+        .def(py::init<>())
+        .def_property_readonly("m", &Curves::get_m)
+        .def("add", &Curves::add)
+        .def("simplify", &Curves::simplify)
+        .def("__getitem__", &Curves::get, py::return_value_policy::reference)
+        .def("__setitem__", &Curves::set)
+        .def("__len__", &Curves::number)
+        .def("__str__", &Curves::str)
+        .def("__iter__", [](Curves &v) { return py::make_iterator(v.begin(), v.end()); }, py::keep_alive<0, 1>())
+        .def("__repr__", &Curves::repr)
+        .def_property_readonly("values", &Curves::as_ndarray)
+    ;
+    
+    py::class_<fc::Distance>(m, "Continuous_Frechet_Distance")
+        .def(py::init<>())
+        .def_readwrite("time_searches", &fc::Distance::time_searches)
+        .def_readwrite("time_bounds", &fc::Distance::time_bounds)
+        .def_readwrite("number_searches", &fc::Distance::number_searches)
+        .def_readwrite("value", &fc::Distance::value)
+        .def("__repr__", &fc::Distance::repr)
+    ;
+    
+    py::class_<fd::Distance>(m, "Discrete_Frechet_Distance")
+        .def(py::init<>())
+        .def_readwrite("time", &fd::Distance::time)
+        .def_readwrite("value", &fd::Distance::value)
+        .def("__repr__", &fd::Distance::repr)
+    ;
+    
+    py::class_<ddtw::Distance>(m, "Discrete_Dynamic_Time_Warping_Distance")
+        .def(py::init<>())
+        .def_readwrite("time", &ddtw::Distance::time)
+        .def_readwrite("value", &ddtw::Distance::value)
+        .def("__repr__", &ddtw::Distance::repr)
+    ;
+    
+    py::class_<Clustering::Distance_Matrix>(m, "Distance_Matrix")
+        .def(py::init<>())
+    ;
+    
+    py::class_<Clustering::Clustering_Result>(m, "Clustering_Result")
+        .def(py::init<>())
+        .def_readwrite("value", &Clustering::Clustering_Result::value)
+        .def_readwrite("time", &Clustering::Clustering_Result::running_time)
+        .def_readwrite("assignment", &Clustering::Clustering_Result::assignment)
+        .def("__getitem__", &Clustering::Clustering_Result::get, py::return_value_policy::reference)
+        .def("__len__", &Clustering::Clustering_Result::size)
+        .def("__iter__", [](Clustering::Clustering_Result &v) { return py::make_iterator(v.cbegin(), v.cend()); }, py::keep_alive<0, 1>())
+        .def("compute_assignment", &Clustering::Clustering_Result::compute_assignment)
+    ;
+    
+    py::class_<Clustering::Cluster_Assignment>(m, "Cluster_Assignment")
+        .def(py::init<>())
+        .def("__len__", &Clustering::Cluster_Assignment::size)
+        .def("count", &Clustering::Cluster_Assignment::count)
+        .def("get", &Clustering::Cluster_Assignment::get)
+    ;
+    
+    py::class_<Coreset::Median_Coreset>(m, "Median_Coreset")
+        .def(py::init<curve_number_t, curve_size_t, Curves&, parameter_t>())
+    ;
+    
+    m.def("set_continuous_frechet_error", &set_frechet_error);
+    m.def("set_continuous_frechet_rounding", &set_frechet_rounding);
+    m.def("get_continuous_frechet_error", &get_frechet_error);
+    m.def("get_continuous_frechet_rounding", &get_frechet_rounding);
+    
+    m.def("continuous_frechet", &fc::distance);
+    m.def("discrete_frechet", &fd::distance);
+    m.def("discrete_dynamic_time_warping", &ddtw::distance);
+    
+    m.def("minimum_error_simplification", &minimum_error_simplification);
+    m.def("approximate_minimum_link_simplification", &approximate_minimum_link_simplification);
+    m.def("approximate_minimum_error_simplification", &approximate_minimum_error_simplification);
+    
+    m.def("dimension_reduction", &JLTransform::transform_naive, py::arg("in") = Curves(), py::arg("epsilon") = 0.5, py::arg("empirical_constant") = true);
+
+    m.def("discrete_klcenter", &Clustering::kl_center, py::arg("num_centers") = 1, py::arg("ell") = 2, py::arg("in") = Curves(), py::arg("distances") = Clustering::Distance_Matrix(), py::arg("random_start_center") = true, py::arg("fast_simplification") = false);
+    m.def("discrete_klmedian", &Clustering::kl_median, py::arg("num_centers") = 1, py::arg("ell") = 2, py::arg("in") = Curves(), py::arg("distances") = Clustering::Distance_Matrix(), py::arg("fast_simplification") = false);
+    
+    // these are experimental
+    //m.def("two_two_dtw_one_two_median", &Clustering::two_two_dtw_one_two_median);
+    //m.def("two_two_dtw_one_two_median_exact", &Clustering::two_two_dtw_one_two_median_exact);
+    //def("discrete_onemedian_sampling", onemedian_sampling, onemedian_sampling_overloads());
+    //def("discrete_onemedian_exhaustive", onemedian_exhaustive, onemedian_exhaustive_overloads());
+    //def("onemedian_coreset", onemedian_coreset, onemedian_coreset_overloads());
+    
+    m.def("set_maximum_number_threads", set_number_threads);
+}
