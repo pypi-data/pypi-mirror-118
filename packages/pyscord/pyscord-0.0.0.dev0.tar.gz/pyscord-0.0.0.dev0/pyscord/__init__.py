34e7f555ef40ecaa4d1010d58543c1b429a6dbce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:58 +0000

    Add package pyscord/0.0.0.dev0/pyscord-0.0.0.dev0.tar.gz

---
+++ packages/pyscord/pyscord-0.0.0.dev0.tar.gz/pyscord-0.0.0.dev0/pyscord/__init__.py
@@ -0,0 +1,16 @@
+"""
+Pyscord library
+====================
+An asynchronous python API wrapper meant to replace discord.py
+
+:copyright: (c) 2021-present Pyscord
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = "Pyscord library"
+__package__ = "pyscord"
+__author__ = "Sigmanificient, Arthurdw"
+__license__ = "MIT"
+__copyright__ = "Copyright 2021-present"
+__version__ = "0.0.0.dev"
+__description__ = "Discord API wrapper rebuild from scratch."
