7fb8a2c2daafbf768257ebd0660ed5d33414c412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:54 +0000

    Add package nulltk/0.0.5/nulltk-0.0.5.tar.gz

---
+++ packages/nulltk/nulltk-0.0.5.tar.gz/nulltk-0.0.5/src/nulltk/mixins.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+def Reactive(obj):
+    activebg = obj['activeBackground']
+    activefg = obj['activeForeground']
+    obj.prevBG = obj['background']
+    try:
+        obj.prevFG = obj['foreground']
+    except tk.TclError: pass
+
+    def on_enter(e):
+        obj.prevBG = obj['background']
+        obj['background'] = activebg
+        if hasattr(obj,'prevFG'):
+            obj.prevFG = obj['foreground']
+            obj['foreground'] = activefg
+
+    def on_leave(e):
+        obj['background'] = obj.prevBG
+        if hasattr(obj,'prevFG'):
+            obj['foreground'] = obj.prevFG
+
+    obj.bind("<Enter>", on_enter)
+    obj.bind("<Leave>", on_leave)
+
+    return obj
---
+++ packages/nulltk/nulltk-0.0.5.tar.gz/nulltk-0.0.5/src/nulltk/toplevel.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+
+class Toplevel(tk.Toplevel):
+    def __init__(self, *args, **kwargs):
+        tk.Toplevel.__init__(self, *args, **kwargs)
