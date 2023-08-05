ff943b142024c009709d8168fc665918f9464f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:03 +0000

    Add package moto/2.2.6.dev5/moto-2.2.6.dev5.tar.gz

---
+++ packages/moto/moto-2.2.6.dev5.tar.gz/moto-2.2.6.dev5/moto/logs/exceptions.py
@@ -0,0 +1,36 @@
+from __future__ import unicode_literals
+from moto.core.exceptions import JsonRESTError
+
+
+class LogsClientError(JsonRESTError):
+    code = 400
+
+
+class ResourceNotFoundException(LogsClientError):
+    def __init__(self, msg=None):
+        self.code = 400
+        super().__init__(
+            "ResourceNotFoundException", msg or "The specified log group does not exist"
+        )
+
+
+class InvalidParameterException(LogsClientError):
+    def __init__(self, msg=None):
+        self.code = 400
+        super().__init__(
+            "InvalidParameterException", msg or "A parameter is specified incorrectly."
+        )
+
+
+class ResourceAlreadyExistsException(LogsClientError):
+    def __init__(self):
+        self.code = 400
+        super().__init__(
+            "ResourceAlreadyExistsException", "The specified log group already exists"
+        )
+
+
+class LimitExceededException(LogsClientError):
+    def __init__(self):
+        self.code = 400
+        super().__init__("LimitExceededException", "Resource limit exceeded.")
