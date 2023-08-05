a82a80896109dd41838c222778e96d49762e50b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:05 +0000

    Add package self-practice-probability/1.3/self_practice_probability-1.3.tar.gz

---
+++ packages/self-practice-probability/self_practice_probability-1.3.tar.gz/self_practice_probability-1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='self_practice_probability',
+      version='1.3',
+      description='Gaussian and Binomial distributions',
+      packages=['self_practice_probability'],
+      author = 'Shamushideen Sule',
+      author_email = 'aneru.sule@gmail.com',
+      zip_safe=False)
