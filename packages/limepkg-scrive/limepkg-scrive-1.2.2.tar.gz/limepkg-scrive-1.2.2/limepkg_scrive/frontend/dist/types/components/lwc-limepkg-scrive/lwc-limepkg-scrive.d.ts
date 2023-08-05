8285ae8b0ea0638095bcc42694d6e4126f789947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:15 +0000

    Add package limepkg-scrive/1.2.2/limepkg-scrive-1.2.2.tar.gz

---
+++ packages/limepkg-scrive/limepkg-scrive-1.2.2.tar.gz/limepkg-scrive-1.2.2/limepkg_scrive/frontend/dist/types/components/lwc-limepkg-scrive/lwc-limepkg-scrive.d.ts
@@ -0,0 +1,18 @@
+import { LimeWebComponent, LimeWebComponentContext, LimeWebComponentPlatform } from '@limetech/lime-web-components-interfaces';
+export declare class Test implements LimeWebComponent {
+    private document;
+    private session;
+    platform: LimeWebComponentPlatform;
+    private config;
+    context: LimeWebComponentContext;
+    element: HTMLElement;
+    includePerson: boolean;
+    includeCoworker: boolean;
+    private cloneDocument;
+    private goToScrive;
+    private files;
+    private allowedExtensions;
+    private isSignable;
+    private setCloneDocument;
+    render(): any;
+}
