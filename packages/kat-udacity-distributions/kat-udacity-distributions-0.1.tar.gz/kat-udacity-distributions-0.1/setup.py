0ed24576cf0807b7bf54e0cc46cd15ebcf2e14b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:04 +0000

    Add package kat-udacity-distributions/0.1/kat-udacity-distributions-0.1.tar.gz

---
+++ packages/kat-udacity-distributions/kat-udacity-distributions-0.1.tar.gz/kat-udacity-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='kat-udacity-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['kat-udacity-distributions'],
+      zip_safe=False)
