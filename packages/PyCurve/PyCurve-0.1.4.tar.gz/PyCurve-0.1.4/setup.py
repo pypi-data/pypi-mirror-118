ec27a84f69d7cd92fc292536654f86d47f7acb15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:01 +0000

    Add package PyCurve/0.1.4/PyCurve-0.1.4.tar.gz

---
+++ packages/PyCurve/PyCurve-0.1.4.tar.gz/PyCurve-0.1.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='PyCurve',
+    version='0.1.4',
+    author="Antoine Perrin",
+    author_email="antoineperrin.pro1@gmail.com",
+    description='Interest Rate Yield Curve Toolkit',
+    py_modules=["actuarial_implementation",
+                "bjork_christensen",
+                "bjork_christensen_augmented",
+                "cubic",
+                "curve",
+                "simulation",
+                "linear",
+                "nelson_siegel",
+                "svensson_nelson_siegel",
+                "vasicek",
+                "hull_white"],
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["matplotlib", "pandas", "numpy", "scipy", "typing"],
+    url="https://github.com/ahgperrin/PyCurve"
+    )
