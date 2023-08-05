25990abe2f6513fb67d80366d80a58924852e82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:58 +0000

    Add package Brainshop/0.0.5/Brainshop-0.0.5.tar.gz

---
+++ packages/Brainshop/Brainshop-0.0.5.tar.gz/Brainshop-0.0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+with open("README.MD", "r") as f:
+    long_description= f.read()
+setup(name='Brainshop',
+version='0.0.5',
+description='An easy to use API wrapper for Brainshop.ai',
+long_description=long_description,
+long_description_content_type = "text/markdown",
+author='Rishikesh',
+packages=['Brainshop'],
+install_requires=['requests'])
