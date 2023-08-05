44a773648aa33a3c0d3c7c2f77caaf26d6ec414e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:50 +0000

    Add package QPT/1.0b1.dev5/QPT-1.0b1.dev5-py3-none-any.whl

---
+++ packages/QPT/QPT-1.0b1.dev5-py3-none-any.whl/qpt/ext/launcher_debug/Debug-进入半虚拟环境.cmd
@@ -0,0 +1,10 @@
+echo off
+chcp 65001
+set QPT_COLOR=False
+set QPT_MODE=Debug
+set PYTHONPATH=./Python/resources;./Python/Lib/site-packages;./Python
+set PATH=%PATH%;./Python/resources;./Python/Lib/site-packages;./Python;./Python/Lib;./Python/Scripts
+set PROMPT=(QPT_VENV) %PROMPT%
+cls
+echo QPT successful!
+cmd /k
---
+++ packages/QPT/QPT-1.0b1.dev5-py3-none-any.whl/qpt/gui/qpt_choose_env.py
@@ -0,0 +1,10 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+# 貌似不需要了
+class MiniChooseEnv(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
