cb1163d7be69731e83ec757ba6bd594265445e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:52 +0000

    Add package discord4py/1.0.5/discord4py-1.0.5.tar.gz

---
+++ packages/discord4py/discord4py-1.0.5.tar.gz/discord4py-1.0.5/discord4py/ui/__init__.py
@@ -0,0 +1,15 @@
+"""
+discord4py.ui
+~~~~~~~~~~~
+
+Bot UI Kit helper for the discord4py API
+
+:copyright: (c) 2015-present Rapptz
+:license: MIT, see LICENSE for more details.
+
+"""
+
+from .view import *
+from .item import *
+from .button import *
+from .select import *
