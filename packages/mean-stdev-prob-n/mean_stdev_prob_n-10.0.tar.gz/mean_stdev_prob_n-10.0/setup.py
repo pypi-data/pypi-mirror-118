53895c0afc43859642fc21f74da3c19b1bb1d997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:21 +0000

    Add package mean-stdev-prob-n/10.0/mean_stdev_prob_n-10.0.tar.gz

---
+++ packages/mean-stdev-prob-n/mean_stdev_prob_n-10.0.tar.gz/mean_stdev_prob_n-10.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mean_stdev_prob_n',
+      version='10.0',
+      description='Probability density function',
+      packages=['mean_stdev_prob_n'],
+      author = 'Shamushideen Sule',
+      author_email = 'aneru.sule@gmail.com',
+      zip_safe=False)
