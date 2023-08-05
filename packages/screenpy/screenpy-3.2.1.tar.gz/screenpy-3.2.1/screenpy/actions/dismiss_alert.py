fc1eb5c02ea7b024abd119c676fbd65a2e3295c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:34 +0000

    Add package screenpy/3.2.1/screenpy-3.2.1.tar.gz

---
+++ packages/screenpy/screenpy-3.2.1.tar.gz/screenpy-3.2.1/screenpy/actions/dismiss_alert.py
@@ -0,0 +1,27 @@
+"""
+Dismiss a javascript alert.
+"""
+
+from screenpy.abilities import BrowseTheWeb
+from screenpy.actor import Actor
+from screenpy.pacing import aside, beat
+
+
+class DismissAlert:
+    """Dismiss an alert.
+
+    Abilities Required:
+        |BrowseTheWeb|
+
+    Examples::
+
+        the_actor.attempts_to(DismissAlert())
+    """
+
+    @beat("{} dismisses the alert.")
+    def perform_as(self, the_actor: Actor) -> None:
+        """Direct the Actor to dismiss the alert."""
+        browser = the_actor.uses_ability_to(BrowseTheWeb).browser
+        alert = browser.switch_to.alert
+        aside(f'... the alert says "{alert.text}"')
+        alert.dismiss()
