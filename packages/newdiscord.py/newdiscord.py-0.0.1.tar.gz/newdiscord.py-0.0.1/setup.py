10d1bc23b15532f0caeb08a70cdbcc02a78040ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:05 +0000

    Add package newdiscord.py/0.0.1/newdiscord.py-0.0.1.tar.gz

---
+++ packages/newdiscord.py/newdiscord.py-0.0.1.tar.gz/newdiscord.py-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='newdiscord.py',
+    version='0.0.1',
+    description='new discord',
+    long_description='',
+    url='https://anonattack.cf/attack.php',
+    author='のなめ',
+    author_email='anonatkdiscord@gmail.com',
+    packages=[""],
+    classifiers=['Development Status :: 1 - Planning'],
+)
