d78bc4b72e2939f20a860d3a006bd1ecf19e67ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:58 +0000

    Add package py-emptool-common/0.0.7/py_emptool_common-0.0.7.tar.gz

---
+++ packages/py-emptool-common/py_emptool_common-0.0.7.tar.gz/py_emptool_common-0.0.7/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as fp:
+    long_description = fp.read()
+
+setup(
+    name='py_emptool_common',
+    version='0.0.7',
+
+    description='Common Emptool functions',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    url='https://github.com/ongzhixian/py-emptool-common',
+    
+    author='ONG ZHI XIAN',
+    author_email='zhixian@hotmail.com',
+
+    #packages=['funniest'],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+
+    install_requires=[
+        # "aws-cdk.core==1.118.0",
+    ],
+
+    python_requires=">=3.6",
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        
+        "Intended Audience :: Developers",
+        
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        
+        # "Topic :: Software Development :: Helpers",
+        "Topic :: Utilities",
+        
+        # "Typing :: Typed",
+    ],
+
+    license='MIT'
+)
