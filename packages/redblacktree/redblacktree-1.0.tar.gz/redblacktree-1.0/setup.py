57ca3bd9d3ee407b2836c83b3760add185014a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:46 +0000

    Add package redblacktree/1.0/redblacktree-1.0.tar.gz

---
+++ packages/redblacktree/redblacktree-1.0.tar.gz/redblacktree-1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='redblacktree',
+    version='1.0',    
+    description='A pure python3 red black tree implementation',
+    url='https://github.com/leryss/redblacktree',
+	download_url='https://github.com/leryss/py-redblacktree/archive/refs/tags/v1.0.tar.gz',
+    author='leryss',
+    license='MIT',
+    packages=['redblacktree'],
+    install_requires=[],
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
