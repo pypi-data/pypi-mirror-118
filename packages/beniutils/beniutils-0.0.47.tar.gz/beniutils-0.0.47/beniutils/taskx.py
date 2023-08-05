3f27dddb336b4fb8c3b45539cf864b8ac8c06569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:55 +0000

    Add package beniutils/0.0.47/beniutils-0.0.47.tar.gz

---
+++ packages/beniutils/beniutils-0.0.47.tar.gz/beniutils-0.0.47/beniutils/taskx.py
@@ -0,0 +1,14 @@
+import beniutils as b
+
+
+def create(targetPath=""):
+    # file = b.getPath("./../data_files/xx.dat")
+    print(__file__)
+    # print(b.readFile(file))
+
+
+def build(targetPath=""):
+    pass
+
+
+create()
