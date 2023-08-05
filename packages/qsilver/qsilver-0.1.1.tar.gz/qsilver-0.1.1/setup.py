516592726a10511b251395ae8708f4f1b8aaab3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:37 +0000

    Add package qsilver/0.1.1/qsilver-0.1.1.tar.gz

---
+++ packages/qsilver/qsilver-0.1.1.tar.gz/qsilver-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+with open("qsilver/README.md") as f:
+    long_description = f.read()
+
+setup(
+    name='qsilver',
+    version='0.1.1',
+    license='MIT',
+    author="Vsevolod Vlasov",
+    author_email='sesevasa64@gmail.com',
+    packages=find_packages('qsilver'),
+    package_dir={'': 'qsilver'},
+    url='https://github.com/sesevasa64/qsilver',
+    keywords=["async"],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
