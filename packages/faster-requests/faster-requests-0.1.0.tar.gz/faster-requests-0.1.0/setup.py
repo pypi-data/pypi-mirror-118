cb2315f74cd4b8f53be5a5cf2923f56d318b8484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:17 +0000

    Add package faster-requests/0.1.0/faster-requests-0.1.0.tar.gz

---
+++ packages/faster-requests/faster-requests-0.1.0.tar.gz/faster-requests-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+version = "0.1.0"
+description = "Faster requests for Python 3"
+long_description = "Faster-Requests is a simple package to send fast requests with Python 3"
+
+setup(
+    name="faster-requests",
+    version=version,
+    author="Dropout",
+    description=description,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[
+        "socket",
+        "ssl"
+    ],
+    keywords=[
+        "python",
+        "requests",
+        "fast requests",
+        "faster requests"
+    ],
+    classifiers=[
+            "Intended Audience :: Developers",
+            "Programming Language :: Python :: 3",
+            "Operating System :: Unix",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
