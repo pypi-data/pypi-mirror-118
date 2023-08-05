f0a3d2756e41537ce873e84039ba5ff3f9d7c123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:46 +0000

    Add package spft/0.0.2/spft-0.0.2.tar.gz

---
+++ packages/spft/spft-0.0.2.tar.gz/spft-0.0.2/spft/multiprocess.py
@@ -0,0 +1,35 @@
+from tqdm import tqdm
+from multiprocessing import Process, Queue
+
+def worker(input, output):
+    for idx, func, data in iter(input.get, 'STOP'):
+        result = func(data)
+        output.put((idx, result))
+
+
+def multi_process(func, iterable, nworkers):
+    NUMBER_OF_PROCESSES = nworkers
+    results = []
+
+    # Create queues
+    task_queue = Queue()
+    done_queue = Queue()
+
+    # Submit tasks
+    for idx, task in enumerate(iterable):
+        task_queue.put((idx, func, task))
+
+    # Start worker processes
+    for i in range(NUMBER_OF_PROCESSES):
+        Process(target=worker, args=(task_queue, done_queue)).start()
+
+    # get result
+    for _ in tqdm(range(len(iterable))):
+        results.append(done_queue.get())
+
+    # Tell child processes to stop
+    for i in range(NUMBER_OF_PROCESSES):
+        task_queue.put('STOP')
+
+    results.sort(key=lambda x: x[0])
+    return [x[1] for x in results]
