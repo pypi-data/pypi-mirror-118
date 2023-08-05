a47a418a4b6fbd7a41e676a6794c3021a5d27947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:17 +0000

    Add package wdapy/0.0.3/wdapy-0.0.3-py3-none-any.whl

---
+++ packages/wdapy/wdapy-0.0.3-py3-none-any.whl/wdapy/_proto.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+#
+
+import enum
+
+DEFAULT_WDA_URL = "http://localhost:8100"
+
+
+class RequestMethod(str, enum.Enum):
+    GET = "GET"
+    POST = "POST"
+
+
+GET = RequestMethod.GET
+POST = RequestMethod.POST
+
+
+class AppState(enum.IntEnum):
+    STOPPED = 1
+    BACKGROUND = 2
+    RUNNING = 4
+
+
+class Orientation(str, enum.Enum):
+    LANDSCAPE = 'LANDSCAPE'
+    PORTRAIT = 'PORTRAIT'
+    LANDSCAPE_RIGHT = 'UIA_DEVICE_ORIENTATION_LANDSCAPERIGHT'
+    PORTRAIT_UPSIDEDOWN = 'UIA_DEVICE_ORIENTATION_PORTRAIT_UPSIDEDOWN'
