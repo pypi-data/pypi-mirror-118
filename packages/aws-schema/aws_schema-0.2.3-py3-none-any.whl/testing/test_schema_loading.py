1f592737f9ddf4c934e61e7c5d40d6c3e0da5071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:11 +0000

    Add package aws-schema/0.2.3/aws_schema-0.2.3-py3-none-any.whl

---
+++ packages/aws-schema/aws_schema-0.2.3-py3-none-any.whl/testing/test_schema_loading.py
@@ -0,0 +1,40 @@
+from unittest import TestCase
+from os.path import dirname, realpath
+from fil_io.json import load_single
+
+
+class TestSchemaLoadingFromFile(TestCase):
+    def test_load_basic_schema(self):
+        from aws_schema.schema_validator import (
+            SchemaValidator,
+        )
+
+        schema_file = (
+            f"{dirname(realpath(__file__))}/test_data/database/schema_basic.json"
+        )
+
+        expected_schema = load_single(schema_file)
+
+        validator = SchemaValidator(file=schema_file)
+        loaded_schema = validator.schema
+        self.assertEqual(expected_schema, loaded_schema)
+
+    def test_load_nested_schema(self):
+        from aws_schema.schema_validator import (
+            SchemaValidator,
+        )
+
+        base_schema_file = (
+            f"{dirname(realpath(__file__))}/test_data/database/schema_nested.json"
+        )
+
+        expected_schema = load_single(base_schema_file)
+
+        validator = SchemaValidator(file=base_schema_file)
+        loaded_schema = validator.schema
+
+        self.assertEqual(expected_schema, loaded_schema)
+
+
+class TestSchemaLoadingFromURL(TestCase):
+    pass
