50835b3c027b9240b567205418cbacd29adb26e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:37 +0000

    Add package htmlToZip/0.2/htmlToZip-0.2.tar.gz

---
+++ packages/htmlToZip/htmlToZip-0.2.tar.gz/htmlToZip-0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="htmlToZip",
+    version=0.2,
+    author="Mahendra kumar",
+    author_email="mksuthar9016@gmail.com",
+    description="Convert your html, css and js code into project",
+    long_description="this is desc",
+    long_description_content_type="text/markdown",
+    url="https://github.com/mahendra-suthar/html-to-url",
+    project_urls={
+        "Bug Tracker": "https://github.com/mahendra-suthar/html-to-url/issues"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "html_to_url"},
+    packages=setuptools.find_packages(where="html_to_url"),
+    python_requires=">=3.6",
+)
