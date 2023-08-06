bc3694948a6dc2137a6ec32b94c76a159d0bf67b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:54 +0000

    Add package pyww3/0.1.4/pyww3-0.1.4.tar.gz

---
+++ packages/pyww3/pyww3-0.1.4.tar.gz/pyww3-0.1.4/pyww3/__init__.py
@@ -0,0 +1,18 @@
+"""Top-level package for pyww3."""
+
+__author__ = """Caio Stringari"""
+__email__ = 'caio.stringari@gmail.com'
+__version__ = '0.1.0'
+
+
+from pyww3.shel import WW3Shel
+from pyww3.grid import WW3GRid
+from pyww3.ounf import WW3Ounf
+from pyww3.ounp import WW3Ounp
+from pyww3.prnc import WW3Prnc
+from pyww3.shel import WW3Shel
+from pyww3.ww3 import WW3Base
+from pyww3.namelists import *
+from pyww3.utils import *
+
+# __all__ = ['shel', 'prnc', 'ounf', 'utils', 'grid', 'ww3', 'namelists', 'bounc']
