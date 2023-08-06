c484ec3a1d975d6e0d7197327be1b7a98911097f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:56 +0000

    Add package canedge-browser/0.0.8/canedge_browser-0.0.8-py3-none-any.whl

---
+++ packages/canedge-browser/canedge_browser-0.0.8-py3-none-any.whl/canedge_browser/__init__.py
@@ -0,0 +1,8 @@
+from canedge_browser.listing import get_first_timestamp, get_log_files
+from canedge_browser.support.LocalFileSystem import LocalFileSystem
+from canedge_browser.support.RelativeFileSystem import RelativeFileSystem
+import canedge_browser.config as config
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
