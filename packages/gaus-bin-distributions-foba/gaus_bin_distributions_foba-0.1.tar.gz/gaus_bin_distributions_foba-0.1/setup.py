3a83d72ffb54ba6112906b35e306e020f2d0319e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:34 +0000

    Add package gaus-bin-distributions-foba/0.1/gaus_bin_distributions_foba-0.1.tar.gz

---
+++ packages/gaus-bin-distributions-foba/gaus_bin_distributions_foba-0.1.tar.gz/gaus_bin_distributions_foba-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaus_bin_distributions_foba',
+      version='0.1',
+      description='Gaussian and binomial distributions foba',
+      packages=['gaus_bin_distributions_foba'],
+      author='Fiyinfoba Ogunkeye',
+      author_email='fobaogunkeye@gmail.com',
+      zip_safe=False)
