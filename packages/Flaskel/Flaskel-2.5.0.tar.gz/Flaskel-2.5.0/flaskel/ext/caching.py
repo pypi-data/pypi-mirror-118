2fbd9978e7ed9a26f756184895b505be8943603a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:42 +0000

    Add package Flaskel/2.5.0/Flaskel-2.5.0.tar.gz

---
+++ packages/Flaskel/Flaskel-2.5.0.tar.gz/Flaskel-2.5.0/flaskel/ext/caching.py
@@ -0,0 +1,19 @@
+from functools import partial
+
+from flask import request
+from flask_caching import Cache as FlaskCache
+
+from flaskel.http import httpcode
+
+caching = FlaskCache()
+
+
+class Cache:
+    cache = caching
+
+    cached = partial(
+        cache.cached,
+        query_string=True,
+        unless=lambda: "no-store" in (request.headers.get("Cache-Control") or ""),
+        response_filter=lambda r: httpcode.is_ok(r.status_code),
+    )
