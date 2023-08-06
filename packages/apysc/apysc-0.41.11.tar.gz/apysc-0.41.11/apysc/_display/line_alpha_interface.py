3819318edfd6bfeb052569cf1f79372914b50d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:20 +0000

    Add package apysc/0.41.11/apysc-0.41.11.tar.gz

---
+++ packages/apysc/apysc-0.41.11.tar.gz/apysc-0.41.11/apysc/_display/line_alpha_interface.py
@@ -0,0 +1,117 @@
+"""Class implementation for line alpha interface.
+"""
+
+from typing import Dict
+from typing import Union
+
+import apysc as ap
+from apysc._type.revert_interface import RevertInterface
+from apysc._type.variable_name_interface import VariableNameInterface
+
+
+class LineAlphaInterface(VariableNameInterface, RevertInterface):
+
+    _line_alpha: ap.Number
+
+    def _initialize_line_alpha_if_not_initialized(self) -> None:
+        """
+        Initialize _line_alpha attribute if it hasn't been initialized yet.
+        """
+        if hasattr(self, '_line_alpha'):
+            return
+        self._line_alpha = ap.Number(1.0)
+
+    @property
+    def line_alpha(self) -> ap.Number:
+        """
+        Get this instance's line alpha (opacity).
+
+        Returns
+        -------
+        line_alpha : Number
+            Current line alpha (opacity. 0.0 to 1.0).
+        """
+        with ap.DebugInfo(
+                callable_='line_alpha', locals_=locals(),
+                module_name=__name__, class_=LineAlphaInterface):
+            from apysc._type import value_util
+            return value_util.get_copy(value=self._line_alpha)
+
+    @line_alpha.setter
+    def line_alpha(self, value: ap.Number) -> None:
+        """
+        Update this instance's line alpha (opacity).
+
+        Parameters
+        ----------
+        value : Number
+            Line alpha (opacity) to set.
+        """
+        with ap.DebugInfo(
+                callable_='line_alpha', locals_=locals(),
+                module_name=__name__, class_=LineAlphaInterface):
+            self._update_line_alpha_and_skip_appending_exp(value=value)
+            self._line_alpha._append_incremental_calc_substitution_expression()
+            self._append_line_alpha_update_expression()
+
+    def _append_line_alpha_update_expression(self) -> None:
+        """
+        Append line alpha updating expression.
+        """
+        with ap.DebugInfo(
+                callable_=self._append_line_alpha_update_expression,
+                locals_=locals(),
+                module_name=__name__, class_=LineAlphaInterface):
+            expression: str = (
+                f'{self.variable_name}.stroke({{opacity: {self.line_alpha}}});'
+            )
+            ap.append_js_expression(expression=expression)
+
+    def _update_line_alpha_and_skip_appending_exp(
+            self, value: Union[float, ap.Number]) -> None:
+        """
+        Update line alpha and skip appending expression.
+
+        Parameters
+        ----------
+        value : float or Number
+            Line alpha (opacity) to set.
+        """
+        from apysc._validation import color_validation
+        from apysc._validation import number_validation
+        number_validation.validate_num(num=value)
+        color_validation.validate_alpha_range(alpha=value)
+        if isinstance(value, float):
+            value = ap.Number(value)
+        self._line_alpha = value
+
+    _line_alpha_snapshots: Dict[str, float]
+
+    def _make_snapshot(self, snapshot_name: str) -> None:
+        """
+        Make value's snapshot.
+
+        Parameters
+        ----------
+        snapshot_name : str
+            Target snapshot name.
+        """
+        if not hasattr(self, '_line_alpha_snapshots'):
+            self._line_alpha_snapshots = {}
+        if self._snapshot_exists(snapshot_name=snapshot_name):
+            return
+        self._initialize_line_alpha_if_not_initialized()
+        self._line_alpha_snapshots[snapshot_name] = self._line_alpha._value
+
+    def _revert(self, snapshot_name: str) -> None:
+        """
+        Revert value if snapshot exists.
+
+        Parameters
+        ----------
+        snapshot_name : str
+            Target snapshot name.
+        """
+        if not self._snapshot_exists(snapshot_name=snapshot_name):
+            return
+        self._line_alpha._value = self._line_alpha_snapshots[snapshot_name]
---
+++ packages/apysc/apysc-0.41.11.tar.gz/apysc-0.41.11/apysc/_display/radius_interface.py
@@ -0,0 +1,131 @@
+"""Class implementation for radius value interface.
+"""
+
+from typing import Dict
+from typing import Union
+
+import apysc as ap
+from apysc._type.revert_interface import RevertInterface
+from apysc._type.variable_name_interface import VariableNameInterface
+
+
+class RadiusInterface(VariableNameInterface, RevertInterface):
+
+    _radius: ap.Int
+
+    def _initialize_radius_if_not_initialized(self) -> None:
+        """
+        Initialize _radius attribute if it hasn't been initialized yet.
+        """
+        if hasattr(self, '_radius'):
+            return
+        self._radius = ap.Int(0)
+
+    @property
+    def radius(self) -> ap.Int:
+        """
+        Get radius value.
+
+        Returns
+        -------
+        radius : Int
+            Radius value.
+        """
+        with ap.DebugInfo(
+                callable_='radius', locals_=locals(),
+                module_name=__name__, class_=RadiusInterface):
+            from apysc._type import value_util
+            self._initialize_radius_if_not_initialized()
+            return value_util.get_copy(value=self._radius)
+
+    @radius.setter
+    def radius(self, value: ap.Int) -> None:
+        """
+        Update radius value.
+
+        Parameters
+        ----------
+        value : int or Int
+            Radius value.
+        """
+        import apysc as ap
+        with ap.DebugInfo(
+                callable_='radius', locals_=locals(),
+                module_name=__name__, class_=RadiusInterface):
+            from apysc._validation import number_validation
+            number_validation.validate_integer(integer=value)
+            value = self._get_converted_radius_int(radius=value)
+            self._radius = value
+            self._radius._append_incremental_calc_substitution_expression()
+            self._append_radius_update_expression()
+
+    def _get_converted_radius_int(self, radius: Union[int, ap.Int]) -> ap.Int:
+        """
+        Get a radius converted Int instance.
+
+        Parameters
+        ----------
+        radius : int or Int
+            Radius value.
+
+        Returns
+        -------
+        radius : Int
+            Type converted radius value.
+        """
+        import apysc as ap
+        with ap.DebugInfo(
+                callable_=self._get_converted_radius_int, locals_=locals(),
+                module_name=__name__, class_=RadiusInterface):
+            if not isinstance(radius, ap.Int):
+                return ap.Int(radius)
+            return radius
+
+    def _append_radius_update_expression(self) -> None:
+        """
+        Append radius value updating expression.
+        """
+        import apysc as ap
+        with ap.DebugInfo(
+                callable_=self._append_radius_update_expression,
+                locals_=locals(),
+                module_name=__name__, class_=RadiusInterface):
+            from apysc._type import value_util
+            self._initialize_radius_if_not_initialized()
+            value_str: str = value_util.get_value_str_for_expression(
+                value=self._radius)
+            expression: str = (
+                f'{self.variable_name}.radius({value_str});'
+            )
+            ap.append_js_expression(expression=expression)
+
+    _radius_snapshots: Dict[str, int]
+
+    def _make_snapshot(self, snapshot_name: str) -> None:
+        """
+        Make value's snapshot.
+
+        Parameters
+        ----------
+        snapshot_name : str
+            Target snapshot name.
+        """
+        if not hasattr(self, '_radius_snapshots'):
+            self._radius_snapshots = {}
+        if self._snapshot_exists(snapshot_name=snapshot_name):
+            return
+        self._initialize_radius_if_not_initialized()
+        self._radius_snapshots[snapshot_name] = int(self._radius._value)
+
+    def _revert(self, snapshot_name: str) -> None:
+        """
+        Revert value if snapshot exists.
+
+        Parameters
+        ----------
+        snapshot_name : str
+            Target snapshot name.
+        """
+        if not self._snapshot_exists(snapshot_name=snapshot_name):
+            return
+        self._radius._value = self._radius_snapshots[snapshot_name]
