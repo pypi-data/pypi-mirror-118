249232a8fb6bdd0deea541b2a6b4dac1d3f69956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:01 +0000

    Add package photosorganisation/1.0.2.1/photosorganisation-1.0.2.1.tar.gz

---
+++ packages/photosorganisation/photosorganisation-1.0.2.1.tar.gz/photosorganisation-1.0.2.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if 'git+' not in x]
+
+setup(
+    name='photosorganisation',
+    version="1.0.2.1",
+    description=(
+                "This python Package helps you sort and organize photos."
+                "Duplicates,Memes,Screenshots and Blurry Photos are identifed."
+                "Photos are tagged and classified for Geo-Location, Selfies,Animals etc."
+    ),
+    packages=find_packages(),
+    install_requires=requirements,
+    test_suite='tests',
+    #include_package_data: to install data from MANIFEST.in
+    include_package_data=True,
+    scripts=[
+        'scripts/photosorganisation-run'
+    ],
+    zip_safe=False)
