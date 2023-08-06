2da11c644edd7ded2f3d481c6686960d6cabed74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:43 +0000

    Add package pysrgrads/0.3/pysrgrads-0.3.tar.gz

---
+++ packages/pysrgrads/pysrgrads-0.3.tar.gz/pysrgrads-0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+	name = 'pysrgrads',
+	packages = ['pysrgrads'],
+	include_package_data=True,
+	version = '0.3',
+	description = 'Graphic user interface for use with Grads and ctl/grb files',
+	author = 'Julio Raime Perez',
+	author_email = 'julio.raime@unmsm.edu.pe',
+	license = 'gpl-3.0',
+	url = "https://github.com/jjr4Programmer/pysrgrads",
+	classifiers = ["Programming Language :: Python :: 3",\
+				   "Operating System :: OS Independent"]
+	)
