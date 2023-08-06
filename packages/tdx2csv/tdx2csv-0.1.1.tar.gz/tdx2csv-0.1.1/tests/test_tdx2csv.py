cf35a3964c6ddba2d4fc66a2b16439b2b5c3381c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:28 +0000

    Add package tdx2csv/0.1.1/tdx2csv-0.1.1.tar.gz

---
+++ packages/tdx2csv/tdx2csv-0.1.1.tar.gz/tdx2csv-0.1.1/tests/test_tdx2csv.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""Tests for `tdx2csv` package."""
+
+import pytest
+from click.testing import CliRunner
+
+from tdx2csv.__main__ import main
+
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+
+def test_command_line_interface():
+    """Test the CLI."""
+    runner = CliRunner()
+    result = runner.invoke(main, ['--help'])
+
+    assert result.exit_code == 0
+    assert 'Show this message and exit.' in result.output
