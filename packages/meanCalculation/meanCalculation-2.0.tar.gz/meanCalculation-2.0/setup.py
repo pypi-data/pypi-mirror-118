e7ab358d0f3902a36b0ec336061f319991e1d7cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:36 +0000

    Add package meanCalculation/2.0/meanCalculation-2.0.tar.gz

---
+++ packages/meanCalculation/meanCalculation-2.0.tar.gz/meanCalculation-2.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='meanCalculation',
+      version='2.0',
+      description='calculation of mean',
+      packages=['meanCalculation'],
+      author_email='varshakt6@gmail.com',
+      zip_safe=False)
