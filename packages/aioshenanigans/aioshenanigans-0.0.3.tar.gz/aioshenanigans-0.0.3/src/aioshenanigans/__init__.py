b7c9f77e2345d10ebc01053cb2fb5e2583f6dc07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:50 +0000

    Add package aioshenanigans/0.0.3/aioshenanigans-0.0.3.tar.gz

---
+++ packages/aioshenanigans/aioshenanigans-0.0.3.tar.gz/aioshenanigans-0.0.3/src/aioshenanigans/__init__.py
@@ -0,0 +1,5 @@
+from .main import simple_main
+
+__all__ = [
+    "simple_main",
+]
---
+++ packages/aioshenanigans/aioshenanigans-0.0.3.tar.gz/aioshenanigans-0.0.3/src/aioshenanigans/main.py
@@ -0,0 +1,29 @@
+import asyncio
+import logging
+import pprint
+import signal
+from typing import Coroutine, Iterable
+
+
+logger = logging.getLogger(__name__)
+
+
+def _signal_callback(quit_event: asyncio.Event) -> None:
+    quit_event.set()
+
+
+async def simple_main(coroutines: Iterable[Coroutine]):
+    loop = asyncio.get_event_loop()
+
+    quit_event = asyncio.Event()
+    loop.add_signal_handler(signal.SIGINT, _signal_callback, quit_event)
+    loop.add_signal_handler(signal.SIGTERM, _signal_callback, quit_event)
+
+    tasks = [asyncio.create_task(coroutine) for coroutine in coroutines]
+
+    await quit_event.wait()
+
+    for task in tasks:
+        task.cancel()
+    res = await asyncio.gather(*tasks, return_exceptions=True)
+    logger.debug("tasks : %s", pprint.pformat(res))
