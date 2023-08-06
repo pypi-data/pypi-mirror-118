260c709461854e8eadd4e6fbed7f81cc89b80c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:44 +0000

    Add package fly-online/0.0.2/fly_online-0.0.2-py3.7.egg

---
+++ packages/fly-online/fly_online-0.0.2-py3.7.egg/fly_online/fly.py
@@ -0,0 +1,44 @@
+import logging
+import time
+import random
+import argparse
+from .login import Login
+from .ping import ping
+
+
+def random_sleep(mean, std):
+    time_gap = int(random.gauss(mean, std))
+    logging.info("sleep:{} min {:.2f} s".format(time_gap // 60, time_gap % 60))
+    time.sleep(time_gap)
+
+
+def schedule(configs):
+    while True:
+        if not ping(url='https://www.baidu.com'):
+            logging.info("网络异常，尝试登录NBU账号")
+            Login(configs.file).try_all()
+        random_sleep(300, 60)
+
+
+def fly():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.FileHandler("/tmp/fly_online.log"),
+            logging.StreamHandler()
+        ]
+    )
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', type=str, default='~/.local/fly-online/password.txt', help="密码文件位置")
+    parser.add_argument('-l', '--login', default=False, action='store_true', help="直接登录校园内网")
+    args = parser.parse_args()
+    if args.login:
+        Login(args.file).try_all()
+    else:
+        schedule(args)
+
+
+if __name__ == '__main__':
+    fly()
---
+++ packages/fly-online/fly_online-0.0.2-py3.7.egg/fly_online/login.py
@@ -0,0 +1,114 @@
+import time
+import os
+import platform
+import logging
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+
+
+def process_file(filename):
+    filename = os.path.expanduser(filename)
+    if not os.path.exists(filename):
+        raise Exception("未在{}找到密码文件，请写入你的密码文件！".format(filename))
+    accounts = {}
+    for line in open(filename):
+        if line != '\n':
+            account, psd = line.strip().split(',')
+            accounts[account] = psd
+    return accounts
+
+
+def _get_geckodriver_path():
+    if platform.system() == "Windows":
+        path = "driver/geckodriver.exe"
+    elif platform.system() == "Darwin":
+        path = "driver/geckodriver.mac"
+    else:
+        path = "driver/geckodriver.linux"
+    return path
+
+
+class Account:
+    def __init__(self, filename):
+        self.filename = filename
+        self.accounts = process_file(filename)
+
+    def items(self):
+        return self.accounts.items()
+
+    def __delitem__(self, key):
+        """
+        example:
+            del my_account["1611082722"]
+        """
+        del self.accounts[key]
+
+    def __repr__(self):
+        result = ""
+        for k, v in self.accounts.items():
+            result += k + ',' + v + '\n'
+        return result
+
+    def save(self):
+        """
+        unused
+        """
+        with open(self.filename, 'w') as f:
+            f.write(self.__repr__())
+
+
+class Login:
+    def __init__(self, filename, arg="--headless"):
+        self.RETRY = 0
+        self.opt = FirefoxOptions()
+        self.opt.add_argument(arg)
+        self.browser = None
+        self.accounts = Account(filename)
+
+    def try_all(self):
+        self.browser = webdriver.Firefox(options=self.opt, executable_path=_get_geckodriver_path())
+        for account, psd in self.accounts.items():
+            if not self.login(account, psd):
+                logging.error("Account {} is not logged successfully".format(account, psd))
+            else:
+                break
+        self.browser.quit()
+
+    def login(self, account, psd):
+        try:
+            self.browser.get(r"http://10.22.63.253/0.htm")
+            username = self.browser.find_element_by_xpath('//input[@type="text"]')
+            password = self.browser.find_element_by_xpath('//input[@type="password"]')
+
+            username.clear()
+            username.send_keys(account)
+            password.clear()
+            password.send_keys(psd)
+
+            commit = self.browser.find_element_by_id("submit")
+            commit.click()
+            time.sleep(2)
+            if "Drcom" not in self.browser.title:
+                commit = self.browser.find_element_by_id("submit")
+                commit.click()
+                time.sleep(2)
+                return False
+            else:
+                print('Online now. Account used: ', account)
+                self.browser.quit()
+                self.RETRY = 0
+                return True
+        except Exception as e:
+            logging.error("ERROR OCCURRED:{}".format(e))
+            # self.browser = webdriver.Firefox(options=self.opt, executable_path=_get_geckodriver_path())
+            # time.sleep(2)
+            # self.RETRY += 1
+            # if self.RETRY < 10:
+            #     return self.login(account, psd)
+            # else:
+            #     return False
+
+
+if __name__ == '__main__':
+    loginer = Login('./password.txt', "")
+    loginer.try_all()
