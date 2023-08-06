6f4af5d83fcfaf5febf64bedb103322dfcdc2226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:52 +0000

    Add package jrm09/1.0.0/jrm09-1.0.0-py3-none-any.whl

---
+++ packages/jrm09/jrm09-1.0.0-py3-none-any.whl/jrm09/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '1.0.0'
+
+from . import Globals
+from ._ReadCoeffs import _ReadCoeffs
+from ._CoeffGrids import _CoeffGrids
+from ._Schmidt import _Schmidt
+from ._SphHarm import _SphHarm
+from ._Legendre import _Legendre
+from .Model import Model
+from .ModelCart import ModelCart
+from .Test import Test
