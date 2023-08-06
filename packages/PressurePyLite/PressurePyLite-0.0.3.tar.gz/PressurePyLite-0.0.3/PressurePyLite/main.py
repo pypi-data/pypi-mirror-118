d1b8dd014eed56751e99d4f197c41d148552f5a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:51 +0000

    Add package PressurePyLite/0.0.3/PressurePyLite-0.0.3.tar.gz

---
+++ packages/PressurePyLite/PressurePyLite-0.0.3.tar.gz/PressurePyLite-0.0.3/PressurePyLite/main.py
@@ -0,0 +1,38 @@
+from PressurePyLite.ardu import Arduino
+
+import argparse
+
+
+def main():
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-s", "--sensor", required=False,
+                    help="change the port of the sensor arduino")
+    ap.add_argument("-p", "--pump", required=False,
+                    help="change the port of the pump arduino")
+    ap.add_argument("-t", "--target_pressure", required=False,
+                    help="change the pressure")
+    args = vars(ap.parse_args())
+
+    if args["sensor"] is None:
+        sensor = "/dev/cu.usbserial-2320"
+    else:
+        sensor = args["sensor"]
+    if args["pump"] is None:
+        pump = "/dev/cu.usbmodem23301"
+    else:
+        pump = args["pump"]
+    if args["target_pressure"] is None:
+        target_pressure = 120
+    else:
+        target_pressure = args["target_pressure"]
+
+    ardu = Arduino(sensor, pump)
+
+    while True:
+        p, t = ardu.read_port()
+        ardu.pump_connect(p, target_pressure, "go")
+
+
+if __name__ == "__main__":
+    main()
