6871369f41ea345f0b0b5df4f64f40d88a1fd881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:10 +0000

    Add package wtf-tortoise/0.0.3/wtf-tortoise-0.0.3.tar.gz

---
+++ packages/wtf-tortoise/wtf-tortoise-0.0.3.tar.gz/wtf-tortoise-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wtf-tortoise",
+    version="0.0.3",
+    author="sinisaos (packaged by fsecada01",
+    author_email="francis.secada@gmail.com",
+    description="Implementation of WTForms for Tortoise ORM",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fsecada01/wtf-tortoise",
+    project_urls={
+        "Bug Tracker": "https://github.com/fsecada01/wtf-tortoise/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+)
