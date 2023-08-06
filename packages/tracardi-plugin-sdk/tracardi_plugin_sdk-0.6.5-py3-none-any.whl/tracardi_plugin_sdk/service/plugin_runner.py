a8a971b086c6816bb4f7d63a35e55a2b49e93d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:05 +0000

    Add package tracardi-plugin-sdk/0.6.5/tracardi_plugin_sdk-0.6.5-py3-none-any.whl

---
+++ packages/tracardi-plugin-sdk/tracardi_plugin_sdk-0.6.5-py3-none-any.whl/tracardi_plugin_sdk/service/plugin_runner.py
@@ -0,0 +1,25 @@
+import asyncio
+from tracardi_plugin_sdk.action_runner import ActionRunner
+
+
+def run_plugin(plugin: ActionRunner, init, payload):
+    async def main(plugin, init, payload):
+        try:
+
+            build_method = getattr(plugin, "build", None)
+            if build_method and callable(build_method):
+                plugin = await build_method(**init)
+            else:
+                plugin = plugin(**init)
+
+            return await plugin.run(payload)
+
+        except Exception as e:
+            if isinstance(plugin, ActionRunner):
+                await plugin.on_error()
+            raise e
+        finally:
+            if isinstance(plugin, ActionRunner):
+                await plugin.close()
+
+    asyncio.run(main(plugin, init, payload))
