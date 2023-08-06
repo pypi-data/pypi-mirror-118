87ad6caea740db7b875fa95d1e22c5f985b273e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:29 +0000

    Add package ci-plumber-azure/0.2.1/ci_plumber_azure-0.2.1.tar.gz

---
+++ packages/ci-plumber-azure/ci_plumber_azure-0.2.1.tar.gz/ci_plumber_azure-0.2.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ci_plumber_azure']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'ci-plumber-azure',
+    'version': '0.2.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Miles Budden',
+    'author_email': 'git@miles.so',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
