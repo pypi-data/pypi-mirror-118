a3f1662c870037a31c6e09cda66231a14e43cf82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:33 +0000

    Add package loveForGSS/1.1.0/loveForGSS-1.1.0.tar.gz

---
+++ packages/loveForGSS/loveForGSS-1.1.0.tar.gz/loveForGSS-1.1.0/loveForGSS.py
@@ -0,0 +1,8 @@
+"""This is the loveForGSS.py module and it provide description PennLi's admiration for GuoShanShan """
+def describleGSS():
+    """Desricription for GuoShanShan: perfect girl！"""
+    print("珠玉在前，光映照人；\n既见君子，云胡不夷；\n既见君子，云胡不瘳；\n既见君子，云胡不喜；\n珠玉在侧，觉我形秽。")
+
+def showLoveToGSS():
+    """One day show love to GuoShanShan"""
+    print("你愿不愿意和我一起停停走走，平平淡淡；有粥喝粥，有饭吃饭？")
---
+++ packages/loveForGSS/loveForGSS-1.1.0.tar.gz/loveForGSS-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name         = 'loveForGSS',
+        version      = '1.1.0',
+        py_modules   = ['loveForGSS'],
+        author       = 'pennli',
+        author_email = 'lipeng1a@gmail.com',
+        url          = 'https://pypi.org/',
+        description  = "A simple description of PennLi's love to GuoShanShan",
+        long_description = "A simple description of PennLi's love to GuoShanShan",
+     )
