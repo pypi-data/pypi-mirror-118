ccd6f197b1ceb80b59bb9f9fa05d67df5ebe9312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:35 +0000

    Add package k3cgrouparch/0.1.0/k3cgrouparch-0.1.0.tar.gz

---
+++ packages/k3cgrouparch/k3cgrouparch-0.1.0.tar.gz/k3cgrouparch-0.1.0/k3cgrouparch/__init__.py
@@ -0,0 +1,23 @@
+"""
+#   Name
+
+cgrouparch
+
+A python lib used to build cgroup directory tree, add set cgroup pid.
+
+#   Status
+
+This library is considered production ready.
+
+#   Description
+
+This lib is used to set up cgroup directory tree according to
+configuration saved in zookeeper, and add pid to cgroup accordingly.
+
+"""
+
+# from .proc import CalledProcessError
+# from .proc import ProcError
+
+__version__ = "0.1.0"
+__name__ = "k3cgrouparch"
