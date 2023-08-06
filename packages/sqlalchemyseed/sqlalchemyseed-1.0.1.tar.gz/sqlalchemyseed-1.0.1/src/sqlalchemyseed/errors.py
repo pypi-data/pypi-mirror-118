4588dfc1d60e4f6dcc4a7145d4f3c1ae36ee609e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:04 +0000

    Add package sqlalchemyseed/1.0.1/sqlalchemyseed-1.0.1.tar.gz

---
+++ packages/sqlalchemyseed/sqlalchemyseed-1.0.1.tar.gz/sqlalchemyseed-1.0.1/src/sqlalchemyseed/errors.py
@@ -0,0 +1,32 @@
+class ClassNotFoundError(Exception):
+    """Raised when the class is not found"""
+    pass
+
+
+class MissingKeyError(Exception):
+    """Raised when a required key is missing"""
+    pass
+
+
+class MaxLengthExceededError(Exception):
+    """Raised when maximum length of data exceeded"""
+    pass
+
+
+class InvalidTypeError(Exception):
+    """Raised when a type of data is not accepted"""
+    pass
+
+
+class EmptyDataError(Exception):
+    """Raised when data is empty"""
+    pass
+
+
+class InvalidKeyError(Exception):
+    """Raised when an invalid key is invoked"""
+    pass
+
+class ParseError(Exception):
+    """Raised when parsing string fails"""
+    pass
