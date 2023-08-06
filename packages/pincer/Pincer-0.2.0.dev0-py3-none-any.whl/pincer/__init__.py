00aa9e8fc99752edde62b8ae0dce904ed0d45b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:43 +0000

    Add package pincer/0.2.0.dev0/Pincer-0.2.0.dev0-py3-none-any.whl

---
+++ packages/pincer/Pincer-0.2.0.dev0-py3-none-any.whl/pincer/__init__.py
@@ -0,0 +1,15 @@
+"""
+Pincer library
+====================
+An asynchronous python API wrapper meant to replace discord.py
+
+:copyright: (c) 2021-present Pincer
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = "Pincer library"
+__package__ = "Pincer"
+__author__ = "Sigmanificient, Arthurdw"
+__license__ = "MIT"
+__version__ = "0.2.0.dev"
+__description__ = "Discord API wrapper rebuild from scratch."
