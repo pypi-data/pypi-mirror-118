09e85ea0ecbeb9a80d43b7502bd9b25cb0dc5413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:32 +0000

    Add package celebrity-births-test/0.0.4/celebrity_births_test-0.0.4.tar.gz

---
+++ packages/celebrity-births-test/celebrity_births_test-0.0.4.tar.gz/celebrity_births_test-0.0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from setuptools import find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+setup(
+    name='celebrity_births_test',
+    version='0.0.4',    
+    description='Mock package that allows you to find celebrity by date of birth',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/IvanYingX/project_structure_pypi.git',
+    author='Ivan Ying',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['requests', 'beautifulsoup4'],
+)
