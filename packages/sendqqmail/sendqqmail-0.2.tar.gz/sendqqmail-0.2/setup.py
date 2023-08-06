8616a286a59c0b5d72e8ad542579079e485fa1c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:25 +0000

    Add package sendqqmail/0.2/sendqqmail-0.2.tar.gz

---
+++ packages/sendqqmail/sendqqmail-0.2.tar.gz/sendqqmail-0.2/setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup as set
+
+set(name='sendqqmail',version='0.2',package_dir={"":"sendqqmail"},py_modules = ['sendqqmail'],author = 'py3141',author_email = '3386165598@qq.com',url = '',description = 'A send QQ mail package')
