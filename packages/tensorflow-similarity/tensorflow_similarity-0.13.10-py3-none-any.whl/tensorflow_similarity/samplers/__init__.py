2fe91151d6aa3dae8e2952ce93090dc11006f0ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:54 +0000

    Add package tensorflow-similarity/0.13.10/tensorflow_similarity-0.13.10-py3-none-any.whl

---
+++ packages/tensorflow-similarity/tensorflow_similarity-0.13.10-py3-none-any.whl/tensorflow_similarity/samplers/__init__.py
@@ -0,0 +1,5 @@
+from .utils import select_examples  # noqa
+from .memory_samplers import MultiShotMemorySampler  # noqa
+from .memory_samplers import SingleShotMemorySampler  # noqa
+from .tfrecords_samplers import TFRecordDatasetSampler  # noqa
+from .tfdataset_samplers import TFDatasetMultiShotMemorySampler  # noqa
