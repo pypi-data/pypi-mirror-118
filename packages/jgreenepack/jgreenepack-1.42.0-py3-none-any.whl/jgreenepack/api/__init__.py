3d37408980ef468c6f940e6534d7bf19ca4032e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:00 +0000

    Add package jgreenepack/1.42.0/jgreenepack-1.42.0-py3-none-any.whl

---
+++ packages/jgreenepack/jgreenepack-1.42.0-py3-none-any.whl/jgreenepack/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from jgreenepack.api.assisted_service_iso_api import AssistedServiceIsoApi
+from jgreenepack.api.events_api import EventsApi
+from jgreenepack.api.installer_api import InstallerApi
+from jgreenepack.api.managed_domains_api import ManagedDomainsApi
+from jgreenepack.api.manifests_api import ManifestsApi
+from jgreenepack.api.operators_api import OperatorsApi
+from jgreenepack.api.versions_api import VersionsApi
