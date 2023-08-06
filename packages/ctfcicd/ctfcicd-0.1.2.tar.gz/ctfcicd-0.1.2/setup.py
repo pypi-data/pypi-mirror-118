e342ade92d9f8be02ded5f16dbe914bdab044aec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:54 +0000

    Add package ctfcicd/0.1.2/ctfcicd-0.1.2.tar.gz

---
+++ packages/ctfcicd/ctfcicd-0.1.2.tar.gz/ctfcicd-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+REQUIRES = [
+    "docker~=5.0.2",
+    "requests~=2.22.0",
+    "PyYAML~=5.4",
+    "paramiko"
+]
+
+setup(
+    name='ctfcicd',
+    version='0.1.2',
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    url='https://github.com/MrSuicideParrot/ctf-cicd',
+    license='MIT',
+    author='André Cirne',
+    description='Tool for CI/CD pipelines to deploy CTFs',
+    entry_points={"console_scripts": ["ctfcicd = ctfcicd.__main__:main"]},
+)
