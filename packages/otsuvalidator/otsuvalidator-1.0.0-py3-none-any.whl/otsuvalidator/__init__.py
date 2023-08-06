1de94b5e925dad3f09a71dde23bdeee8bd39f001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:35 +0000

    Add package otsuvalidator/1.0.0/otsuvalidator-1.0.0-py3-none-any.whl

---
+++ packages/otsuvalidator/otsuvalidator-1.0.0-py3-none-any.whl/otsuvalidator/__init__.py
@@ -0,0 +1,22 @@
+from .convertors import CBool, CFloat, CInt, CNumber, CPath, CString
+from .validators import (VBool, VChoice, VDict, VFloat, VInt, VList, VNumber, VPath, VRegex, VString, VTuple)
+
+__all__ = (
+    'CBool',
+    'CFloat',
+    'CInt',
+    'CNumber',
+    'CPath',
+    'CString',
+    'VBool',
+    'VChoice',
+    'VDict',
+    'VFloat',
+    'VInt',
+    'VList',
+    'VNumber',
+    'VPath',
+    'VRegex',
+    'VString',
+    'VTuple',
+)
