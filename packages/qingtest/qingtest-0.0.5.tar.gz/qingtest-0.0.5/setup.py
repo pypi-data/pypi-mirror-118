c5c02b32ba11dccfa2da86211ac9b7a19cecd88d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:27 +0000

    Add package qingtest/0.0.5/qingtest-0.0.5.tar.gz

---
+++ packages/qingtest/qingtest-0.0.5.tar.gz/qingtest-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qingtest",
+    version="0.0.5",
+    author="zhoujiahui",
+    author_email="zhoujiahui@exiao.tech",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.qingflow.com",
+    packages=setuptools.find_packages(),
+    install_requires=['selenium==3.141.0','xlrd==1.2.0','XlsxWriter==1.3.7','requests==2.24.0','Appium-Python-Client==1.0.2','Pillow==8.0.1','openpyxl==3.0.5','arrow==0.17.0'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
