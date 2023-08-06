48599421ebf44c3e7c3b630522db08543bf40e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:09 +0000

    Add package pyneval/0.0.13/pyneval-0.0.13-py3-none-any.whl

---
+++ packages/pyneval/pyneval-0.0.13-py3-none-any.whl/pyneval/tools/tiff_cut.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+from pyneval.io import read_json
+from pyneval.io.swc_writer import swc_save
+from pyneval.metric.utils.klib import TiffFile
+from pyneval.metric.utils.tiff_utils import front_expend_step
+from pyneval.model.swc_node import SwcTree
+
+TIFF_PATH = "../../data/optimation/6656_test.tif"
+
+
+def tiff_cut(tiff_data, LDF = (0, 0, 0), len = (64, 64, 64)):
+    res = tiff_data[LDF[0]:LDF[0]+len[0], LDF[1]:LDF[1]+len[1], LDF[2]:LDF[2]+len[2]]
+    return res
