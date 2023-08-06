9e00bdfb5cbaa0d6d58bbe60a70378cb20a63309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:32 +0000

    Add package optifolio/0.4.6/optifolio-0.4.6.tar.gz

---
+++ packages/optifolio/optifolio-0.4.6.tar.gz/optifolio-0.4.6/optifolio/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for optifolio."""
+
+__author__ = """Kristian Bonnici"""
+__email__ = 'kristiandaaniel@gmail.com'
+__version__ = '0.4.6'
+
+from ._base import PortfolioOptimizer
