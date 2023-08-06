e437c7b9f136130f8ee242d7783d0879d35cae42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:54 +0000

    Add package sq-native/1.1.2.0.0/sq_native-1.1.2.0.0-py2.py3-none-manylinux1_x86_64.whl

---
+++ packages/sq-native/sq_native-1.1.2.0.0-py2.py3-none-manylinux1_x86_64.whl/sq_native/__about__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2016, 2017, 2018, 2019, 2020, Sqreen. All rights reserved.
+# Please refer to our terms for more information:
+#
+#     https://www.sqreen.io/terms.html
+#
+
+# The lib version is always contained at the beginning of __version__
+__version__ = "1.1.2.0.0"
+__lib_version__ = "1.1.2"
+__waf_version__ = "1.0.6"
