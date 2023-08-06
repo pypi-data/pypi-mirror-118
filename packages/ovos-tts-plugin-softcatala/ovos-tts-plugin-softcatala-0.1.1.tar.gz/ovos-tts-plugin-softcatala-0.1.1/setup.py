950f27244e3f3ee6ecd5841d0304a9392602fa5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:29 +0000

    Add package ovos-tts-plugin-softcatala/0.1.1/ovos-tts-plugin-softcatala-0.1.1.tar.gz

---
+++ packages/ovos-tts-plugin-softcatala/ovos-tts-plugin-softcatala-0.1.1.tar.gz/ovos-tts-plugin-softcatala-0.1.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+
+PLUGIN_ENTRY_POINT = 'ovos-tts-plugin-softcatala = ' \
+                     'ovos_tts_plugin_softcatala:SoftcatalaTTSPlugin'
+setup(
+    name='ovos-tts-plugin-softcatala',
+    version='0.1.1',
+    description='A catalan tts plugin for mycroft',
+    url='https://github.com/OpenVoiceOS/ovos-tts-plugin-softcatala',
+    author='JarbasAi',
+    author_email='jarbasai@mailfence.com',
+    license='Apache-2.0',
+    packages=['ovos_tts_plugin_softcatala'],
+    install_requires=["ovos-plugin-manager>=0.0.1a12",
+                      "requests"],
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Text Processing :: Linguistic',
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='mycroft ovos plugin tts',
+    entry_points={'mycroft.plugin.tts': PLUGIN_ENTRY_POINT}
+)
