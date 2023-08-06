0f796ebe1ef1aa4d35dbb49409224a906b4f2856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:35 +0000

    Add package tex-engine/0.1.2/tex_engine-0.1.2-py3-none-any.whl

---
+++ packages/tex-engine/tex_engine-0.1.2-py3-none-any.whl/tex_engine/fragment.py
@@ -0,0 +1,22 @@
+from tex_engine.vars import VAR_FRAGMENT, VAR_TOKEN_START, BLOCK_TOKEN_START, OPEN_BLOCK_FRAGMENT, TEXT_FRAGMENT, CLOSE_BLOCK_FRAGMENT
+
+
+class Fragment(object):
+    def __init__(self, raw_text):
+        self.raw = raw_text
+        self.clean = self.clean_fragment()
+
+    def clean_fragment(self):
+        if self.raw[:2] in (VAR_TOKEN_START, BLOCK_TOKEN_START):
+            return self.raw.strip()[2:-2].strip()
+        return self.raw
+
+    @property
+    def type(self):
+        raw_start = self.raw[:2]
+        if raw_start == VAR_TOKEN_START:
+            return VAR_FRAGMENT
+        elif raw_start == BLOCK_TOKEN_START:
+            return CLOSE_BLOCK_FRAGMENT if self.clean[:3] == 'end' else OPEN_BLOCK_FRAGMENT
+        else:
+            return TEXT_FRAGMENT
