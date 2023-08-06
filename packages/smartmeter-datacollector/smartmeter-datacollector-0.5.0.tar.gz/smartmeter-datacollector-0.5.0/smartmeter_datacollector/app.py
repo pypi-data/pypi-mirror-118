bb0dcca2680508b58c12805a943307cc5f59ebae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:08 +0000

    Add package smartmeter-datacollector/0.5.0/smartmeter-datacollector-0.5.0.tar.gz

---
+++ packages/smartmeter-datacollector/smartmeter-datacollector-0.5.0.tar.gz/smartmeter-datacollector-0.5.0/smartmeter_datacollector/app.py
@@ -0,0 +1,61 @@
+#
+# Copyright (C) 2021 Supercomputing Systems AG
+# This file is part of smartmeter-datacollector.
+#
+# SPDX-License-Identifier: GPL-2.0-only
+# See LICENSES/README.md for more information.
+#
+import argparse
+import asyncio
+import logging
+from asyncio import CancelledError
+from configparser import ConfigParser
+
+from . import config, factory
+
+logging.basicConfig(level=logging.WARNING)
+
+
+async def build_and_start(app_config: ConfigParser):
+    readers = factory.build_readers(app_config)
+    sinks = factory.build_sinks(app_config)
+    data_collector = factory.build_collector(readers, sinks)
+
+    await asyncio.gather(*[sink.start() for sink in sinks])
+
+    try:
+        await asyncio.gather(
+            *[reader.start() for reader in readers],
+            data_collector.process_queue(),
+            return_exceptions=True)
+    except CancelledError:
+        logging.info("App shutting down now.")
+        await asyncio.gather(*[sink.stop() for sink in sinks])
+
+
+def set_logging_levels(app_config: ConfigParser) -> None:
+    if not app_config.has_section("logging"):
+        return
+    # configure root logger
+    logging.getLogger().setLevel(app_config["logging"].get('default', logging.WARNING))
+    # configure individual loggers
+    for name, level in app_config["logging"].items():
+        logging.getLogger(name).setLevel(level)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Smart Meter Data Collector", add_help=True)
+    parser.add_argument(
+        '-c', '--config', help="File path of the configuration (.ini) file.", default="./datacollector.ini")
+    parser.add_argument(
+        '-d', '--dev', help="Development mode", action='store_true')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    debug_mode = True if args.dev else False
+    app_config = config.read_config_files(args.config)
+    set_logging_levels(app_config)
+    asyncio.run(build_and_start(app_config), debug=debug_mode)
---
+++ packages/smartmeter-datacollector/smartmeter-datacollector-0.5.0.tar.gz/smartmeter-datacollector-0.5.0/smartmeter_datacollector/sinks/logger_sink.py
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2021 Supercomputing Systems AG
+# This file is part of smartmeter-datacollector.
+#
+# SPDX-License-Identifier: GPL-2.0-only
+# See LICENSES/README.md for more information.
+#
+import logging
+
+from ..smartmeter.reader_data import ReaderDataPoint
+from .data_sink import DataSink
+
+
+class LoggerSink(DataSink):
+    def __init__(self, logger_name: str) -> None:
+        self._logger = logging.getLogger(logger_name)
+        self._logger.setLevel(logging.INFO)
+
+    async def start(self) -> None:
+        pass
+
+    async def stop(self) -> None:
+        pass
+
+    async def send(self, data_point: ReaderDataPoint) -> None:
+        self._logger.info(str(data_point))
