b22f1c0bd4fa480f0efdc7dfdfb1e1b4a026340a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:32 +0000

    Add package Dalpsy/0.12/Dalpsy-0.12.tar.gz

---
+++ packages/Dalpsy/Dalpsy-0.12.tar.gz/Dalpsy-0.12/setup.py
@@ -0,0 +1,23 @@
+import setuptools as x
+
+with open('README.md') as f:
+    long_description = f.read()
+
+x.setup(
+    name='Dalpsy',
+    version='0.12',
+    author='Siddhant Kumar',
+    description='Anime, Manga API wrapper for Anime-Planet',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=x.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.5",
+    py_modules=['Dalpsy'],
+    package_dir={'':'Dalpsy/src'},
+    install_requires=['bs4']
+)
