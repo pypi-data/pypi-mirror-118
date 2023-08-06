2c8fd1cdbf6545478b2efde0a9e2f30f245d8f1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:48 +0000

    Add package divbasiccalculator/0.0.4/divbasiccalculator-0.0.4.tar.gz

---
+++ packages/divbasiccalculator/divbasiccalculator-0.0.4.tar.gz/divbasiccalculator-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Unix',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='divbasiccalculator',
+  version='0.0.4',
+  description='A very basic calculator',
+  long_description=open('readme.md').read(),
+  long_description_content_type='text/markdown',
+  url='',  
+  author='Divit Kanath',
+  author_email='divitkanath@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='calculator', 
+  packages=['divcalc'],
+  include_package_data=True,
+  install_requires=[''] 
+)
