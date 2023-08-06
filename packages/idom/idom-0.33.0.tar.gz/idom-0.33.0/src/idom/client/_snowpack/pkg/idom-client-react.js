a2c7c2a9c22ef6e0bf0b9090c8c74f05ef034c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:59 +0000

    Add package idom/0.33.0/idom-0.33.0.tar.gz

---
+++ packages/idom/idom-0.33.0.tar.gz/idom-0.33.0/src/idom/client/_snowpack/pkg/idom-client-react.js
@@ -0,0 +1,5 @@
+export { m as mountLayoutWithWebSocket } from './common/mount-1506d689.js';
+import './common/index-6ed86a98.js';
+import './common/index-21e68f69.js';
+import './common/htm.module-dd7abb54.js';
+import './common/index-6ddd8323.js';
