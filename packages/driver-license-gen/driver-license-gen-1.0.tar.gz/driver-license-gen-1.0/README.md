471ed8210709c56e282a73229194dbeba861e1a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:22 +0000

    Add package driver-license-gen/1.0/driver-license-gen-1.0.tar.gz

---
+++ packages/driver-license-gen/driver-license-gen-1.0.tar.gz/driver-license-gen-1.0/README.md
@@ -0,0 +1,10 @@
+# driver license generator
+
+U.S. driver license generator
+
+
+## Test
+
+For running unit tests, use
+
+    python -m unittest test
---
+++ packages/driver-license-gen/driver-license-gen-1.0.tar.gz/driver-license-gen-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='driver-license-gen',
+      version='1.0',
+      description='U.S. driver license generator',
+      packages=['distributions'],
+      author = 'Roggeo',
+      author_email = 'geovanirog@gmail.com',
+      zip_safe=False)
