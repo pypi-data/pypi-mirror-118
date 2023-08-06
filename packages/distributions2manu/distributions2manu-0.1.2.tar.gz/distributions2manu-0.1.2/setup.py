729ac79da7e377e14890c37ab8c2e3ec5fbe00ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:34 +0000

    Add package distributions2manu/0.1.2/distributions2manu-0.1.2.tar.gz

---
+++ packages/distributions2manu/distributions2manu-0.1.2.tar.gz/distributions2manu-0.1.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions2manu',
+      version='0.1.2',
+      description='Gaussian distributions',
+      packages=['distributions2manu'],
+      zip_safe=False)
