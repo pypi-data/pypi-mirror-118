53725f38bc75c850cfa45e417d898ffde9c14437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:09 +0000

    Add package pyXhWinClipTool/0.0.3/pyXhWinClipTool-0.0.3.tar.gz

---
+++ packages/pyXhWinClipTool/pyXhWinClipTool-0.0.3.tar.gz/pyXhWinClipTool-0.0.3/src/pyXhWinClipTool.py
@@ -0,0 +1,31 @@
+import subprocess
+import argparse
+import sys
+
+
+def from_clip() -> str:
+    return subprocess.run("powershell get-clipboard", universal_newlines=True, capture_output=True).stdout
+
+
+def to_clip(text: str, replace: bool = False):
+    if replace:
+        subprocess.run("clip", universal_newlines=True, input=text.replace("\n", "\\n").replace("\t", "\\t"))
+    else:
+        subprocess.run("clip", universal_newlines=True, input=text)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="clip board tools for windows only(default --copy)")
+    parser.add_argument("-p", "--paste", dest='copy_cmd', help="retrieve data from clipboard", action='store_false')
+    parser.add_argument("-c", "--copy", dest='copy_cmd', help="copy input to clipboard", action='store_true')
+    parser.set_defaults(copy_cmd=True)
+    args = parser.parse_args()
+
+    if args.copy_cmd :
+        data = ""
+        for line in sys.stdin:
+            data = data+line
+        to_clip(data)
+    else:
+        data = from_clip()
+        print(data)
