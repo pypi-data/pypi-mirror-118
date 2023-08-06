078e956ad9e34df882dd91264cb80c3dadecc565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:16 +0000

    Add package sanime/0.2/sanime-0.2.tar.gz

---
+++ packages/sanime/sanime-0.2.tar.gz/sanime-0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    ld = f.read()
+
+setuptools.setup(
+    name="sanime",
+    version="0.2",
+    description="AniManga wrapper for Anime-Planet.",
+    long_description=ld,
+    url="https://github.com/centipede000",
+    author="Siddhant Kumar",
+    author_email="centipedemonster@gmail.com",
+    license="MIT",
+    packages=setuptools.find_packages(where="src"),
+    zip_safe=False,
+    package_dir={"":"sanime"},
+    python_requires=">=3.6",
+)
