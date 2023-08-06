7e557a1186aee2d35d2bbcac3c42adc1dee22dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:17 +0000

    Add package as4pgc/1.1.4/as4pgc-1.1.4.tar.gz

---
+++ packages/as4pgc/as4pgc-1.1.4.tar.gz/as4pgc-1.1.4/README.md
@@ -0,0 +1,42 @@
+# as4pgc
+Hide files inside audio files.
+
+Use "Audio Steganography for Pretty Good Concealing" to compress, encrypt, and hide small files of any type inside compressed audio files (e.g. MP3, FLAC, OGG) or inside uncompressed .WAV files.
+
+## Installation
+```sh
+  pip install as4pgc
+```
+You also need to install ffmpeg. Download it from here:
+https://www.ffmpeg.org/
+
+## How to use it?
+Hide a file of any type inside an .mp3 file:
+```sh
+  as4pgc -w secret.zip carrier.mp3
+```
+Use option -p to activate plots and track signal processing steps.
+
+Use option -v to output details.
+
+Then recover the hidden file:
+```sh
+  as4pgc -r stego.mp3
+```
+
+Use the option -h for more information:
+
+```sh
+  as4pgc -h
+```
+
+Configuration settings can be adapted in config.ini (detailed documentation to be provided soon).
+## GitHub Project
+
+https://github.com/ClarkFieseln/as4pgc
+
+## License
+
+(c) 2021 Clark Fieseln
+
+This repository is licensed under the MIT license. See LICENSE for details.
---
+++ packages/as4pgc/as4pgc-1.1.4.tar.gz/as4pgc-1.1.4/setup.py
@@ -0,0 +1,49 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+__version__ = "1.1.4"
+
+# This call to setup() does all the work
+setup(
+    name="as4pgc",
+    version=__version__,
+    description = "Audio Steganography: compress, encrypt and hide a secret file inside an audio file (MP3, WAV, OGG, FLAC, ..)",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ClarkFieseln/as4pgc",
+    author="Clark Fieseln",
+    author_email="",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Security",
+    ],
+    packages=["as4pgc"],
+    include_package_data=True,
+    install_requires=['SoundFile','matplotlib','dataclasses','cryptography','numpy','scipy','bitarray','tinytag','simpleaudio'],
+    dependency_links=['https://www.ffmpeg.org/'],
+    keywords=['steganography','stego','audio','hide','cryptography>=3.4.7','encryption','compression','security','cybersecurity','mp3','flac','ogg','wav','windows','linux'],
+    entry_points={
+        "console_scripts": [
+            "as4pgc=as4pgc.AS4PGC:main",
+        ]
+    },
+)
+
+
