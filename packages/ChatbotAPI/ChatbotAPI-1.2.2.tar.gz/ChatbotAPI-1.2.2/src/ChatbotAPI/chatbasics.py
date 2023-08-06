1173d82a3fc5b2e6752b47bc60f4747a78c7cef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:30 +0000

    Add package ChatbotAPI/1.2.2/ChatbotAPI-1.2.2.tar.gz

---
+++ packages/ChatbotAPI/ChatbotAPI-1.2.2.tar.gz/ChatbotAPI-1.2.2/src/ChatbotAPI/chatbasics.py
@@ -0,0 +1,36 @@
+import requests
+import pyjokes
+
+URL = "http://api.brainshop.ai/get"
+
+
+brain = ""
+apikey = ""
+uiid = ""
+msg = ""
+
+def chatbotsetup(brainid, apikeyuser, uiiduser = "PythonChatbot"):
+    global brain
+    global apikey
+    global uiid
+    brain = brainid
+    apikey = apikeyuser
+    uiid = uiiduser
+
+def sendmsg(message1):
+    msg = message1
+    if 'jokes' in msg or 'joke' in msg :
+        data = "Here is a joke : " + pyjokes.get_joke()
+        return data
+    PARAMS = {
+        'bid': brain,
+        'key': apikey,
+        'uid': uiid,
+        'msg': msg
+    }
+    data = requests.get(url=URL, params=PARAMS).json()['cnt']
+    
+    return data
+
+
+                                                                                                                                                                                                                                                              
