36bdc04ec79cc07e7c52f48f8bea41f70ee2941e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:19 +0000

    Add package temci/0.8.5/temci-0.8.5.tar.gz

---
+++ packages/temci/temci-0.8.5.tar.gz/temci-0.8.5/setup.py
@@ -0,0 +1,57 @@
+from os import path
+
+from setuptools import setup, find_packages
+
+import temci.scripts.version as version
+
+print(version.version)
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='temci',
+    author="Johannes Bechberger",
+    author_email="me@mostlynerdless.de",
+    description='Advanced benchmarking tool',
+    long_description=long_description,
+    url="https://github.com/parttimenerd/temci",
+    version=version.version,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'humanfriendly', 'pytimeparse',
+        'fn',
+        'cpuset-py3',
+        'wcwidth',
+        'rainbow_logging_handler',
+        'tablib',
+        'pyyaml',
+        'seaborn', 'matplotlib',
+        'psutil'
+    ],
+    tests_require=['pytest'],
+    license='GPLv3',
+    platforms='linux',
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: System :: Benchmark",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        'Intended Audience :: Developers',
+
+    ],
+    entry_points='''
+        [console_scripts]
+        temci=temci.scripts.run:run
+        temci_completion=temci.scripts.temci_completion:cli
+    '''
+)
---
+++ packages/temci/temci-0.8.5.tar.gz/temci-0.8.5/temci/scripts/version.py
@@ -0,0 +1,6 @@
+"""
+Contains the current version of temci.
+"""
+
+version = "0.8.5"
+""" The current version of temci """
