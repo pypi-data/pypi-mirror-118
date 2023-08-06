9e4da170962c38b19d9e30b9c84c72506cb5db3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:23 +0000

    Add package sportinf/1.0.0/sportinf-1.0.0.tar.gz

---
+++ packages/sportinf/sportinf-1.0.0.tar.gz/sportinf-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sportinf',
+    version='1.0.0',
+    packages=['tests', 'sport_info'],
+    url='https://github.com/AlexBlackHawk/sportinf',
+    license='MIT',
+    author='alexm',
+    author_email='alex.maudza@gmail.com',
+    description='Package to get sport information'
+)
