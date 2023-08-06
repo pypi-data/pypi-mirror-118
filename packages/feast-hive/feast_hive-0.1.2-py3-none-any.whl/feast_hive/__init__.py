4c3afe25dd18672d431afa74d44a0e403abdb7b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:53 +0000

    Add package feast-hive/0.1.2/feast_hive-0.1.2-py3-none-any.whl

---
+++ packages/feast-hive/feast_hive-0.1.2-py3-none-any.whl/feast_hive/__init__.py
@@ -0,0 +1,4 @@
+from .hive import HiveOfflineStore, HiveOfflineStoreConfig
+from .hive_source import HiveOptions, HiveSource
+
+__all__ = ["HiveOptions", "HiveSource", "HiveOfflineStoreConfig", "HiveOfflineStore"]
