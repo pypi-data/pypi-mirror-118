bafb15d5498c5a3ecdf7d80db2a8dbc8afad946c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:24 +0000

    Add package smsman/1.0.0/smsman-1.0.0.tar.gz

---
+++ packages/smsman/smsman-1.0.0.tar.gz/smsman-1.0.0/smsman/errors.py
@@ -0,0 +1,17 @@
+class Error(Exception):
+    pass
+
+
+class WrongTokenError(Error):
+    def __init__(self, error_message):
+        self.error_message = error_message
+
+
+class SMSnotReceivedError(Error):
+    def __init__(self, error_message):
+        self.error_message = error_message
+
+
+class LowBalance(Error):
+    def __init__(self, error_message):
+        self.error_message = error_message
