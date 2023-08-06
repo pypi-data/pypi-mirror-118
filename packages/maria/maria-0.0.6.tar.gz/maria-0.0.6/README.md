0daa483bb6f8443cda7426e26dfbdd135c5d4c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:17 +0000

    Add package maria/0.0.6/maria-0.0.6.tar.gz

---
+++ packages/maria/maria-0.0.6.tar.gz/maria-0.0.6/README.md
@@ -0,0 +1,1 @@
+maria is a Python package that simulates and realizes time-ordered atmospheric fluctuations in ground-based telescopes.
---
+++ packages/maria/maria-0.0.6.tar.gz/maria-0.0.6/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="maria",                     # This is the name of the package
+    version="0.0.6",                 # The initial release version
+    author="Thomas Morris",          # Full name of the author
+    description='maria (Modeling Auto-Regressive Integrated Atmosphere)',
+    url='https://github.com/tomachito/maria',
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.7',                   # Minimum version requirement of the package
+
+    install_requires=['numpy',
+                      'scipy',
+                      'ephem',
+                      'weathergen'],                     # Install other dependencies if any
+    
+    py_modules = ['maria','tools','objects','tests'], 
+    include_package_data=True,
+    package_data={'': ['maria/site_info.csv']},
+)
