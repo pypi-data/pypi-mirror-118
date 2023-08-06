a31961369067956bced7fa529f715533945540d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:01 +0000

    Add package tensorflow-similarity/0.13.11/tensorflow_similarity-0.13.11.tar.gz

---
+++ packages/tensorflow-similarity/tensorflow_similarity-0.13.11.tar.gz/tensorflow_similarity-0.13.11/tensorflow_similarity/losses/__init__.py
@@ -0,0 +1,8 @@
+"""
+Contrastive learning specialized losses.
+"""
+from .pn_loss import PNLoss  # noqa
+from .triplet_loss import TripletLoss  # noqa
+from .metric_loss import MetricLoss  # noqa
+from .circle_loss import CircleLoss  # noqa
+from .multisim_loss import MultiSimilarityLoss  # noqa
