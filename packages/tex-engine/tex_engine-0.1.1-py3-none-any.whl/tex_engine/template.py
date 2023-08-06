39912b1e0975d1a44140ca09ce34054994c99484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:34 +0000

    Add package tex-engine/0.1.1/tex_engine-0.1.1-py3-none-any.whl

---
+++ packages/tex-engine/tex_engine-0.1.1-py3-none-any.whl/tex_engine/template.py
@@ -0,0 +1,9 @@
+from tex_engine.compiler import Compiler
+
+class Template(object):
+    def __init__(self, contents):
+        self.contents = contents
+        self.root = Compiler(contents).compile()
+
+    def render(self, **kwargs):
+        return self.root.render(kwargs)
