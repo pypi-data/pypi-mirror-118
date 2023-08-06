219333eb9b8e08f72d8cf1edb2cf2b2940e470d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:58 +0000

    Add package tensorbay/1.11.0/tensorbay-1.11.0.tar.gz

---
+++ packages/tensorbay/tensorbay-1.11.0.tar.gz/tensorbay-1.11.0/tensorbay/cli/rm.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021 Graviti. Licensed under MIT License.
+#
+
+"""Implementation of gas rm."""
+
+import click
+
+from .tbrn import TBRN, TBRNType
+from .utility import ContextInfo, error, get_dataset_client, get_gas
+
+
+def _implement_rm(obj: ContextInfo, tbrn: str, is_recursive: bool) -> None:
+    gas = get_gas(*obj)
+    info = TBRN(tbrn=tbrn)
+    dataset_client = get_dataset_client(gas, info, is_fusion=False)
+
+    if info.type not in (TBRNType.SEGMENT, TBRNType.NORMAL_FILE):
+        error(f'"{tbrn}" is an invalid path to remove')
+
+    if not info.is_draft:
+        error(f'To remove the data, "{info}" must be in draft status, like "{info}#1"')
+
+    if info.type == TBRNType.SEGMENT:
+        if not is_recursive:
+            error("Please use -r option to remove the whole segment")
+
+        dataset_client.delete_segment(info.segment_name)
+    else:
+        segment = dataset_client.get_segment(info.segment_name)
+        segment.delete_data(info.remote_path)
+
+    click.echo(f"{tbrn} is deleted successfully")
