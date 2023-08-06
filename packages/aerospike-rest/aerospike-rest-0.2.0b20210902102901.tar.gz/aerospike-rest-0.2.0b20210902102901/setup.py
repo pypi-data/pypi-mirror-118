35dfd7e8d2d857b8f75a1e92caea578fe7624237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:21 +0000

    Add package aerospike-rest/0.2.0b20210902102901/aerospike-rest-0.2.0b20210902102901.tar.gz

---
+++ packages/aerospike-rest/aerospike-rest-0.2.0b20210902102901.tar.gz/aerospike-rest-0.2.0b20210902102901/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+from datetime import datetime
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+import aerospike_rest
+
+setup(
+    name = aerospike_rest.NAME,
+    version = '{}{}{}'.format(aerospike_rest.get_version(), 'b', datetime.now().strftime("%Y%m%d%H%M%S")),
+    description = "Python interface to Aerospike REST Client",
+    url = "https://github.com/aerospike-community/aerospike-python-rest",
+    long_description = read("README.md"),
+    long_description_content_type='text/markdown',
+    packages = find_packages(),
+    python_requires=">=3.3",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = [
+        'requests'
+    ],
+)
