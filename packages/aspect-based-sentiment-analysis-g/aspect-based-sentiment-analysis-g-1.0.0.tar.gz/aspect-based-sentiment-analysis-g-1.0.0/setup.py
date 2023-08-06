997537d869e50a08f69470e9513db87e0e119939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:02 +0000

    Add package aspect-based-sentiment-analysis-g/1.0.0/aspect-based-sentiment-analysis-g-1.0.0.tar.gz

---
+++ packages/aspect-based-sentiment-analysis-g/aspect-based-sentiment-analysis-g-1.0.0.tar.gz/aspect-based-sentiment-analysis-g-1.0.0/setup.py
@@ -0,0 +1,37 @@
+import pathlib
+from setuptools import setup
+from setuptools import find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The README text
+README = (HERE / "README.md").read_text()
+description = 'Aspect Based Sentiment Analysis: ' \
+              'Transformer & Interpretability (TensorFlow)'
+
+setup(
+    name='aspect-based-sentiment-analysis-g',
+    version="1.0.0",  # Semantic: MAJOR, MINOR, and PATCH
+    url='https://github.com/ozkanonur/Aspect-Based-Sentiment-Analysis',
+    description=description,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="Apache-2.0",
+    author=' Rafal Rolczynski',
+    author_email='rafal.rolczynski@gmail.com',
+    include_package_data=False,
+    packages=find_packages(),
+    install_requires=[
+        'tensorflow==2.5',
+        'transformers==4.8',
+        'pytest',
+        'scikit-learn',
+        'ipython',
+        'google-cloud-storage',
+        'testfixtures',
+        'optuna',
+        'spacy'
+    ],
+    python_requires='>=3.7.*',
+)
