c221b7880db6bf21d6e8b05885c605bbf66912e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:41 +0000

    Add package audio-video-tools/0.16.1/audio_video_tools-0.16.1-py3-none-any.whl

---
+++ packages/audio-video-tools/audio_video_tools-0.16.1-py3-none-any.whl/mediatools/slideshow.py
@@ -0,0 +1,50 @@
+#!python3
+#
+# media-tools
+# Copyright (C) 2019-2021 Olivier Korach
+# mailto:olivier.korach AT gmail DOT com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+import sys
+import mediatools.log as log
+import mediatools.utilities as util
+import mediatools.videofile as video
+import mediatools.media_config as conf
+
+def main():
+    files = []
+    log.set_logger('video-slideshow')
+    resolution = conf.get_property('video.default.resolution')
+    sys.argv.pop(0)
+    while sys.argv:
+        arg = sys.argv.pop(0)
+        if arg == "-g":
+            util.set_debug_level(sys.argv.pop(0))
+        elif arg == "--resolution":
+            resolution = sys.argv.pop(0)
+        else:
+            files.append(arg)
+    if len(files) > 0:
+        output = video.slideshow(*files, resolution=resolution)
+        log.logger.info("File %s generated", output)
+        print("File {} generated".format(output))
+    else:
+        log.logger.error("No inputs files could be used for slideshow, no slideshow generated")
+
+
+if __name__ == "__main__":
+    main()
