5fa1f610f727074237f105e60636e57a94151ac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:40 +0000

    Add package rest.connector/21.8/rest.connector-21.8-py3-none-any.whl

---
+++ packages/rest.connector/rest.connector-21.8-py3-none-any.whl/rest/connector/utils.py
@@ -0,0 +1,43 @@
+""" Utilities shared by all plugin libraries. """
+
+try:
+    from pyats.utils.secret_strings import to_plaintext
+except ImportError:
+    def to_plaintext(string):
+        return(str(string))
+
+def get_username_password(connection):
+    username = password = None
+    if connection.connection_info.get('credentials'):
+        try:
+            username = str(
+                connection.connection_info['credentials']['rest']['username'])
+            password = to_plaintext(
+                connection.connection_info['credentials']['rest']['password'])
+        except Exception:
+            pass
+
+    if not username:
+        username = connection.connection_info.get('username', \
+            connection.device.tacacs.username \
+            if connection.device.tacacs.username \
+            else 'admin')
+
+    if not password:
+        password = connection.connection_info.get('password', \
+            connection.device.passwords.tacacs \
+            if connection.device.passwords.tacacs \
+            else 'admin')
+
+    return (username, password)
+
+def get_token(connection):
+    token = None
+    if connection.connection_info.get('credentials'):
+        try:
+            token = str(
+                connection.connection_info['credentials']['rest']['token'])
+        except Exception:
+            pass
+
+    return token
