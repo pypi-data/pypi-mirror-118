c9028f02b7cc79d9703e36c6ebcefe44e2f9deb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:52 +0000

    Add package tableconv/1.9632.20210903/tableconv-1.9632.20210903-py3-none-any.whl

---
+++ packages/tableconv/tableconv-1.9632.20210903-py3-none-any.whl/tableconv/__version__.py
@@ -0,0 +1,3 @@
+VERSION = (1, 9632, 20210903)
+
+__version__ = '.'.join(map(str, VERSION))
---
+++ packages/tableconv/tableconv-1.9632.20210903-py3-none-any.whl/tableconv/adapters/df/__init__.py
@@ -0,0 +1,15 @@
+from .base import write_adapters, read_adapters, adapters
+
+from .aws_dynamodb import *
+from .aws_athena import *
+from .json import *
+from .yaml import *
+from .python import *
+from .ascii import *
+from .text_array import *
+from .pandas_io import *
+from .gsheets import *
+from .jira import *
+from .sql import *
+from .sumo_logic import *
+from .smart_sheet import *
