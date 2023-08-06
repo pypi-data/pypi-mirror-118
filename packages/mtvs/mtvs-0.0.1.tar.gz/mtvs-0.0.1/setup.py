c51e5a9f8f69c2fefbfecd73e318abb788962b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:50 +0000

    Add package mtvs/0.0.1/mtvs-0.0.1.tar.gz

---
+++ packages/mtvs/mtvs-0.0.1.tar.gz/mtvs-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from setuptools import find_packages
+
+NAME = "mtvs"
+AUTHOR = "HaveTwoBrush"
+EMAIL = "kinggreenhall@163.com"
+URL = "https://github.com/HaveTwoBrush/mtvs"
+LICENSE = "MIT License"
+DESCRIPTION = "MeTaVerSe"
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        version="0.0.1",
+        author=AUTHOR,
+        author_email=EMAIL,
+        url=URL,
+        license=LICENSE,
+        description=DESCRIPTION,
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=open("./requirements.txt", "r").read().splitlines(),
+        long_description=open("./README.md", "r").read(),
+        long_description_content_type='text/markdown',
+        zip_safe=True,
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            f"License :: OSI Approved :: {LICENSE}",
+            "Operating System :: OS Independent",
+        ],
+        python_requires=">=3.6"
+    )
