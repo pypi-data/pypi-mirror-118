ed883f0f38890d5610927e0afd38ef7e79543cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:53 +0000

    Add package beamer-slider/0.1.4/beamer-slider-0.1.4.tar.gz

---
+++ packages/beamer-slider/beamer-slider-0.1.4.tar.gz/beamer-slider-0.1.4/scripts/slider.py
@@ -0,0 +1,10 @@
+#!python
+# This is needed to get this to compile
+import clize
+from slider.slide import set_svg_background_images
+
+# Make two functions:
+if __name__ == "__main__":
+    clize.run(set_svg_background_images)
+    # print( fun(3) )
+
