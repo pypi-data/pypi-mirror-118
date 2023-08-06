38f038e48884b2fa6a5214b10d5f0ab6c26b9535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:11 +0000

    Add package orangepipes/0.1.3/orangepipes-0.1.3.tar.gz

---
+++ packages/orangepipes/orangepipes-0.1.3.tar.gz/orangepipes-0.1.3/orangepipes/__init__.py
@@ -0,0 +1,10 @@
+# from argparse import ArgumentParser
+
+# parser = ArgumentParser()
+# parser.add_argument("-p", "--pipeline", dest="pipeline",
+#                     help="enter the pipeline name")
+
+# args = vars(parser.parse_args())
+
+# if (args['pipeline'] == None):
+# 	raise RuntimeError('you must pass the pipeline name')
