43b0db6862ae4be3b3edde83ccca2c43328b4946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:36 +0000

    Add package xontrib-autojump/1.4/xontrib-autojump-1.4.tar.gz

---
+++ packages/xontrib-autojump/xontrib-autojump-1.4.tar.gz/xontrib-autojump-1.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+long_description = open('README.md').read()
+
+setup(
+    name='xontrib-autojump',
+    version='1.4',
+    url='https://github.com/willsALMANJ/autojump-xonsh',
+    license='GPLv3+',
+    author='Will Shanks',
+    author_email='wshaos@posteo.net',
+    description='autojump support for xonsh',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['xontrib'],
+    package_dir={'xontrib': 'xontrib'},
+    package_data={'xontrib': ['*.py']},
+    platforms='any',
+)
