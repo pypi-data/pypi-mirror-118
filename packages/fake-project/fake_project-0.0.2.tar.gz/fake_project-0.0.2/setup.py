0582e1294c97f702020622f82387d4314987b0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:17 +0000

    Add package fake-project/0.0.2/fake_project-0.0.2.tar.gz

---
+++ packages/fake-project/fake_project-0.0.2.tar.gz/fake_project-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='fake_project',
+    version='0.0.2',
+    description='',
+    long_description='',
+    url='https://www.example.com',
+    author='me',
+    author_email='me@example.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
