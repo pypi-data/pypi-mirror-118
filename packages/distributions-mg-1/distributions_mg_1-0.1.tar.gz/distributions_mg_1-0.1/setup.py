1a54225a3a2b255f2900e9b667aadc04cdc76390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:47 +0000

    Add package distributions-mg-1/0.1/distributions_mg_1-0.1.tar.gz

---
+++ packages/distributions-mg-1/distributions_mg_1-0.1.tar.gz/distributions_mg_1-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_mg_1',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_mg_1'],
+      author='Matthew Gray',
+      zip_safe=False)
