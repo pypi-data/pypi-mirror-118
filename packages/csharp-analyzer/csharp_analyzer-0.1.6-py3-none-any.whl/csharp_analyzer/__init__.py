94ab357cab16b110bd654e5bdb8cd4024d8447cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:43 +0000

    Add package csharp-analyzer/0.1.6/csharp_analyzer-0.1.6-py3-none-any.whl

---
+++ packages/csharp-analyzer/csharp_analyzer-0.1.6-py3-none-any.whl/csharp_analyzer/__init__.py
@@ -0,0 +1,6 @@
+from csharp_analyzer.attribute import Attribute
+from csharp_analyzer.class_ import Class
+from csharp_analyzer.method import Method
+from csharp_analyzer.namespace import Namespace
+from csharp_analyzer.property import Property
+
