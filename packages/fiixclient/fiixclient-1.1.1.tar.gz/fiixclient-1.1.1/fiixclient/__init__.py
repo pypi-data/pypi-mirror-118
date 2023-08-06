35368a48414e4d3d90ad01669f23a49055b9c7ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:14 +0000

    Add package fiixclient/1.1.1/fiixclient-1.1.1.tar.gz

---
+++ packages/fiixclient/fiixclient-1.1.1.tar.gz/fiixclient-1.1.1/fiixclient/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+Fiix CMMS API Client
+~~~~~~~~~~~~~~~
+A Python wrapper for the Fiix CMMS API
+:copyright: (c) 2021 by Ronald Langeved.
+:license: MIT, see LICENSE for details.
+"""
+
+__title__ = "python fiix client"
+__version__ = "1.1"
+__author__ = "Ronald Langeveld"
+__license__ = "MIT"
+
+from fiixclient.client import FiixClient
