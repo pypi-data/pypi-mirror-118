66e9d01d9f8a88ac2ea5febfbdc164f2a43c4a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:16 +0000

    Add package ura-gcn/0.0.1/ura_gcn-0.0.1.tar.gz

---
+++ packages/ura-gcn/ura_gcn-0.0.1.tar.gz/ura_gcn-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ura_gcn',
+    version='0.0.1',
+    packages=[],
+    license='',
+    description='A library for Graph Convolutional Networks',
+    long_description='A library for Graph Convolutional Networks',
+    install_requires=[],
+    classifiers=['Development Status :: 1 - Planning']
+)
