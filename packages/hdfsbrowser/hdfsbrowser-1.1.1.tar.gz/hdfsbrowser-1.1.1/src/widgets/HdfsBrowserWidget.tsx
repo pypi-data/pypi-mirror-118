b2e9f2ff9f05f5cd9488c4e6b332bfc205eb1387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:37 +0000

    Add package hdfsbrowser/1.1.1/hdfsbrowser-1.1.1.tar.gz

---
+++ packages/hdfsbrowser/hdfsbrowser-1.1.1.tar.gz/hdfsbrowser-1.1.1/src/widgets/HdfsBrowserWidget.tsx
@@ -0,0 +1,22 @@
+import { IFrame } from '@jupyterlab/apputils';
+import { ServerConnection } from '@jupyterlab/services';
+
+/**
+ * A class that exposes the git plugin Widget.
+ */
+export class HdfsBrowserWidget extends IFrame {
+
+    /**
+     * Construct a console panel.
+     */
+    constructor() {
+        super();
+
+        this.sandbox = [
+            'allow-same-origin',
+            'allow-scripts'
+        ];
+        
+        this.url = ServerConnection.makeSettings().baseUrl + "hdfsbrowser/explorer.html";
+    }
+}
