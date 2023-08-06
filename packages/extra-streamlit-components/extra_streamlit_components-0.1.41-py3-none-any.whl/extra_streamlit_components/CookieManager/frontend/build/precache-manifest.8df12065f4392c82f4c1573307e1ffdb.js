5051c01eff3484c0f79bfcd3e88d5c5a4fe4fe84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:07 +0000

    Add package extra-streamlit-components/0.1.41/extra_streamlit_components-0.1.41-py3-none-any.whl

---
+++ packages/extra-streamlit-components/extra_streamlit_components-0.1.41-py3-none-any.whl/extra_streamlit_components/CookieManager/frontend/build/precache-manifest.8df12065f4392c82f4c1573307e1ffdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02737f6361b79d9639a21f5b98f8ac2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b7ca0a971749981247f8",
+    "url": "./static/js/2.273650fd.chunk.js"
+  },
+  {
+    "revision": "ebcbc126ac2f4e1d923c109be3a13ab8",
+    "url": "./static/js/2.273650fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ce6685f7d113a8f6e63",
+    "url": "./static/js/main.3caa9b96.chunk.js"
+  },
+  {
+    "revision": "3739a9830da056fd5075",
+    "url": "./static/js/runtime-main.e1d294c3.js"
+  }
+]);
