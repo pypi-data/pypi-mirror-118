5264766a031711dd5e71ba84b2c6f00ed4e152af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:14 +0000

    Add package BionomialSJPackage/0.1/BionomialSJPackage-0.1.tar.gz

---
+++ packages/BionomialSJPackage/BionomialSJPackage-0.1.tar.gz/BionomialSJPackage-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='BionomialSJPackage',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['BionomialSJPackage'],
+      zip_safe=False)
