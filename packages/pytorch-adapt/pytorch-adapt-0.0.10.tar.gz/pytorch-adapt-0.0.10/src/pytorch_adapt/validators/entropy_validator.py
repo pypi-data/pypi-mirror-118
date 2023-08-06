1f6dc21a23afb023e387ee453219c0dfe626fc36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:49 +0000

    Add package pytorch-adapt/0.0.10/pytorch-adapt-0.0.10.tar.gz

---
+++ packages/pytorch-adapt/pytorch-adapt-0.0.10.tar.gz/pytorch-adapt-0.0.10/src/pytorch_adapt/validators/entropy_validator.py
@@ -0,0 +1,7 @@
+from ..layers import EntropyLoss
+from .base_validator import BaseValidator
+
+
+class EntropyValidator(BaseValidator):
+    def compute_score(self, target_train):
+        return -EntropyLoss()(target_train["logits"]).item()
