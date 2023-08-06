00b84b7b532b7bc2465528f3a24c6f01be69a0a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:57 +0000

    Add package quant-risk/1.1.1/quant_risk-1.1.1-py3-none-any.whl

---
+++ packages/quant-risk/quant_risk-1.1.1-py3-none-any.whl/quant_risk/models/__init__.py
@@ -0,0 +1,11 @@
+""" This file deals with modelling functions related to statistics such as:
+    1. Auto-ARIMA model
+    2. Regression models such as Ridge/Lasso/OLS
+"""
+
+from quant_risk.models import *
+
+__all__ = [
+    'regression',
+    'time_series',
+]
