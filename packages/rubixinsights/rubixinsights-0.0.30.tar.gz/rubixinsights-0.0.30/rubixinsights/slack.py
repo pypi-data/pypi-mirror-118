91849e8108cd10c962b5648518d5bece82344c45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:32 +0000

    Add package rubixinsights/0.0.30/rubixinsights-0.0.30.tar.gz

---
+++ packages/rubixinsights/rubixinsights-0.0.30.tar.gz/rubixinsights-0.0.30/rubixinsights/slack.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+
+def report_failure_to_slack_wrapper(slack_webhook_url: str):
+    """Callback to send slack message if pipeline fails
+
+    :param slack_webhook_url: slack webhook url
+    :type slack_webhook_url: str
+    """
+    def report_failure_to_slack(context):
+        channel = context['dag'].tags[0]
+        execution_date = context['execution_date']
+        execution_date = f"{execution_date.year}-{execution_date.month}-{execution_date.day}"
+        message = f"[Pipeline Error] [{channel}] [{execution_date}]"
+        payload = {'text': message}
+        headers = {'Content-type': 'application/json'}
+        requests.post(slack_webhook_url, data=json.dumps(payload), headers=headers)
+    return report_failure_to_slack
