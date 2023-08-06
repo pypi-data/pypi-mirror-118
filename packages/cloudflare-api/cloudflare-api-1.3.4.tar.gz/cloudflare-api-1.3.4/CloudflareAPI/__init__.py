8d1f5dfa2d24fc978c098fa0978f20d8edd4c13d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:18 +0000

    Add package cloudflare-api/1.3.4/cloudflare-api-1.3.4.tar.gz

---
+++ packages/cloudflare-api/cloudflare-api-1.3.4.tar.gz/cloudflare-api-1.3.4/CloudflareAPI/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from __future__ import absolute_import
+from CloudflareAPI.cloudflare import Cloudflare
+from CloudflareAPI.utils import jsonPrint, Fetch
+
+
+__ALL__ = ["Cloudflare", "jsonPrint", "Fetch"]
