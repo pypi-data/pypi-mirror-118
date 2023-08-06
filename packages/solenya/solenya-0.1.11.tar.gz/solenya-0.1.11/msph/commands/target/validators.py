e078afa2618924ed385419f3ad217fea73eb8b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:46 +0000

    Add package solenya/0.1.11/solenya-0.1.11.tar.gz

---
+++ packages/solenya/solenya-0.1.11.tar.gz/solenya-0.1.11/msph/commands/target/validators.py
@@ -0,0 +1,34 @@
+import argparse
+import re
+
+from msph.app import Validator, current_app
+
+from ... import settings
+
+class CliValidator(Validator):
+
+    def validate(self):
+        if settings.delete_target:
+            if not settings.target_names:
+                current_app.active_command.parser.error('Must specify {target_names} with [-d] flag.')
+        if settings.reset_target:
+            if settings.target_names and settings.all_targets:
+                current_app.active_command.parser.error('Can no specify {target_names} with [-a] flag.')
+        if settings.all_targets and not settings.reset_target:
+            current_app.active_command.parser.error("[-a] flag can only be set with [-r] flag.")
+        if settings.reset_hard:
+            if not settings.delete_target and not settings.reset_target:
+                current_app.active_command.parser.error("[--hard] flag can only be set with [-d, -r] flags.")
+        if settings.delete_target or not settings.target_name:
+            if settings.verbose:
+                current_app.active_command.parser.error("[-v] flag can only be set when creating or reseting targets.")
+        return True
+        
+def target_names(name):
+    name = str(name)
+    pattern = re.compile("^[a-z0-9_]*$")
+    if len(name) > 10 or len(name) < 3 or not pattern.match(name):
+        raise argparse.ArgumentTypeError(
+            "target name must be between 3-10 characters long "
+            "and can only contain lowercase letters, numbers and underscores.")
+    return name
