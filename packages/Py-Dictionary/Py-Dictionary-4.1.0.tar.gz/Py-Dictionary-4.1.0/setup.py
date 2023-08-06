758bdcbcfdeba9dcd92f63251aebc1b44694f3ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:32 +0000

    Add package Py-Dictionary/4.1.0/Py-Dictionary-4.1.0.tar.gz

---
+++ packages/Py-Dictionary/Py-Dictionary-4.1.0.tar.gz/Py-Dictionary-4.1.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Py-Dictionary",
+    version="4.1.0",
+    author="nit-in",
+    author_email="nit_in@live.com",
+    description="Dictionary module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nit-in/py-dictionary",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "string-color",
+        "lxml",
+    ],
+)
