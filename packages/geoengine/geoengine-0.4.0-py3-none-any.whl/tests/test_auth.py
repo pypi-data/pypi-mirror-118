99f14cd747f8c3ce10151a4bcff12b6f41292538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:15 +0000

    Add package geoengine/0.4.0/geoengine-0.4.0-py3-none-any.whl

---
+++ packages/geoengine/geoengine-0.4.0-py3-none-any.whl/tests/test_auth.py
@@ -0,0 +1,50 @@
+'''Tests regarding Geo Engine authentication'''
+
+from datetime import datetime
+
+import unittest
+import requests_mock
+
+import geoengine as ge
+from geoengine.types import QueryRectangle
+
+
+class AuthTests(unittest.TestCase):
+    '''Tests runner regarding Geo Engine authentication'''
+
+    def setUp(self) -> None:
+        ge.reset()
+
+    def test_uninitialized(self):
+        with self.assertRaises(ge.UninitializedException) as exception:
+            ge.workflow_by_id("foobar").get_dataframe(
+                QueryRectangle(
+                    [-180, -90, 180, 90],
+                    [datetime.now(), datetime.now()]
+                )
+            )
+
+        self.assertEqual(str(exception.exception),
+                         'You have to call `initialize` before using other functionality')
+
+    def test_initialize(self):
+        with requests_mock.Mocker() as m:
+            m.post('http://mock-instance/anonymous', json={
+                "id": "e327d9c3-a4f3-4bd7-a5e1-30b26cae8064",
+                "user": {
+                    "id": "328ca8d1-15d7-4f59-a989-5d5d72c98744",
+                },
+                "created": "2021-06-08T15:22:22.605891994Z",
+                "validUntil": "2021-06-08T16:22:22.605892183Z",
+                "project": None,
+                "view": None
+            })
+
+            ge.initialize("http://mock-instance")
+
+            self.assertEqual(type(ge.get_session()),
+                             ge.Session)
+
+
+if __name__ == '__main__':
+    unittest.main()
