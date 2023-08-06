934a7c013f56bbe4d24858bdd9b2c5ba1395ff92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:44 +0000

    Add package pkgdistributions/0.1/pkgdistributions-0.1.tar.gz

---
+++ packages/pkgdistributions/pkgdistributions-0.1.tar.gz/pkgdistributions-0.1/README.md
@@ -0,0 +1,3 @@
+# pkgdistributions package
+
+Library for distributions like Binomial and Gaussian
---
+++ packages/pkgdistributions/pkgdistributions-0.1.tar.gz/pkgdistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='pkgdistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['pkgdistributions'],
+      zip_safe=False)
