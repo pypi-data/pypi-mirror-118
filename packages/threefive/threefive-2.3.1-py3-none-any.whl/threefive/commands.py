1804fef92560679a4ce09d2514694c8b4b9d54c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:50 +0000

    Add package threefive/2.3.1/threefive-2.3.1-py3-none-any.whl

---
+++ packages/threefive/threefive-2.3.1-py3-none-any.whl/threefive/commands.py
@@ -0,0 +1,255 @@
+"""
+SCTE35 Splice Commands
+"""
+from .bitn import BitBin
+from .base import SCTE35Base
+
+
+class SpliceCommand(SCTE35Base):
+    """
+    Base class, not used directly.
+    """
+
+    def __init__(self, bites=None):
+        self.calculated_length = None
+        self.name = None
+        self.bites = bites
+
+    def decode(self):
+        """
+        default decode method
+        """
+
+    def _set_len(self, start, end):
+        """
+        _set_len sets
+        self.calculated_length
+        """
+        self.calculated_length = (start - end) >> 3
+
+
+class BandwidthReservation(SpliceCommand):
+    """
+    Table 11 - bandwidth_reservation()
+    """
+
+    def decode(self):
+        """
+        BandwidthReservation.decode method
+        """
+        self.name = "Bandwidth Reservation"
+
+
+class PrivateCommand(SpliceCommand):
+    """
+    Table 12 - private_command
+    """
+
+    def __init__(self, bites=None):
+        self.identifier = None
+        super().__init__(bites)
+
+    def decode(self):
+        """
+        PrivateCommand.decode method
+        """
+        self.name = "Private Command"
+        self.identifier = int.from_bytes(
+            self.bites[0:3], byteorder="big"
+        )  # 3 bytes = 24 bits
+        self.bites = self.bites[3:]
+
+
+class SpliceNull(SpliceCommand):
+    """
+    Table 7 - splice_null()
+    """
+
+    def decode(self):
+        """
+        SpliceNull.decode method
+        """
+        self.calculated_length = 0
+        self.name = "Splice Null"
+
+
+class TimeSignal(SpliceCommand):
+    """
+    Table 10 - time_signal()
+    """
+
+    def __init__(self, bites=None):
+        super().__init__(bites)
+        self.time_specified_flag = None
+        self.pts_time = None
+
+    def decode(self):
+        """
+        TimeSignal.decode method
+        """
+        self.name = "Time Signal"
+        bitbin = BitBin(self.bites)
+        start = bitbin.idx
+        self._splice_time(bitbin)
+        self._set_len(start, bitbin.idx)
+
+    def _splice_time(self, bitbin):
+        """
+        parse_pts is called by either a
+        TimeSignal or SpliceInsert instance
+        to decode pts.
+        """
+        self.time_specified_flag = bitbin.as_flag(1)
+        if self.time_specified_flag:
+            bitbin.forward(6)
+            self.pts_time = bitbin.as_90k(33)
+        else:
+            bitbin.forward(7)
+
+
+class SpliceInsert(TimeSignal):
+    """
+    Table 9 - splice_insert()
+    """
+
+    def __init__(self, bites=None):
+        super().__init__(bites)
+        self.name = "Splice Insert"
+        self.break_auto_return = None
+        self.break_duration = None
+        self.splice_event_id = None
+        self.splice_event_cancel_indicator = None
+        self.out_of_network_indicator = None
+        self.program_splice_flag = None
+        self.duration_flag = None
+        self.splice_immediate_flag = None
+        self.components = None
+        self.unique_program_id = None
+        self.avail_num = None
+        self.avail_expected = None
+
+    def _decode_break(self, bitbin):
+        if self.duration_flag:
+            self.break_auto_return = bitbin.as_flag(1)
+            bitbin.forward(6)
+            self.break_duration = bitbin.as_90k(33)
+
+    def _decode_event(self, bitbin):
+        self.splice_event_id = bitbin.as_int(32)
+        self.splice_event_cancel_indicator = bitbin.as_flag(1)
+        bitbin.forward(7)
+
+    def _decode_flags(self, bitbin):
+        self.out_of_network_indicator = bitbin.as_flag(1)
+        self.program_splice_flag = bitbin.as_flag(1)
+        self.duration_flag = bitbin.as_flag(1)
+
+    def _decode_components(self, bitbin):
+        component_count = bitbin.as_int(8)
+        self.components = []
+        for i in range(0, component_count):
+            self.components[i] = bitbin.as_int(8)
+
+    def _decode_unique_avail(self, bitbin):
+        self.unique_program_id = bitbin.as_int(16)
+        self.avail_num = bitbin.as_int(8)
+        self.avail_expected = bitbin.as_int(8)
+
+    def decode(self):
+        """
+        SpliceInsert.decode
+        """
+        bitbin = BitBin(self.bites)
+        start = bitbin.idx
+        self._decode_event(bitbin)
+        if not self.splice_event_cancel_indicator:
+            self._decode_flags(bitbin)
+            self.splice_immediate_flag = bitbin.as_flag(1)
+            bitbin.forward(4)
+            if self.program_splice_flag:
+                if not self.splice_immediate_flag:
+                    self._splice_time(bitbin)
+            else:
+                self._decode_components(bitbin)
+                if not self.splice_immediate_flag:
+                    self._splice_time(bitbin)
+            self._decode_break(bitbin)
+            self._decode_unique_avail(bitbin)
+        self._set_len(start, bitbin.idx)
+
+
+class SpliceSchedule(SpliceCommand):
+    """
+    Table 8 - splice_schedule()
+    """
+
+    class SpliceEvent(SpliceInsert):
+        """
+        SpliceEvent is a class declared in
+        the SpliceSchedule class.
+        """
+
+        def __init__(self):
+            """
+            SpliceEvent
+            """
+            super().__init__(None)
+            self.name = None
+            self.utc_splice_time = None
+
+        def _decode_components(self, bitbin):
+            """
+            SpliceEvent._decode_components
+            """
+            component_count = bitbin.as_int(8)
+            self.components = []
+            for j in range(0, component_count):
+                self.components[j] = {
+                    "component_tag": bitbin.as_int(8),
+                    "utc_splice_time": bitbin.as_int(32),
+                }
+
+        def decode(self, bitbin):
+            """
+            SpliceEvent.decode
+            """
+            self._decode_event(bitbin)
+            if not self.splice_event_cancel_indicator:
+                self._decode_flags(bitbin)
+                bitbin.forward(5)
+                if self.program_splice_flag:
+                    self.utc_splice_time = bitbin.as_int(32)
+                else:
+                    self._decode_components(bitbin)
+                self._decode_break(bitbin)
+                self._decode_unique_avail(bitbin)
+
+    def __init__(self, bites=None):
+        """
+        SpliceSchedule.__init__
+        """
+        self.splices = []
+        super().__init__(bites)
+
+    def decode(self):
+        """
+        SpliceSchedule.decode
+        """
+        self.name = "Splice Schedule"
+        bitbin = BitBin(self.bites)
+        start = bitbin.idx
+        splice_count = bitbin.as_int(8)
+        for i in range(0, splice_count):
+            self.splices[i] = self.SpliceEvent()
+            self.splices[i].decode(bitbin)
+        self._set_len(start, bitbin.idx)
+
+
+command_map = {
+    0: SpliceNull,
+    4: SpliceSchedule,
+    5: SpliceInsert,
+    6: TimeSignal,
+    7: BandwidthReservation,
+    255: PrivateCommand,
+}
---
+++ packages/threefive/threefive-2.3.1-py3-none-any.whl/threefive/upid.py
@@ -0,0 +1,114 @@
+"""
+threefive/upid.py
+
+threefve.upid exposes one function,
+upid_decoder for use by the
+SegmentationDescriptor class.
+
+"""
+
+
+def upid_decoder(bitbin, upid_type, upid_length):
+    """
+    upid_decoder
+    decodes segmentation_upids by type,
+    from a bitbin instance.
+
+    Used by the SegmentationDescriptor class.
+    """
+    upid_map = {
+        0x01: ["Deprecated", _decode_uri],
+        0x02: ["Deprecated", _decode_uri],
+        0x03: ["AdID", _decode_uri],
+        0x04: ["UMID", _decode_umid],
+        0x05: ["ISAN", _decode_isan],
+        0x06: ["ISAN", _decode_isan],
+        0x07: ["TID", _decode_uri],
+        0x08: ["AiringID", _decode_air_id],
+        0x09: ["ADI", _decode_uri],
+        0x0A: ["EIDR", _decode_eidr],
+        0x0B: ["ATSC", _decode_atsc],
+        0x0C: ["MPU", _decode_mpu],
+        0x0D: ["MID", _decode_mid],
+        0x0E: ["ADS Info", _decode_uri],
+        0x0F: ["URI", _decode_uri],
+        0x10: ["UUID", _decode_uri],
+        0xFD: ["Unknown", _decode_uri],
+    }
+    if upid_type not in upid_map.keys():
+        upid_type = 0xFD
+    return upid_map[upid_type][0], upid_map[upid_type][1](bitbin, upid_length)
+
+
+def _decode_air_id(bitbin, upid_length):
+    return bitbin.as_hex(upid_length << 3)
+
+
+def _decode_atsc(bitbin, upid_length):
+    return {
+        "TSID": bitbin.as_int(16),
+        "reserved": bitbin.as_int(2),
+        "end_of_day": bitbin.as_int(5),
+        "unique_for": bitbin.as_int(9),
+        "content_id": bitbin.as_ascii(((upid_length - 4) << 3)),
+    }
+
+
+def _decode_eidr(bitbin, upid_length):
+    if upid_length < 12:
+        raise Exception(f"upid_length is {upid_length} should be 12 bytes")
+    pre = bitbin.as_int(16)
+    post = []
+    bit_count = 80
+    while bit_count:
+        bit_count -= 16
+        post.append(bitbin.as_hex(16)[2:])
+    return f"10.{pre}/{'-'.join(post)}"
+
+
+def _decode_isan(bitbin, upid_length):
+    return bitbin.as_hex(upid_length << 3)
+
+
+def _decode_mid(bitbin, upid_length):
+    upids = []
+    ulb = upid_length << 3
+    while ulb:
+        upid_type = bitbin.as_int(8)  # 1 byte
+        ulb -= 8
+        upid_length = bitbin.as_int(8)
+        ulb -= 8
+        upid_type_name, segmentation_upid = upid_decoder(bitbin, upid_type, upid_length)
+        mid_upid = {
+            "upid_type": hex(upid_type),
+            "upid_type_name": upid_type_name,
+            "upid_length": upid_length,
+            "segmentation_upid": segmentation_upid,
+        }
+        ulb -= upid_length << 3
+        upids.append(mid_upid)
+    return upids
+
+
+def _decode_mpu(bitbin, upid_length):
+    ulbits = upid_length << 3
+    mpu_data = {
+        "format_identifier": bitbin.as_int(32),
+        "private_data": bitbin.as_raw(ulbits - 32),
+    }
+    return mpu_data
+
+
+def _decode_umid(bitbin, upid_length):
+    chunks = []
+    ulb = upid_length << 3
+    while ulb:
+        chunks.append(bitbin.as_hex(32).split("x", 1)[1])
+        ulb -= 32
+    return ".".join(chunks)
+
+
+def _decode_uri(bitbin, upid_length):
+    if upid_length > 0:
+        return bitbin.as_ascii(upid_length << 3)
+    return 0
