eed93a623e81adc7f62c64b3d7b67b3fd25c405e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:00 +0000

    Add package take-text-preprocess/0.0.2/take_text_preprocess-0.0.2-py3-none-any.whl

---
+++ packages/take-text-preprocess/take_text_preprocess-0.0.2-py3-none-any.whl/take_text_preprocess/logic/basic_preprocessing/basic_callable.py
@@ -0,0 +1,17 @@
+from .lower_case import to_lower_case
+from .remove_non_ascii import remove_non_ascii_characters
+from .replace_abbreviations import replace_abbreviations
+from .abbreviations import ABBREVIATIONS
+
+
+def base_preprocessing(text_input: str) -> str:
+    """Apply basic preprocessing methods on text.
+
+    :param text_input: Input to be processed.
+    :type text_input: `str`
+    :return: Processed text
+    :rtype: `str`
+    """
+    sentence = to_lower_case(text_input)
+    sentence = replace_abbreviations(sentence, ABBREVIATIONS)
+    return remove_non_ascii_characters(sentence)
