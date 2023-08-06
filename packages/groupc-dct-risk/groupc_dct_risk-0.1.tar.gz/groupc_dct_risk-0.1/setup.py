7a5d7d777cc60a0771f02c72965f157a2423ed3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:27 +0000

    Add package groupc-dct-risk/0.1/groupc_dct_risk-0.1.tar.gz

---
+++ packages/groupc-dct-risk/groupc_dct_risk-0.1.tar.gz/groupc_dct_risk-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='groupc_dct_risk',
+      version='0.1',  # Development release
+      description='Python Packaging Starting Kit - Simple Structure to create your own package - for training purposes!',
+      url='https://github.com/artadini/groupc_dct_risk',
+      author='Group C',
+      author_email='artadini@student.ie.edu',
+      license='MIT',
+          packages=['groupc_dct_risk'],
+      zip_safe=False)
