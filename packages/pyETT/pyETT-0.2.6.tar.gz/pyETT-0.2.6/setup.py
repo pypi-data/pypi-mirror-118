3e042814a5f549ff3c8f19b5b4b03569b3d12cf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:04 +0000

    Add package pyETT/0.2.6/pyETT-0.2.6.tar.gz

---
+++ packages/pyETT/pyETT-0.2.6.tar.gz/pyETT-0.2.6/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://pyett.readthedocs.io/
+
+from setuptools import setup, find_packages
+
+
+with open("README.md") as fh:
+    readme = fh.read()
+
+with open("LICENSE.md") as f:
+    license = f.read()
+
+setup(
+    name="pyETT",
+    version="0.2.6",
+    description="Python library for Eleven VR Table Tennis data",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Tharsis T. P. Souza",
+    url="https://pyett.readthedocs.io/",
+    license=license,
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    install_requires=[
+       "requests==2.22.0",
+	"pandas==1.3.0",
+	"nbsphinx==0.8.6",
+	"aiohttp==3.6.2",
+	"lxml",
+	"pandas_stubs==1.1.0.12",
+	"nest_asyncio==1.5.1"
+    ],
+)
