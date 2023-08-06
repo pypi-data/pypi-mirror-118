1f5bc4fe90ae3258ed17261156593884daae9bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:08 +0000

    Add package python-copaco-connections/0.1.2/python-copaco-connections-0.1.2.tar.gz

---
+++ packages/python-copaco-connections/python-copaco-connections-0.1.2.tar.gz/python-copaco-connections-0.1.2/copaco/constants/mappings.py
@@ -0,0 +1,22 @@
+PRICELISTITEM_MAPPINGS = {
+    'article' : ['Artikel', 'Copaco artikelnummer'],
+    'vendorCode' : ['Fabrikantscode'],
+    'description' : ['Omschrijving'],
+    'price' : ['Prijs'],
+    'priceWithLevies' : ['Prijs_incl_heffingen'],
+    'stock' : ['Voorraad'],
+    'hierarchy' : ['produktindeling'],
+    'unspscCode' : ['UNSPSC_code'],
+    'EAN' : ['EAN_code'],
+    'statusCode' : ['status'],
+    'auvibel' : ['Auvibel'],
+    'reprobel' : ['Reprobel'],
+    'recupel' : ['Recupel'],
+    'bebat' : ['Bebat']
+}
+
+STOCK_MAPPINGS = {
+    'nextDelivery' : ['Copaco datum eerstvolgende ontvangst'],
+    'nextDeliveryAmount' : ['Copaco aantal eerstvolgende ontvangst'],
+    'inventoryStatusCode' : ['Copaco ATP kwalificaties']
+}
