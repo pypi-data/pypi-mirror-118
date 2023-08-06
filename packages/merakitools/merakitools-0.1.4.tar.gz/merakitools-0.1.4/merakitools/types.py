6c5bc14d081388069155d924e41f9866464fd62c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:00 +0000

    Add package merakitools/0.1.4/merakitools-0.1.4.tar.gz

---
+++ packages/merakitools/merakitools-0.1.4.tar.gz/merakitools-0.1.4/merakitools/types.py
@@ -0,0 +1,60 @@
+"""
+merakitools - networks.py
+Billy Zoellers
+
+CLI tools for managing Meraki networks based on Typer
+"""
+from enum import Enum
+
+class ProductType(str, Enum):
+  """
+  Types of Meraki products
+  """
+  appliance = "appliance"
+  switch = "switch"
+  wireless = "wireless"
+  camera = "camera"
+  systemsManager = "systemsManager"
+  enviornmental = "enviornmental"
+  sensor = "sensor"
+  cellularGateway = "cellularGateway"
+
+class DeviceModel(str, Enum):
+  """
+  Models of Meraki devices
+  """
+  MX = "MX"
+  MR = "MR"
+  MS = "MS"
+  MV = "MV"
+  MT = "MT"
+  Z = "Z"
+
+class DeviceSortOptions(str, Enum):
+  """
+  Options to sort a list of devices
+  """
+  name = "name"
+  model = "model"
+
+class MXInternetUplinks(str, Enum):
+  """
+  Uplink types on an MX device
+  """
+  one = "internet1"
+  two = "internet2"
+
+class TrafficDirection(str, Enum):
+  """
+  Directions traffic can flow from an interface
+  """
+  total = "total"
+  recv = "recv"
+  sent = "sent"
+
+class MSInterfaceMode(str, Enum):
+  """
+  Interface modes on an MS switch
+  """
+  access = "access"
+  trunk = "trunk"
