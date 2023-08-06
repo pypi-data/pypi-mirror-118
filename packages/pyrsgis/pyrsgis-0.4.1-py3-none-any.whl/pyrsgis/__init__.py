b9faff49717f9cdd8358fdc64ca6078c7410ade4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:31 +0000

    Add package pyrsgis/0.4.1/pyrsgis-0.4.1-py3-none-any.whl

---
+++ packages/pyrsgis/pyrsgis-0.4.1-py3-none-any.whl/pyrsgis/__init__.py
@@ -0,0 +1,12 @@
+"""
+pyrsgis - Python for Remote Sensing and GIS
+author: pratkrt<at>gmail.com
+Compatible with Python versions 3+
+"""
+
+name = 'pyrsgis'
+__version__ = "0.4.1"
+doc_address = r'https://pyrsgis.readthedocs.io/en/master/'
+
+#Importing all functions from beta
+from .beta import *
