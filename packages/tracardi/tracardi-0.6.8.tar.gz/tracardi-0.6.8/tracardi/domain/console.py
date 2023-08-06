a19d542677fd3b62c6cbeae7c6a454857fed803e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:19 +0000

    Add package tracardi/0.6.8/tracardi-0.6.8.tar.gz

---
+++ packages/tracardi/tracardi-0.6.8.tar.gz/tracardi-0.6.8/tracardi/domain/console.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from typing import Any
+from pydantic import BaseModel
+
+from tracardi.domain.value_object.storage_info import StorageInfo
+from tracardi.service.storage.collection_crud import CollectionCrud
+from tracardi.service.storage.crud import StorageCrud
+
+
+class Metadata(BaseModel):
+    timestamp: datetime = None
+
+    def __init__(self, **data: Any):
+        super().__init__(**data)
+        self.timestamp = datetime.utcnow()
+
+
+class Console(BaseModel):
+    metadata: Metadata = Metadata()
+    event_id: str
+    flow_id: str
+    origin: str
+    class_name: str
+    module: str
+    type: str
+    message: str
+
+    # Persistence
+
+    def storage(self) -> StorageCrud:
+        return StorageCrud("console-log", Console, entity=self)
+
+    @staticmethod
+    def storage_info() -> StorageInfo:
+        return StorageInfo(
+            'console-log',
+            Console
+        )
+
+
+class ConsoleLog(list):
+
+    def bulk(self) -> CollectionCrud:
+        return CollectionCrud("console-log", self)
