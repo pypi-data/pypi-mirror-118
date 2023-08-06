c4cff3c39de52b8084b9bdf184ea18380f9e11c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:34 +0000

    Add package PyChance/0.1.11/PyChance-0.1.11-py3-none-any.whl

---
+++ packages/PyChance/PyChance-0.1.11-py3-none-any.whl/pychance/data/simpletable_class.py
@@ -0,0 +1,27 @@
+import random
+
+
+class SimpleTable:
+    def __init__(self, table_name, table_values=None, temporary=False):
+        self.table_name = table_name
+        self.temporary = temporary
+
+        if table_values is not None:
+            if isinstance(table_values, list):
+                self.table_values = table_values
+            else:
+                self.table_values = []
+        else:
+            self.table_values = []
+
+    def add_value(self, table_value):
+        self.table_values.append(table_value)
+
+    def get_value(self):
+        random.shuffle(self.table_values)
+        return self.table_values[0]
+
+
+if __name__ == '__main__':
+    test = SimpleTable("foo", ["bar", "baz"])
+    print(test.get_value())
