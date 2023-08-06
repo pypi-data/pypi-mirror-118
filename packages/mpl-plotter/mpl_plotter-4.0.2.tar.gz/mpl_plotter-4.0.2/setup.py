98b8430419e30b55c0ec114d10c8eacc142d5518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:20 +0000

    Add package mpl-plotter/4.0.2/mpl_plotter-4.0.2.tar.gz

---
+++ packages/mpl-plotter/mpl_plotter-4.0.2.tar.gz/mpl_plotter-4.0.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mpl_plotter",
+    version="4.0.2",
+    author="Antonio Lopez Rivera",
+    author_email="antonlopezr99@gmail.com",
+    description="Matplotlib-based plotting library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/antonlopezr/mpl_plotter",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "scikit-image",
+        "termcolor",
+        "pandas",
+        "PyQt5",
+        "Python-Alexandria",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
