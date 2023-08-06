fdd915dc2d9dc55b3ecdba8c28d6132d740113aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:58 +0000

    Add package PyHelium/0.1.1/PyHelium-0.1.1.tar.gz

---
+++ packages/PyHelium/PyHelium-0.1.1.tar.gz/PyHelium-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+   name='PyHelium',
+   version='0.1.1',
+   author='Aimé Risson',
+   author_email='aime.risson.1@gmail.com',
+   packages=['pyHelium', 'pyHelium.blockchain'],
+
+   url='http://pypi.python.org/pypi/PackageName/',
+   license='Mit License',
+   description="A simple python package to access Helium network's APi",
+   long_description=open('README.md').read(),
+   long_description_content_type = "text/markdown",
+   install_requires=[
+       "requests",
+   ],
+)
