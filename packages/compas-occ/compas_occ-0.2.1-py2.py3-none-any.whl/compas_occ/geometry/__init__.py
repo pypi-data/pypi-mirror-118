d1c4ed6181126946019186bb6b2a44382757fb5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:00 +0000

    Add package compas-occ/0.2.1/compas_occ-0.2.1-py2.py3-none-any.whl

---
+++ packages/compas-occ/compas_occ-0.2.1-py2.py3-none-any.whl/compas_occ/geometry/__init__.py
@@ -0,0 +1,32 @@
+"""
+********************************************************************************
+compas_occ.geometry
+********************************************************************************
+
+.. currentmodule:: compas_occ.geometry
+
+Curves
+======
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    Curve
+    NurbsCurve
+
+Surfaces
+========
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    Surface
+    NurbsSurface
+
+"""
+from .curves import Curve  # noqa: F401
+from .curves import NurbsCurve  # noqa: F401
+from .surfaces import Surface  # noqa: F401
+from .surfaces import NurbsSurface  # noqa: F401
