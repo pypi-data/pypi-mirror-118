54e67d4523e777737651c6bb117696bea7c068e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:54 +0000

    Add package DataLeopard/2.1/DataLeopard-2.1.tar.gz

---
+++ packages/DataLeopard/DataLeopard-2.1.tar.gz/DataLeopard-2.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'DataLeopard',
+    version = '2.1',
+    url = 'https://github.com/probablyJustJordan/DataLeopard.git',
+    description = 'Scripts to work with the DataLeopart Inventory System',
+    author = 'Jordan Shierstone',
+    author_email = 'jobr4644@colorado.edu',
+    license = 'for Private Use only',
+    packages = find_packages(),
+    include_package_data = True
+)
