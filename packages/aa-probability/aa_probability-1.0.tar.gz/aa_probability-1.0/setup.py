b735ef712de6f4956761fcf1cca351ca5db9ed71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:02 +0000

    Add package aa-probability/1.0/aa_probability-1.0.tar.gz

---
+++ packages/aa-probability/aa_probability-1.0.tar.gz/aa_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='aa_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['aa_probability'],
+      author='bigdateros',
+      author_email='aalvarez@bigdateros.com',
+      zip_safe=False)
