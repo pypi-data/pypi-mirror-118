83ca554b737f7977369ac2a5198557bff7a368df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:24 +0000

    Add package deep-sea-treasure/1.1.0/deep_sea_treasure-1.1.0-py3-none-any.whl

---
+++ packages/deep-sea-treasure/deep_sea_treasure-1.1.0-py3-none-any.whl/deep_sea_treasure/__version__.py
@@ -0,0 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+VERSION: str = "1.1.0"
+COMMIT_HASH: str = "1fa2c8fddf483fb77da30a6f416b48bdcfea4f69"
