7d2075515188defa77d64b4cdcd8cb17bf45c5a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:32 +0000

    Add package telegramlog/0.0.4/telegramlog-0.0.4.tar.gz

---
+++ packages/telegramlog/telegramlog-0.0.4.tar.gz/telegramlog-0.0.4/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='telegramlog',
+    version='0.0.4',
+    author='dean',
+    author_email='deanzhou56@gmail.com',
+    url='https://github.com/deanzhou69/telegram_log',
+    description=u'基于flylog的telegram日志发送',
+    packages=find_packages(),
+    # urllib3 应该要依赖
+    install_requires=['supervisor', 'flylog'],
+)
+
