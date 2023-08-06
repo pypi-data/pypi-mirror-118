10dd1bc296a9579e7b1da08645ddeb4d801bdd8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:39 +0000

    Add package sweelimiter/0.1.12/sweelimiter-0.1.12.tar.gz

---
+++ packages/sweelimiter/sweelimiter-0.1.12.tar.gz/sweelimiter-0.1.12/sweelimiter/api/decorator.py
@@ -0,0 +1,34 @@
+from functools import wraps
+from typing import List
+
+from fastapi import HTTPException
+from starlette.requests import Request
+
+from .error_handler import LimiterErrorHandler
+from .limiter_engine import LimiterEngine
+from .limit_config import LimitConfig, Limit
+
+
+def create_ratelimit_decorator(engine: LimiterEngine, error_handler: LimiterErrorHandler):
+    def ratelimit(key: str, *args: Limit):
+        config = LimitConfig(key, args)
+
+        def decorator(func):
+            @wraps(func)
+            async def wrapper(request: Request, *args, **kwargs):
+                limit_result = await engine.limit(config, request)
+                # print(limit_result)
+
+                if limit_result.limit_applied:
+                    if error_handler:
+                        error_handler(limit_result)
+                    else:
+                        raise HTTPException(status_code=429, detail=limit_result.description)
+
+                await func(request, *args, **kwargs)
+
+            return wrapper
+
+        return decorator
+
+    return ratelimit
