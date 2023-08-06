3a100e68ba18b304a1800c4c005558c736e8f88c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:08 +0000

    Add package chopin/0.1.1/chopin-0.1.1-py3-none-any.whl

---
+++ packages/chopin/chopin-0.1.1-py3-none-any.whl/chopin/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+Chopin's Compositions
+~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2021-present ster
+:license: Mit, see LICENSE for more details.
+
+"""
+
+__title__ = "chopin"
+__author__ = "ster"
+__license__ = "MIT"
+__copyright__ = "Copyright 2021-present ster"
+__version__ = "0.1.1"
+
+from .chopin import *
+
+__all__ = (
+    "Chopin",
+)
