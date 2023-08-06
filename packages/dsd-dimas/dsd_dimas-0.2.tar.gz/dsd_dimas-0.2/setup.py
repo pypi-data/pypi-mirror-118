6965688ef7634dd75e885ffa210b67ffda26792a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:02 +0000

    Add package dsd-dimas/0.2/dsd_dimas-0.2.tar.gz

---
+++ packages/dsd-dimas/dsd_dimas-0.2.tar.gz/dsd_dimas-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsd_dimas',
+      version='0.2',
+      description='distributions',
+      packages=['dsd_dimas'],
+      author='Dimas Azevedo',
+      author_email='mrvsonic87@yahoo.com.br',
+      zip_safe=False)
