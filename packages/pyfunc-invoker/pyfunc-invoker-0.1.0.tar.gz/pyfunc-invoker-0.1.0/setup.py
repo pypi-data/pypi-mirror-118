8afeb4c6b93ed190ad2a996182a9c96150837948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:02 +0000

    Add package pyfunc-invoker/0.1.0/pyfunc-invoker-0.1.0.tar.gz

---
+++ packages/pyfunc-invoker/pyfunc-invoker-0.1.0.tar.gz/pyfunc-invoker-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='pyfunc-invoker',
+    packages=find_packages(include=['pyfunc']),
+    version='0.1.0',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='Apache',
+    install_requires=[
+        'cloudevents >=1.2, <2',
+        'Flask>=1.0.2,<2',
+        ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: Unix',
+    ],
+)
