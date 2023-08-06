c7ef9ad3e6c063ef8ed79ab3c3452942118a78b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:15 +0000

    Add package cgmodels/0.8.0/cgmodels-0.8.0-py3-none-any.whl

---
+++ packages/cgmodels/cgmodels-0.8.0-py3-none-any.whl/cgmodels/cg/constants.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class StrEnum(str, Enum):
+    def __str__(self) -> str:
+        return str.__str__(self)
+
+
+class Pipeline(StrEnum):
+    BALSAMIC: str = "balsamic"
+    FASTQ: str = "fastq"
+    FLUFFY: str = "fluffy"
+    MICROSALT: str = "microsalt"
+    MIP_DNA: str = "mip-dna"
+    MIP_RNA: str = "mip-rna"
+    SARS_COV_2: str = "sars-cov-2"
+    DEMULTIPLEX: str = "demultiplex"
+    SPRING: str = "spring"
+    RSYNC: str = "rsync"
