0ac410bcd09221465ad374a5838f72136b6d06ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:43 +0000

    Add package sapien/1.1.1/sapien-1.1.1-cp37-cp37m-manylinux2014_x86_64.whl

---
+++ packages/sapien/sapien-1.1.1-cp37-cp37m-manylinux2014_x86_64.whl/sapien/core/__init__.pyi
@@ -0,0 +1,175 @@
+import sapien.core
+import typing
+from sapien.core.pysapien import Actor
+from sapien.core.pysapien import ActorBase
+from sapien.core.pysapien import ActorBuilder
+from sapien.core.pysapien import ActorDynamicBase
+from sapien.core.pysapien import ActorStatic
+from sapien.core.pysapien import Articulation
+from sapien.core.pysapien import ArticulationBase
+from sapien.core.pysapien import ArticulationBuilder
+from sapien.core.pysapien import ArticulationDrivable
+from sapien.core.pysapien import BoxGeometry
+from sapien.core.pysapien import CameraSpec
+from sapien.core.pysapien import CapsuleGeometry
+from sapien.core.pysapien import CollisionGeometry
+from sapien.core.pysapien import CollisionShape
+from sapien.core.pysapien import Constraint
+from sapien.core.pysapien import Contact
+from sapien.core.pysapien import ContactPoint
+from sapien.core.pysapien import ConvexMeshGeometry
+from sapien.core.pysapien import DirectionalLight
+from sapien.core.pysapien import DirectionalLightEntity
+from sapien.core.pysapien import Drive
+from sapien.core.pysapien import Engine
+from sapien.core.pysapien import Entity
+from sapien.core.pysapien import ICamera
+from sapien.core.pysapien import IPxrRenderer
+from sapien.core.pysapien import ISensor
+from sapien.core.pysapien import Input
+from sapien.core.pysapien import Joint
+from sapien.core.pysapien import JointBase
+from sapien.core.pysapien import JointRecord
+from sapien.core.pysapien import KinematicArticulation
+from sapien.core.pysapien import KinematicJoint
+from sapien.core.pysapien import KinematicJointFixed
+from sapien.core.pysapien import KinematicJointPrismatic
+from sapien.core.pysapien import KinematicJointRevolute
+from sapien.core.pysapien import KinematicJointSingleDof
+from sapien.core.pysapien import KinematicLink
+from sapien.core.pysapien import Light
+from sapien.core.pysapien import LightEntity
+from sapien.core.pysapien import Link
+from sapien.core.pysapien import LinkBase
+from sapien.core.pysapien import LinkBuilder
+from sapien.core.pysapien import NonconvexMeshGeometry
+from sapien.core.pysapien import OptifuserCamera
+from sapien.core.pysapien import OptifuserConfig
+from sapien.core.pysapien import OptifuserController
+from sapien.core.pysapien import OptifuserMaterial
+from sapien.core.pysapien import OptifuserRenderer
+from sapien.core.pysapien import PhysicalMaterial
+from sapien.core.pysapien import PinocchioModel
+from sapien.core.pysapien import PlaneGeometry
+from sapien.core.pysapien import PointLight
+from sapien.core.pysapien import PointLightEntity
+from sapien.core.pysapien import Pose
+from sapien.core.pysapien import RenderBody
+from sapien.core.pysapien import RenderGeometry
+from sapien.core.pysapien import RenderMaterial
+from sapien.core.pysapien import RenderScene
+from sapien.core.pysapien import RenderShape
+from sapien.core.pysapien import Scene
+from sapien.core.pysapien import SceneConfig
+from sapien.core.pysapien import ShapeRecord
+from sapien.core.pysapien import SphereGeometry
+from sapien.core.pysapien import SpotLight
+from sapien.core.pysapien import SpotLightEntity
+from sapien.core.pysapien import Subscription
+from sapien.core.pysapien import Trigger
+from sapien.core.pysapien import URDFLoader
+from sapien.core.pysapien import VisualRecord
+from sapien.core.pysapien import VulkanCamera
+from sapien.core.pysapien import VulkanDirectionalLight
+from sapien.core.pysapien import VulkanMaterial
+from sapien.core.pysapien import VulkanPointLight
+from sapien.core.pysapien import VulkanRenderer
+from sapien.core.pysapien import VulkanRigidbody
+from sapien.core.pysapien import VulkanScene
+from sapien.core.pysapien import VulkanSpotLight
+from sapien.core.pysapien import VulkanWindow
+import os
+import pkg_resources
+import sapien.core.pysapien.renderer
+import sys
+
+__all__ = [
+    "Actor",
+    "ActorBase",
+    "ActorBuilder",
+    "ActorDynamicBase",
+    "ActorStatic",
+    "Articulation",
+    "ArticulationBase",
+    "ArticulationBuilder",
+    "ArticulationDrivable",
+    "BoxGeometry",
+    "CameraSpec",
+    "CapsuleGeometry",
+    "CollisionGeometry",
+    "CollisionShape",
+    "Constraint",
+    "Contact",
+    "ContactPoint",
+    "ConvexMeshGeometry",
+    "DirectionalLight",
+    "DirectionalLightEntity",
+    "Drive",
+    "Engine",
+    "Entity",
+    "ICamera",
+    "IPxrRenderer",
+    "ISensor",
+    "Input",
+    "Joint",
+    "JointBase",
+    "JointRecord",
+    "KinematicArticulation",
+    "KinematicJoint",
+    "KinematicJointFixed",
+    "KinematicJointPrismatic",
+    "KinematicJointRevolute",
+    "KinematicJointSingleDof",
+    "KinematicLink",
+    "Light",
+    "LightEntity",
+    "Link",
+    "LinkBase",
+    "LinkBuilder",
+    "NonconvexMeshGeometry",
+    "OptifuserCamera",
+    "OptifuserConfig",
+    "OptifuserController",
+    "OptifuserMaterial",
+    "OptifuserRenderer",
+    "PhysicalMaterial",
+    "PinocchioModel",
+    "PlaneGeometry",
+    "PointLight",
+    "PointLightEntity",
+    "Pose",
+    "RenderBody",
+    "RenderGeometry",
+    "RenderMaterial",
+    "RenderScene",
+    "RenderShape",
+    "Scene",
+    "SceneConfig",
+    "ShapeRecord",
+    "SphereGeometry",
+    "SpotLight",
+    "SpotLightEntity",
+    "Subscription",
+    "Trigger",
+    "URDFLoader",
+    "VisualRecord",
+    "VulkanCamera",
+    "VulkanDirectionalLight",
+    "VulkanMaterial",
+    "VulkanPointLight",
+    "VulkanRenderer",
+    "VulkanRigidbody",
+    "VulkanScene",
+    "VulkanSpotLight",
+    "VulkanWindow",
+    "enable_default_gl3",
+    "enable_default_gl4",
+    "ensure_icd",
+    "os",
+    "pkg_resources",
+    "pysapien",
+    "renderer",
+    "sys"
+]
+
+
