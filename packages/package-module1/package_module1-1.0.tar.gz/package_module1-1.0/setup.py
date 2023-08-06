a5b613166a4d95fab13663bf877e8b44e968f072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:30 +0000

    Add package package-module1/1.0/package_module1-1.0.tar.gz

---
+++ packages/package-module1/package_module1-1.0.tar.gz/package_module1-1.0/setup.py
@@ -0,0 +1,15 @@
+# TODO: Fill out this file with information about your package
+
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
+
+from setuptools import setup
+
+setup(name = 'package_module1',
+      version = '1.0',
+      description = 'Math_functions',
+      author = 'Lesly Cadena',
+      author_mail = 'leslycadena@hotmail.com',
+   zip_safe = False)
