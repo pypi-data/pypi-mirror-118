295f71d836fb078ca0822f991adab39829c2efa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:03 +0000

    Add package addonenishka/0.1.5/addonenishka-0.1.5.tar.gz

---
+++ packages/addonenishka/addonenishka-0.1.5.tar.gz/addonenishka-0.1.5/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="addonenishka",
+    version="0.1.5",
+    author="Nishka Arora",
+    author_email="naarora@caltech.edu",
+    description="Add one example package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NishkaArora/packagingtutorial",
+    # project_urls={
+        # "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    # },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    package_data={'addonenishka':['data/*.csv']},
+    python_requires=">=3.6",
+    install_requires=['pandas', 'numpy'],
+    entry_points = {
+        'console_scripts': ['addone=examplepackagenishka.example:main']
+    }
+)
---
+++ packages/addonenishka/addonenishka-0.1.5.tar.gz/addonenishka-0.1.5/src/examplepackagenishka/example.py
@@ -0,0 +1,23 @@
+import argparse
+import pandas as pd
+import numpy as np
+
+class addone(object):
+
+    number = 0
+    def __init__(self, **kw):
+        self.number = kw['number']
+
+    def addtest(self):
+        print(self.number + 1)
+        print('Random numpy integer: ' + str(np.random.randint(4)))
+        sample_df = pd.DataFrame(pd.read_csv('src/examplepackagenishka/data/sample_data.csv'))
+        print(sample_df.to_markdown())
+
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-number', type=int, help='input number', default=0)
+    args = parser.parse_args()
+    
+    obj = addone(number=args.number)
+    obj.addtest()
