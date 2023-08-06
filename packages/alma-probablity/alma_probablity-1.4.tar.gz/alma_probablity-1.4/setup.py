3d82d25d1fd3d1af5e6dad4b0d34dce645dc893f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:48 +0000

    Add package alma-probablity/1.4/alma_probablity-1.4.tar.gz

---
+++ packages/alma-probablity/alma_probablity-1.4.tar.gz/alma_probablity-1.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='alma_probablity',
+      version='1.4',      description='Gaussian and Binomial distributions',
+      packages=['alma_probablity'],
+      author= 'Ali Balati',
+      author_email='ali.balati@yahoo.com',
+      zip_safe=False)
