8ead5287918998b5626c051c90aaf161ab6f393d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:42 +0000

    Add package ptrail/0.4.Beta/ptrail-0.4b0-py3-none-any.whl

---
+++ packages/ptrail/ptrail-0.4b0-py3-none-any.whl/ptrail/__init__.py
@@ -0,0 +1,16 @@
+from .core.TrajectoryDF import PTRAILDataFrame
+from .features import helper_functions
+from .features import semantic_features
+from .features import kinematic_features
+from .features import temporal_features
+from .preprocessing import filters
+from .preprocessing import helpers
+from .preprocessing import interpolation
+from .utilities import (
+    constants,
+    conversions,
+    DistanceCalculator,
+    exceptions,
+)
+
+__version__ = "0.4 Beta"
