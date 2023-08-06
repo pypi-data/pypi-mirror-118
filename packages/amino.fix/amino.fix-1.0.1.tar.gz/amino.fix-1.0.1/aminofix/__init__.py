8e03d6a0e69884cce6ff73449d4aa270349af8d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:12 +0000

    Add package amino.fix/1.0.1/amino.fix-1.0.1.tar.gz

---
+++ packages/amino.fix/amino.fix-1.0.1.tar.gz/amino.fix-1.0.1/aminofix/__init__.py
@@ -0,0 +1,17 @@
+__title__ = 'Amino.fix'
+__author__ = 'Minori'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020-2021 Minori'
+__version__ = '1.0.1'
+
+from .client import Client
+from .lib.util import device, exceptions, helpers, objects
+from .socket import Callbacks, SocketHandler
+from requests import get
+from json import loads
+
+__newest__ = loads(get("https://pypi.org/pypi/amino.fix/json").text)["info"]["version"]
+
+if __version__ != __newest__:
+    print(f"New version of {__title__} available: {__newest__} (Using {__version__})")
+    print(f"To update to the latest version, write to pip this command - pip install fixed.lib=={__newest__}")
