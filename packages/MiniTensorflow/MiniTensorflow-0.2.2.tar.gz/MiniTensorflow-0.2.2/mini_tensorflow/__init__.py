f051782b338b07f623db8cebd954d8f801dfc322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:57 +0000

    Add package MiniTensorflow/0.2.2/MiniTensorflow-0.2.2.tar.gz

---
+++ packages/MiniTensorflow/MiniTensorflow-0.2.2.tar.gz/MiniTensorflow-0.2.2/mini_tensorflow/__init__.py
@@ -0,0 +1,5 @@
+from mini_tensorflow.MiniTensorflow import Layer
+from mini_tensorflow.MiniTensorflow import Network
+__version__ = '0.2.2'
+__author__ = 'Aayushmaan Jain'
+__email__ = 'aayushmaan1306@gmail.com'
