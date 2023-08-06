8425e81dba6dc331b923af8e78c613f547770861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:26 +0000

    Add package vcspack100/0.1/vcspack100-0.1.tar.gz

---
+++ packages/vcspack100/vcspack100-0.1.tar.gz/vcspack100-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'vcspack100',           # pip install <name>
+  packages = ['vcspack100'],      # packages to be installed  
+  version = '0.1',              
+
+  description = 'vcspack utilities',   # text description
+  license='MIT',        # Chose a license from here: 
+                        # https://opensource.org/licenses
+
+  author = 'vinay',
+  author_email = 'vinaygn@gmail.com',
+)
