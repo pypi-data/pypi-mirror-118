8a64df200c2987d5a2645df3539131c68e71ed6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:43 +0000

    Add package QM-Spider/0.0.130/QM_Spider-0.0.130.tar.gz

---
+++ packages/QM-Spider/QM_Spider-0.0.130.tar.gz/QM_Spider-0.0.130/qm_spider/asm_appsa.py
@@ -0,0 +1,14 @@
+"""
+@FileName：asm_appsa.py\n
+@Description：\n
+@Author：道长\n
+@Time：2021/8/19 16:29\n
+@Department：运营部\n
+@Website：www.geekaso.com.com\n
+@Copyright：©2019-2021 七麦数据
+"""
+
+from qm_spider import *
+
+
+
