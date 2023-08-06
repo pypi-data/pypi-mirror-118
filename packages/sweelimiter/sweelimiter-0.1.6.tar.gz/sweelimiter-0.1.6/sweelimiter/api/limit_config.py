5a4ff3c157cf96b52d6f645010427627473dc33e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:33 +0000

    Add package sweelimiter/0.1.6/sweelimiter-0.1.6.tar.gz

---
+++ packages/sweelimiter/sweelimiter-0.1.6.tar.gz/sweelimiter-0.1.6/sweelimiter/api/limit_config.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Limit:
+
+    def __init__(
+            self,
+            interval: int,
+            max_requests: int
+    ):
+        self.interval = interval
+        self.max_requests = max_requests
+
+    def __repr__(self):
+        return f'Limit(interval={self.interval},max_requests={self.max_requests})'
+
+
+class LimitConfig:
+
+    def __init__(
+            self,
+            key: str,
+            limits: List[Limit],
+    ):
+        self.key = key
+        self.limits = limits
+
+    def __repr__(self):
+        return f'LimitConfig(key={self.key},limits={self.limits})'
