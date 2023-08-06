87d13eb8e35baae8bd2efc8e941562d00681b085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:20 +0000

    Add package fox-orm/0.3.2.1/fox-orm-0.3.2.1.tar.gz

---
+++ packages/fox-orm/fox-orm-0.3.2.1.tar.gz/fox-orm-0.3.2.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='fox-orm',
+    version='0.3.2.1',
+    author='vanutp',
+    author_email='hello@vanutp.dev',
+    description='Simple pydantic & databases based orm',
+    url='https://hub.vanutp.dev',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'databases~=0.4.3',
+        'pydantic~=1.8.2',
+        'SQLAlchemy~=1.3.24',
+    ],
+    python_requires='>=3.8',
+)
