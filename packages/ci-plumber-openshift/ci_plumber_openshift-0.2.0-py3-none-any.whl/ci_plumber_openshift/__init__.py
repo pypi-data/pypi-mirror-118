63df2f4639ec6031bf51646aa7cfc39cc8d6b9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:36 +0000

    Add package ci-plumber-openshift/0.2.0/ci_plumber_openshift-0.2.0-py3-none-any.whl

---
+++ packages/ci-plumber-openshift/ci_plumber_openshift-0.2.0-py3-none-any.whl/ci_plumber_openshift/__init__.py
@@ -0,0 +1,29 @@
+import typer
+
+from ci_plumber import Module_attribute
+from ci_plumber_openshift.openshift_wrapper import (
+    create_db,
+    list_projects,
+    openshift_deploy,
+)
+
+attributes = [Module_attribute.consumer]
+
+name = "openshift"
+
+app: typer.Typer = typer.Typer()
+
+app.command(name="deploy")(openshift_deploy)
+app.command(name="ls")(list_projects)
+app.command(name="create-db")(create_db)
+
+
+@app.callback()
+def callback() -> None:
+    """
+    Tools to manage deploying to Openshift
+    """
+
+
+if __name__ == "__main__":
+    app()
