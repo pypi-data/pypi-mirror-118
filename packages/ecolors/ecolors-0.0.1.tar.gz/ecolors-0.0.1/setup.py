121b5e9fcf519cecd777f83fe54848eb0e255912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:35 +0000

    Add package ecolors/0.0.1/ecolors-0.0.1.tar.gz

---
+++ packages/ecolors/ecolors-0.0.1.tar.gz/ecolors-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+import codecs
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Colors for the printing functions'
+
+# Setting up
+setup(
+    name="ecolors",
+    version=VERSION,
+    author="Electro Development",
+    author_email="contactus.electro@gmail.com",
+    description=DESCRIPTION,
+    keywords=['python', 'colors'],
+)
