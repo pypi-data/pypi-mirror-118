5829bc260aee1ec11e7a5d0fe626127b771e6854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:17 +0000

    Add package catalyst/21.8/catalyst-21.8-py2.py3-none-any.whl

---
+++ packages/catalyst/catalyst-21.8-py2.py3-none-any.whl/catalyst/data/__init__.py
@@ -0,0 +1,36 @@
+# flake8: noqa
+from catalyst.data.collate_fn import FilteringCollateFn
+from catalyst.data.dataset import (
+    DatasetFromSampler,
+    ListDataset,
+    MergeDataset,
+    NumpyDataset,
+    PathsDataset,
+    MetricLearningTrainDataset,
+    QueryGalleryDataset,
+)
+from catalyst.data.loader import (
+    ILoaderWrapper,
+    BatchLimitLoaderWrapper,
+    BatchPrefetchLoaderWrapper,
+)
+from catalyst.data.sampler import (
+    BalanceClassSampler,
+    BalanceBatchSampler,
+    BatchBalanceClassSampler,
+    DistributedSamplerWrapper,
+    DynamicLenBatchSampler,
+    DynamicBalanceClassSampler,
+    MiniEpochSampler,
+)
+from catalyst.data.sampler_inbatch import (
+    IInbatchTripletSampler,
+    InBatchTripletsSampler,
+    AllTripletsSampler,
+    HardTripletsSampler,
+    HardClusterSampler,
+)
+
+from catalyst.data.transforms import Compose, Normalize, ToTensor, to_tensor, normalize
+
+from catalyst.contrib.data import *
