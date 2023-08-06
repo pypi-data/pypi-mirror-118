be3881c098188b8350ed5748542b16b4aa561873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:54 +0000

    Add package cicdtest/0.47/cicdtest-0.47-py3-none-any.whl

---
+++ packages/cicdtest/cicdtest-0.47-py3-none-any.whl/buildpan/buildpan.py
@@ -0,0 +1,47 @@
+"""
+    Title: buildpan
+    Module Name: buildpan
+    Author: Akash Dwivedi
+    Language: Python
+    Date Created: 26-07-2021
+    Date Modified: 20-08-2021
+    Description:
+        ###############################################################
+        ##  Main operating file for all the cli commands             ## 
+         ###############################################################
+ """
+
+import git
+import os
+import click
+import git
+import sys
+from github import Github
+import subprocess
+
+from pyfiglet import Figlet
+
+'''
+This contains the code snippet for the cli header.
+'''
+f = Figlet(font='slant')
+print (f.renderText('Buildpan'))
+
+
+# cli_commands for the buildpan 
+
+from ci_commands import init
+from ci_commands import version
+
+@click.group(help="CLI tool to manage CI- CD of projects")
+def buildpan():
+    pass
+
+'''
+ This containes all the commands for the buildpan cli
+'''
+buildpan.add_command(init.init)
+buildpan.add_command(version.version)
+
+if __name__ == '__main__':
+    buildpan()
