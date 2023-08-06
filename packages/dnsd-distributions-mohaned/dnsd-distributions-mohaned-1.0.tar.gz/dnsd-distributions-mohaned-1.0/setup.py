4be18bfa18adcab244275b6348eb87703aeda108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:38 +0000

    Add package dnsd-distributions-mohaned/1.0/dnsd-distributions-mohaned-1.0.tar.gz

---
+++ packages/dnsd-distributions-mohaned/dnsd-distributions-mohaned-1.0.tar.gz/dnsd-distributions-mohaned-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dnsd-distributions-mohaned',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['dnsd-distributions-mohaned'],
+      author = 'Mohaned El-Hadidi',
+      author_email = 'mohanedeh@gmail.com',
+      zip_safe=False)
