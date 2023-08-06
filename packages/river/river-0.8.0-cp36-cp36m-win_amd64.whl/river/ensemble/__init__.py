0992540d71375d7fe07f0bce8aedc6c24894c762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:31 +0000

    Add package river/0.8.0/river-0.8.0-cp36-cp36m-win_amd64.whl

---
+++ packages/river/river-0.8.0-cp36-cp36m-win_amd64.whl/river/ensemble/__init__.py
@@ -0,0 +1,32 @@
+"""Ensemble learning.
+
+This module includes ensemble methods. This kind of methods improve predictive performance by
+combining the prediction of their members.
+
+"""
+from .adaptive_random_forest import (
+    AdaptiveRandomForestClassifier,
+    AdaptiveRandomForestRegressor,
+)
+from .bagging import (
+    ADWINBaggingClassifier,
+    BaggingClassifier,
+    BaggingRegressor,
+    LeveragingBaggingClassifier,
+)
+from .boosting import AdaBoostClassifier
+from .streaming_random_patches import SRPClassifier, SRPRegressor
+from .voting import VotingClassifier
+
+__all__ = [
+    "AdaptiveRandomForestClassifier",
+    "AdaptiveRandomForestRegressor",
+    "AdaBoostClassifier",
+    "ADWINBaggingClassifier",
+    "BaggingClassifier",
+    "BaggingRegressor",
+    "LeveragingBaggingClassifier",
+    "SRPClassifier",
+    "SRPRegressor",
+    "VotingClassifier",
+]
