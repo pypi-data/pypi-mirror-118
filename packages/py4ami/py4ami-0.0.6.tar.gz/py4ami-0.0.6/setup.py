77e8237f4e40ff5946e65fc6f8f1f0c041081594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:57 +0000

    Add package py4ami/0.0.6/py4ami-0.0.6.tar.gz

---
+++ packages/py4ami/py4ami-0.0.6.tar.gz/py4ami-0.0.6/setup.py
@@ -0,0 +1,40 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='py4ami',
+    url='https://github.com/petermr/pyami',
+    version='0.0.6',
+    description='Semantic Reader of the Scientific Literature.',
+    author="Peter Murray-Rust",
+    author_email='petermurrayrust@googlemail.com',
+    license='Apache2',
+    install_requires=[],
+    include_package_data=True,
+    zip_safe=False,
+    keywords='text and data mining',
+    packages=[
+        'py4ami'
+    ],
+    package_dir={'py4ami': 'py4ami'},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+    ],
+    entry_points={
+        'console_scripts': [
+            'py4ami=py4ami.pyamix:main',
+        ],
+    },
+)
