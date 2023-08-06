6bca276d15f00734cd5d9faa84c2e7f7f36f70b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:35 +0000

    Add package beamer-slider/0.1.5/beamer_slider-0.1.5-py3-none-any.whl

---
+++ packages/beamer-slider/beamer_slider-0.1.5-py3-none-any.whl/slider/__main__.py
@@ -0,0 +1,6 @@
+# from jinjafy import execute_command
+from slider.latexutils import latexmk
+from slider.slider_cli import clize_main_entry_point
+
+if __name__ == "__main__":
+    clize_main_entry_point()
