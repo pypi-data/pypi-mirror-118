d8a080f664d11760519c2887e9e551a66d846102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:46 +0000

    Add package meanCalculation/3.0/meanCalculation-3.0.tar.gz

---
+++ packages/meanCalculation/meanCalculation-3.0.tar.gz/meanCalculation-3.0/meanCalculation/MeanCalculations.py
@@ -0,0 +1,77 @@
+import math
+from .AvgCalculation import MeanCalc
+
+class CalcMean(MeanCalc):
+	""" Gaussian distribution class for calculating and visualizing a Gaussian distribution.
+	
+	Attributes:
+		mean (float) representing the mean value of the distribution
+		stdev (float) representing the standard deviation of the distribution
+		data_list (list of floats) a list of floats extracted from the data file
+			
+	"""
+	def __init__(self, mu=0, sigma=1):
+		
+		Distribution.__init__(self, mu, sigma)
+
+
+	def calculate_mean(self):
+		"""Function to calculate the mean of the data set.
+
+		Args:
+			None
+
+		Returns:
+			float: mean of the data set
+
+		"""
+
+		avg = 1.0 * sum(self.data) / len(self.data)
+
+		self.mean = avg
+
+		return self.mean
+
+	def calculate_stdev(self, sample=True):
+
+		"""Function to calculate the standard deviation of the data set.
+		
+		Args: 
+			sample (bool): whether the data represents a sample or population
+		
+		Returns: 
+			float: standard deviation of the data set
+	
+		"""
+
+		if sample:
+			n = len(self.data) - 1
+		else:
+			n = len(self.data)
+	
+		mean = self.calculate_mean()
+	
+		sigma = 0
+	
+		for d in self.data:
+			sigma += (d - mean) ** 2
+		
+		sigma = math.sqrt(sigma / n)
+	
+		self.stdev = sigma
+		
+		return self.stdev
+	
+	def __repr__(self):
+	
+		"""Function to output the characteristics of the Gaussian instance
+		
+		Args:
+			None
+		
+		Returns:
+			string: characteristics of the Gaussian
+		
+		"""
+		
+		return "mean {}, standard deviation {}".format(self.mean, self.stdev)
---
+++ packages/meanCalculation/meanCalculation-3.0.tar.gz/meanCalculation-3.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='meanCalculation',
+      version='3.0',
+      description='calculation of mean',
+      packages=['meanCalculation'],
+      author_email='varshakt6@gmail.com',
+      zip_safe=False)
