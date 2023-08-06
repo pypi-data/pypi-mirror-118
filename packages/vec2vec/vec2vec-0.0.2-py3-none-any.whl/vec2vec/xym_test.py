abb76a952a66beccc845b0c88f4e7fa1ec928953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:42 +0000

    Add package vec2vec/0.0.2/vec2vec-0.0.2-py3-none-any.whl

---
+++ packages/vec2vec/vec2vec-0.0.2-py3-none-any.whl/vec2vec/xym_test.py
@@ -0,0 +1,7 @@
+print('this is one')
+for num_walks in range(5, 55, 5):
+    print("************* The number of num_walks is : " + str(num_walks) + " *******************")
+
+if __name__=='__main__':
+    print('this is two')
+
