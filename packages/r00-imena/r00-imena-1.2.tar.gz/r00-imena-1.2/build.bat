ccd74a06be3c8efc9c8a0b3f46e992b274e71d33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:25 +0000

    Add package r00-imena/1.2/r00-imena-1.2.tar.gz

---
+++ packages/r00-imena/r00-imena-1.2.tar.gz/r00-imena-1.2/build.bat
@@ -0,0 +1,6 @@
+cd i:\projects\packages\imena-package\
+python setup.py sdist
+pip uninstall -y r00-imena
+REM Вручную выполнить
+REM twine upload -u r00ft1h -p %M9Y*x@CBiys2e8t --verbose dist/*
+REM pip install r00-imena
