301ddadb80a91e8e0bb26fbae14d6712f3c0227e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:57 +0000

    Add package pypj/0.3.1/pypj-0.3.1-py3-none-any.whl

---
+++ packages/pypj/pypj-0.3.1-py3-none-any.whl/pypj/exception.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+
+class Message(object):
+    pass
+
+
+class Emsg(Enum):
+    OS_NOT_SUPPORTED = "Pypj only supports unix system."
+    PIP_NOT_FOUND = "pip is not installed."
+    FAILED_GET_POETRY_VER = "Failed to get poetry version."
+    FAILED_POETRY_NEW = "Failed to 'poetry new'."
+    FAILED_POETRY_CONFIG = "Failed to configure poetry."
+    FAILED_POETRY_ADD = "Failed to add package via poetry."
+    RESOURCE_NOT_FOUND = "Resource does not exist. Please report this issue."
+    PACKAGE_NOT_FOUND = "The target package does not exist."
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class PypjError(Exception):
+    def __init__(self, message: Emsg):
+        self.message = message.value
+        super().__init__(self.message)
