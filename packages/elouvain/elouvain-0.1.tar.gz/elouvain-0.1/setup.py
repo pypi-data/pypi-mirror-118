25487dab5336df1141ca272a03a0776caf28fffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:42 +0000

    Add package elouvain/0.1/elouvain-0.1.tar.gz

---
+++ packages/elouvain/elouvain-0.1.tar.gz/elouvain-0.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name='elouvain',
+    packages=['elouvain'],
+    version='0.1',
+    license='MIT',
+    description='Ensemble wrapper of Louvain for stable and accurate communities',
+    author='Bojan Evkoski',
+    author_email='bojan.evkoski@ijs.si',
+    url='https://github.com/boevkoski/ensemble-louvain',
+    download_url='https://github.com/boevkoski/ensemble-louvain/archive/refs/tags/v_01.zip',
+    keywords=['Communities', 'Clustering', 'Networks'],
+    install_requires=[
+        'networkx',
+        'python-louvain',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',  # Define that your audience are developers
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
