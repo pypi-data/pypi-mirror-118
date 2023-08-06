6418edd3727a287fe97b1b92dbbfb5a1524fd489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:42 +0000

    Add package nemesis-scan/1.0.3/nemesis-scan-1.0.3.tar.gz

---
+++ packages/nemesis-scan/nemesis-scan-1.0.3.tar.gz/nemesis-scan-1.0.3/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+import pkg_resources
+
+pkg_resources.require('setuptools>=39.2')
+setuptools.setup(
+    version = "1.0.3",
+    url = "https://github.com/machinexa2/Nemesis",
+    download_url = 'https://github.com/machinexa2/Nemesis/archive/refs/tags/v1.2.tar.gz',
+)
