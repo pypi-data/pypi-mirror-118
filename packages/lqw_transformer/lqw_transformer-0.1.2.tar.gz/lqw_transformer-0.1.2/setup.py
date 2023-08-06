e9ff6777c1db0ae9c8909a87da564d8ab7098f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:42 +0000

    Add package lqw_transformer/0.1.2/lqw_transformer-0.1.2.tar.gz

---
+++ packages/lqw_transformer/lqw_transformer-0.1.2.tar.gz/lqw_transformer-0.1.2/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  1 22:14:07 2021
+
+@author: 林庆文
+"""
+
+import setuptools  
+
+setuptools.setup(
+    name="lqw_transformer",
+    version="0.1.2",
+    author="林庆文",
+    author_email="935499957@qq.com",
+    description="A  example transformer",
+    url="https://github.com/pass-lin/lqw_transformer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
