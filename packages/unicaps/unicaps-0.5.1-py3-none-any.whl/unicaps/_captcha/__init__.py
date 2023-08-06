b7f7c770db7643b50c4f2ea2fb193cc3ced74261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:29 +0000

    Add package unicaps/0.5.1/unicaps-0.5.1-py3-none-any.whl

---
+++ packages/unicaps/unicaps-0.5.1-py3-none-any.whl/unicaps/_captcha/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+
+""" CAPTCHAs """
+
+# pylint: disable=unused-import,import-error
+from .image import ImageCaptcha
+from .text import TextCaptcha
+from .recaptcha_v2 import RecaptchaV2
+from .recaptcha_v3 import RecaptchaV3
+from .hcaptcha import HCaptcha
+from .funcaptcha import FunCaptcha
+from .keycaptcha import KeyCaptcha
+from .geetest import GeeTest
+from .capy import Capy
+from .tiktok import TikTokCaptcha
+from .base import CaptchaType
+
+__all__ = ('ImageCaptcha', 'TextCaptcha', 'RecaptchaV2', 'RecaptchaV3', 'HCaptcha', 'FunCaptcha',
+           'KeyCaptcha', 'GeeTest', 'Capy', 'TikTokCaptcha', 'CaptchaType')
