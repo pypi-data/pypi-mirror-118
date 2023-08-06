9199136973ff19dc2c90ceaa5060c4a47a25e5a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:42 +0000

    Add package colabexts/0.11700000000000002/colabexts-0.11700000000000002.tar.gz

---
+++ packages/colabexts/colabexts-0.11700000000000002.tar.gz/colabexts-0.11700000000000002/colabexts/log.py
@@ -0,0 +1,13 @@
+import logging
+
+
+LOG_FMT="%(name)s - %(levelname)s - %(message)s"
+logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(format=LOG_FMT)
+#-----------------------------------------------------------------------------------
+def ldeb(*kwargs):    logging.debug   ( kwargs )
+def linf(*kwargs):    logging.info    ( kwargs )
+def lwar(*kwargs):    logging.warning ( kwargs )
+def lerr(*kwargs):    logging.error   ( kwargs )
+def lcri(*kwargs):    logging.critical( kwargs )
+
