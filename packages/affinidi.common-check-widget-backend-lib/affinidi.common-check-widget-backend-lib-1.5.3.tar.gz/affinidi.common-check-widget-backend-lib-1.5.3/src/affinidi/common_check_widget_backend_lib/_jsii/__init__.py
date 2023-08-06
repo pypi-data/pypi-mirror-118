34450fe8bb3e743805827cd45599b5b8514ebaa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:34 +0000

    Add package affinidi.common-check-widget-backend-lib/1.5.3/affinidi.common-check-widget-backend-lib-1.5.3.tar.gz

---
+++ packages/affinidi.common-check-widget-backend-lib/affinidi.common-check-widget-backend-lib-1.5.3.tar.gz/affinidi.common-check-widget-backend-lib-1.5.3/src/affinidi/common_check_widget_backend_lib/_jsii/__init__.py
@@ -0,0 +1,22 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@affinidi/common-check-widget-backend-lib",
+    "1.5.3",
+    __name__[0:-6],
+    "common-check-widget-backend-lib@1.5.3.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
