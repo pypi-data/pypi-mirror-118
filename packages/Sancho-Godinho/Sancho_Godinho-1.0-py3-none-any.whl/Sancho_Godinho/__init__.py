49efb812ad0eeb529bf4bb5ec9d1b1631652e7d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:34 +0000

    Add package Sancho-Godinho/1.0/Sancho_Godinho-1.0-py3-none-any.whl

---
+++ packages/Sancho-Godinho/Sancho_Godinho-1.0-py3-none-any.whl/Sancho_Godinho/__init__.py
@@ -0,0 +1,22 @@
+class __init__():
+    print('''
+I Am A Boy From India Who Likes To Code As My Hobby!
+I Develop Free & Open Source Apps!
+I Believe That Open Source Apps Can Help Beginners Learn Coding Fast!
+
+My Favourite Coding Languages Are: 
+1) Web (HTML, CSS & JS),
+2) Python 3,
+3) Scratch (block-based Coding),
+4) Batch (Windows CMD Programming),
+And Many More...
+
+At Last, I Would Like to Share My Links:
+1) Free & Open Source Projects on Github: https://github.com/sancho1952007
+2) My Youtube Channel: https://www.youtube.com/channel/UCA_3t4pPc9GRIFiP86kRD2Q
+3) One Of My Video For Short URL To My Channel: https://youtu.be/BAZkp7oKbEU
+4) See My Quick Code Bits on Sololearn: https://www.sololearn.com/profile/19684449/?ref=app
+5) See My Latest Mobile & PC Games on Scratch: https://scratch.mit.edu/users/scratch1952007/
+6) See My Latest Mobile/PC Apps & Games on Itch: https://sancho1952007.itch.io/
+7) Last But Not The Least, See My Official Website: https://sancho1952007.github.io/
+''')
---
+++ packages/Sancho-Godinho/Sancho_Godinho-1.0-py3-none-any.whl/Sancho_Godinho/__main__.py
@@ -0,0 +1,1 @@
+import Sancho_Godinho
