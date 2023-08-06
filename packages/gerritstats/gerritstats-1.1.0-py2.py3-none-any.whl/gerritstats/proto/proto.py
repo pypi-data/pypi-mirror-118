10cc8285b120cc9038d167e62bd206a5bdb89e5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:55 +0000

    Add package gerritstats/1.1.0/gerritstats-1.1.0-py2.py3-none-any.whl

---
+++ packages/gerritstats/gerritstats-1.1.0-py2.py3-none-any.whl/gerritstats/proto/proto.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+class Commit:
+    BRANCH = "branch"
+    COMMIT = "commit"
+    DELETIONS = "deletions"
+    INSERTIONS = "insertions"
+    LABELS = "labels"
+    MESSAGE = "message"
+    OWNER = "owner"
+    REPO = "repo"
+    SUBMITTED = "submitted"
+    UPDATED = "updated"
