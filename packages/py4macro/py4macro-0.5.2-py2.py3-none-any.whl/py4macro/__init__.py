46c72c92403720030976138c6f343049bda05249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:21 +0000

    Add package py4macro/0.5.2/py4macro-0.5.2-py2.py3-none-any.whl

---
+++ packages/py4macro/py4macro-0.5.2-py2.py3-none-any.whl/py4macro/__init__.py
@@ -0,0 +1,25 @@
+# __init__.py
+
+# https://github.com/Py4Macro/py4macro.git
+
+from .py4macro import *
+
+__all__ = ['data','trend','show']
+
+__author__ = 'Tetsu Haruyama'
+__version__ = '0.5.2'
+__copyright__ = 'Copyright (c) 2021 Tetsu Haruyama'
+
+__doc__ = """
+    「Pythonで学ぶマクロ経済学 (中級＋α)」のためのモジュール
+
+        * HPフィルター関数
+        * DataFrameを全て表示するshow関数
+        * データ・セット
+            * Penn World Tables 10.0
+            * IMF World Economic Outlook 2021
+            * Maddison Project Database 2020
+            * 日本の四半期データ（GDPなど）
+            * 日本の四半期データ（マネーストックなど）
+            * 177ヵ国のマネーストックなど
+            * 円/ドル為替レート"""
