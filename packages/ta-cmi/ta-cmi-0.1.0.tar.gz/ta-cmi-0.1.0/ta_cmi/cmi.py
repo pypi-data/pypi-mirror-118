7a34ea9373bc4a6962e43f136c0a3f59150c285c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:20 +0000

    Add package ta-cmi/0.1.0/ta-cmi-0.1.0.tar.gz

---
+++ packages/ta-cmi/ta-cmi-0.1.0.tar.gz/ta-cmi-0.1.0/ta_cmi/cmi.py
@@ -0,0 +1,31 @@
+from typing import List
+
+from .baseApi import BaseAPI
+from .device import Device, ReadOnlyClass
+
+
+class CMI(BaseAPI, metaclass=ReadOnlyClass):
+    """Main class to interact with CMI"""
+
+    def __init__(self, host: str, username: str, password: str):
+        """Initialize."""
+        super().__init__(username, password)
+        self.host = host
+        self.username = username
+        self.password = password
+
+    async def getDevices(self) -> List[Device]:
+        """List connected devices."""
+        url: str = f"{self.host}/INCLUDE/can_nodes.cgi?_=1"
+        data: str = await self._makeRequestNoJson(url)
+
+        nodeIDs: List[str] = data.split(";")
+
+        devices: List[Device] = []
+
+        for nID in nodeIDs:
+            if len(nID) == 0:
+                continue
+            devices.append(Device(nID, self.host, self.username, self.password))
+
+        return devices
