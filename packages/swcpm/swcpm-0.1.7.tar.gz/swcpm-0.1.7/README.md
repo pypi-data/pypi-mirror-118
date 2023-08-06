a73f91a8607e08993afbdc183205d3682dc07266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:19 +0000

    Add package swcpm/0.1.7/swcpm-0.1.7.tar.gz

---
+++ packages/swcpm/swcpm-0.1.7.tar.gz/swcpm-0.1.7/README.md
@@ -0,0 +1,16 @@
+# SWC_packageinstaller
+
+##### Easily installs packages from the Software City Projects Repository to your machine.
+
+### Commands
+`swcpm`
+* `info <package>` Gives into about a package
+* `install <package>` Installs a package
+* `run <package>` Runs executable packages
+* `update <package>` Update a package
+* `remove <package>` Removes a package
+* `wget <url>` Uses built-in installer like linux wget
+* `debug` Lists all command modules
+
+Standard installation directory:
+`/home/<user>/SWC/Packages` or `C:\Users\<user>\SWC\Packages`
---
+++ packages/swcpm/swcpm-0.1.7.tar.gz/swcpm-0.1.7/swcpm/__init__.py
@@ -0,0 +1,68 @@
+import os
+import json
+import click
+import tempfile
+import platform
+from pathlib import Path
+
+
+class LocationDatabase:
+    database_dir = os.path.join(str(Path.home()), "SWC", "database")
+    database_path = os.path.join(database_dir, "locations.json")
+
+    def __init__(self):
+        if not os.path.exists(self.database_path):
+            os.makedirs(self.database_dir, exist_ok=True)
+            with open(self.database_path, "w") as f:
+                f.write("{}")
+
+        with open(self.database_path, "r") as f:
+            self.locations = json.loads(f.read())
+
+    def __update(self):
+        with open(self.database_path, "w") as f:
+            f.write(json.dumps(self.locations))
+
+    def get_location(self, package):
+        try:
+            return self.locations[package]
+        except KeyError:
+            return None
+
+    def set_location(self, package, location):
+        self.locations[package] = location
+        self.__update()
+
+    def remove_location(self, package):
+        del self.locations[package]
+        self.__update()
+
+
+_param_verbose = False
+
+constant_projectURL = "https://projects.software-city.org/resources"
+constant_packageURL = "https://projects.software-city.org/resources/swc_packages"
+
+constant_tempPATH = tempfile.gettempdir()
+constant_defaultAppInstallPATH = "C:\\Program Files\\SWC" if platform.system() == "windows" else "/usr/swc"
+constant_defaultModInstallPATH = os.path.join(str(Path.home()), "SWC", "Packages")
+
+location_database = LocationDatabase()
+
+
+def get_option_verbose():
+    return _param_verbose
+
+
+def verbose_echo(*strings):
+    if _param_verbose:
+        for string in strings:
+            click.echo(string)
+
+
+@click.group()
+@click.option("-v", "--verbose", is_flag=True, help="Show more info while running.")
+def swc_pm(verbose):
+    global _param_verbose
+
+    _param_verbose = verbose
