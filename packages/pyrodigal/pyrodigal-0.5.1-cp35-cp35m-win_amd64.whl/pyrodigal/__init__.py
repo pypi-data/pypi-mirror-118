17ee85c53a8e464454c8a636a1eab6bb8d322c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:56 +0000

    Add package pyrodigal/0.5.1/pyrodigal-0.5.1-cp35-cp35m-win_amd64.whl

---
+++ packages/pyrodigal/pyrodigal-0.5.1-cp35-cp35m-win_amd64.whl/pyrodigal/__init__.py
@@ -0,0 +1,13 @@
+from collections.abc import Sequence as _Sequence
+
+from . import _pyrodigal
+from ._pyrodigal import Gene, Genes, Pyrodigal, TrainingInfo
+
+__all__ = ["Gene", "Genes", "Pyrodigal", "TrainingInfo"]
+__doc__ = _pyrodigal.__doc__
+
+__author__ = "Martin Larralde <martin.larralde@embl.de>"
+__license__ = "GPLv3"
+__version__ = "0.5.1"
+
+_Sequence.register(Genes)
