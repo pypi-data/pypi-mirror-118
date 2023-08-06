996114a545ad369203d46ca4acff6c6904085001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:49 +0000

    Add package pyiotown/0.0.5/pyiotown-0.0.5-py3-none-any.whl

---
+++ packages/pyiotown/pyiotown-0.0.5-py3-none-any.whl/pyiotown/get.py
@@ -0,0 +1,31 @@
+import requests
+
+def downloadAnnotations(url, token, classname):
+    ''' 
+    url : IoT.own Server Address
+    token : IoT.own API Token
+    classname : Image Class ex) car, person, airplain
+    '''
+    apiaddr = url + "/api/v1.0/nn/images?labels=" + classname
+    header = {'Accept':'application/json', 'token':token}
+    r = requests.get(apiaddr, headers=header)
+    if r.status_code == 200:
+        return r.json()
+    else:
+        print(r)
+        return None
+
+def downloadImage(url, token, imageID):
+    ''' 
+    url : IoT.own Server Address
+    token : IoT.own API Token
+    imageID : IoT.own imageID ( using annotation's 'id' not '_id' ) 
+    '''
+    apiaddr = url + "/nn/dataset/img/" + imageID
+    header = {'Accept':'application/json', 'token':token}
+    r = requests.get(apiaddr, headers=header)
+    if r.status_code == 200:
+        return r.content
+    else:
+        print(r)
+        return None
