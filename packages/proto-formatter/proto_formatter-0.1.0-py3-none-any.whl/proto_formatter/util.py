1b70d99d70bb60f02a8052ef5fc642f1764d3f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:17 +0000

    Add package proto-formatter/0.1.0/proto_formatter-0.1.0-py3-none-any.whl

---
+++ packages/proto-formatter/proto_formatter-0.1.0-py3-none-any.whl/proto_formatter/util.py
@@ -0,0 +1,10 @@
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
+
+
+def remove_suffix(text, suffix):
+    if text.endswith(suffix):
+        return text[:-len(suffix)]
+    return text
