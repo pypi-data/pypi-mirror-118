706aba9726e66ee1a154e553f10b37b271217d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:15 +0000

    Add package vcspack101/0.0.2/vcspack101-0.0.2.tar.gz

---
+++ packages/vcspack101/vcspack101-0.0.2.tar.gz/vcspack101-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'vcspack101',           # pip install <name>
+  packages = ['vcspack101'],      # packages to be installed  
+  version = '0.0.2',              
+
+  description = 'vcspack utilities',   # text description
+  license='MIT',        # Chose a license from here: 
+                        # https://opensource.org/licenses
+
+  author = 'vinay',
+  author_email = 'vinaygn@gmail.com',
+)
