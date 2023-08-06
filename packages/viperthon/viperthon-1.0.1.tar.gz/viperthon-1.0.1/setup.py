c27ee5ef6a1e66bcb5c0a5066089185771fac1e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:51 +0000

    Add package viperthon/1.0.1/viperthon-1.0.1.tar.gz

---
+++ packages/viperthon/viperthon-1.0.1.tar.gz/viperthon-1.0.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+import setuptools
+import pathlib
+
+
+
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md" ).read_text()
+setup(
+    name ='viperthon',
+    version ='1.0.1',
+    description ='Tool to perform basic operations on your Dataset',
+    author='Swati Pandey',
+    url = 'https://github.com/swatishayna/viperthon.git',
+    long_description =README,
+    long_description_content_type = 'text/markdown',
+    license = "MIT",
+    keyword =['read csv', 'basic operations on dataset','read categorical column'],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    packages = ["viperthon"],
+    include_packages_data =True,
+    install_requires = ['pandas'],
+    python_requires = '>=3.6',
+
+
+
+
+
+
+)
