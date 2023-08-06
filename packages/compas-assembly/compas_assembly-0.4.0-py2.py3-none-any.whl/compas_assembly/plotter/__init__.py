1fad3742ecabc86864abfdbcc7915179cd762cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:24 +0000

    Add package compas-assembly/0.4.0/compas_assembly-0.4.0-py2.py3-none-any.whl

---
+++ packages/compas-assembly/compas_assembly-0.4.0-py2.py3-none-any.whl/compas_assembly/plotter/__init__.py
@@ -0,0 +1,26 @@
+"""
+********************************************************************************
+compas_assembly.plotter
+********************************************************************************
+
+.. currentmodule:: compas_assembly.plotter
+
+This package defines various classes and functions for working with assemblies
+in Rhino.
+
+
+Classes
+=======
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    AssemblyPlotter
+
+"""
+from .plotter import AssemblyPlotter
+
+__all__ = [
+    'AssemblyPlotter'
+]
