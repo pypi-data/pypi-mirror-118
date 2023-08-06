f08888795552c9a96169b77e06fb27a545ef19e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:16 +0000

    Add package eigmod/0.0.11/eigmod-0.0.11.tar.gz

---
+++ packages/eigmod/eigmod-0.0.11.tar.gz/eigmod-0.0.11/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.11'
+DESCRIPTION = 'replenishment to pygame indluding: collision detection, textboxes, buttons, input_boxes, camera, graphs and gernal numpy/aa drawing assistance'
+
+# Setting up
+setup(
+    name="eigmod",
+    version=VERSION,
+    author="felix-95",
+    author_email="felix.schaefer6@gmx.de",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["pygame"],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3"
+    ]
+)
