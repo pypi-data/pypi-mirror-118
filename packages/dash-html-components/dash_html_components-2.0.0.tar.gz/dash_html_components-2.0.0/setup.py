a0d1547fd4b6275ad181cb13edc847908f43a87d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:55 +0000

    Add package dash-html-components/2.0.0/dash_html_components-2.0.0.tar.gz

---
+++ packages/dash-html-components/dash_html_components-2.0.0.tar.gz/dash_html_components-2.0.0/setup.py
@@ -0,0 +1,17 @@
+import io
+from setuptools import setup
+
+setup(
+    name="dash_html_components",
+    version="2.0.0",
+    author="Chris Parmer <chris@plotly.com>",
+    author_email="chris@plotly.com",
+    packages=["dash_html_components"],
+    url="https://github.com/plotly/dash-html-components",
+    include_package_data=True,
+    license="MIT",
+    description="Vanilla HTML components for Dash",
+    long_description=io.open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    install_requires=[],
+)
