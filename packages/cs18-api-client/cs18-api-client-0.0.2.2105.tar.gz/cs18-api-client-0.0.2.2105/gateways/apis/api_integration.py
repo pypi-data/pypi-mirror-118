678430f488236934ec2a3caf57d96640f2b0704e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:16 +0000

    Add package cs18-api-client/0.0.2.2105/cs18-api-client-0.0.2.2105.tar.gz

---
+++ packages/cs18-api-client/cs18-api-client-0.0.2.2105.tar.gz/cs18-api-client-0.0.2.2105/gateways/apis/api_integration.py
@@ -0,0 +1,6 @@
+﻿from gateways.apis.api_base_class import ApiBase
+
+
+class ApiIntegration(ApiBase):
+    def get_insided_token(self):
+        return self.build_route("integrations/insided/token")
