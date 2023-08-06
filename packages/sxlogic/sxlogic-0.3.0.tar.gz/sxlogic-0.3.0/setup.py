62818e26f28e0827b3cd1f98d7ce73ff58578d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:07 +0000

    Add package sxlogic/0.3.0/sxlogic-0.3.0.tar.gz

---
+++ packages/sxlogic/sxlogic-0.3.0.tar.gz/sxlogic-0.3.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sxlogic",
+    version="0.3.0",
+    author="StreamLogic, LLC",
+    description="StreamLogic Python utilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=["sxlogic"],
+    entry_points={
+        'console_scripts': [
+            'fpgacap = sxlogic.fpgacap:fpgacap',
+            'iconvert = sxlogic.iconvert:iconvert',
+            ]
+        },
+    install_requires=['pyserial','numpy','opencv-python']
+)
