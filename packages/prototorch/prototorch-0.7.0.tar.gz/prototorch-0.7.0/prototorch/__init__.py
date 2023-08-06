f6bd8f2d6fb0a54358f0d8d0e0481efc92ab852d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:04 +0000

    Add package prototorch/0.7.0/prototorch-0.7.0.tar.gz

---
+++ packages/prototorch/prototorch-0.7.0.tar.gz/prototorch-0.7.0/prototorch/__init__.py
@@ -0,0 +1,66 @@
+"""ProtoTorch package"""
+
+import pkgutil
+from typing import List
+
+import pkg_resources
+
+from . import (
+    datasets,
+    nn,
+    utils,
+)
+from .core import (
+    competitions,
+    components,
+    distances,
+    initializers,
+    losses,
+    pooling,
+    similarities,
+    transforms,
+)
+
+# Core Setup
+__version__ = "0.7.0"
+
+__all_core__ = [
+    "competitions",
+    "components",
+    "core",
+    "datasets",
+    "distances",
+    "initializers",
+    "losses",
+    "nn",
+    "pooling",
+    "similarities",
+    "transforms",
+    "utils",
+]
+
+# Plugin Loader
+__path__: List[str] = pkgutil.extend_path(__path__, __name__)
+
+
+def discover_plugins():
+    return {
+        entry_point.name: entry_point.load()
+        for entry_point in pkg_resources.iter_entry_points(
+            "prototorch.plugins")
+    }
+
+
+discovered_plugins = discover_plugins()
+locals().update(discovered_plugins)
+
+# Generate combines __version__ and __all__
+version_plugins = "\n".join([
+    "- " + name + ": v" + plugin.__version__
+    for name, plugin in discovered_plugins.items()
+])
+if version_plugins != "":
+    version_plugins = "\nPlugins: \n" + version_plugins
+
+version = "core: v" + __version__ + version_plugins
+__all__ = __all_core__ + list(discovered_plugins.keys())
