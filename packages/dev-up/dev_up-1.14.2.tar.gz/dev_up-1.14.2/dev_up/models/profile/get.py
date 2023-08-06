36934f376877815c0f255f912b4c42b1199b460c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:37 +0000

    Add package dev-up/1.14.2/dev_up-1.14.2.tar.gz

---
+++ packages/dev-up/dev_up-1.14.2.tar.gz/dev_up-1.14.2/dev_up/models/profile/get.py
@@ -0,0 +1,62 @@
+from datetime import datetime, timedelta
+from typing import Optional, Union
+
+from pydantic import BaseModel
+
+
+class ProfileGetResponseApi(BaseModel):
+    key: str
+    balance: float
+    limit: int
+    rate_limit: int
+    warn: int
+    used: int
+
+
+class ProfileGetResponseLP(BaseModel):
+    created: bool = False
+    status: bool = False
+
+
+class ProfileGetResponse(BaseModel):
+    id: int
+    id_vk: int
+    first_name: str
+    last_name: str 
+    last_ip: Optional[str]
+    last_online: int
+    req_time: int
+    api: Optional[ProfileGetResponseApi]
+    verified: int = False    
+    notifications: bool = False
+    premium: Union[int, bool] = False
+    unlimited: bool = False
+    ban: bool = False
+    banip: bool = False
+    tester: bool = False
+
+    lp: ProfileGetResponseLP = ProfileGetResponseLP()
+
+    @property
+    def last_online_datetime(self) -> datetime:
+        return datetime.fromtimestamp(self.last_online / 1000)
+
+    @property
+    def req_datetime(self) -> datetime:
+        return datetime.fromtimestamp(self.req_time / 1000)
+    
+    @property
+    def premium_datetime(self) -> datetime:
+        if isinstance(self.premium, bool):
+            raise ValueError("User don't have premium")
+        return datetime.fromtimestamp(self.premium / 1000)
+
+
+    @property
+    def premium_timedelta(self) -> timedelta:
+        if isinstance(self.premium, bool):
+            raise ValueError("User don't have premium")
+        return datetime.fromtimestamp(self.premium / 1000) - datetime.now()
+
+class ProfileGet(BaseModel):
+    response: ProfileGetResponse
