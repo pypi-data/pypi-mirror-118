2ca5af157bde33686e4e95f90e0e98dc760ce1d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:40 +0000

    Add package erix-python-utils/0.0.2/erix-python-utils-0.0.2.tar.gz

---
+++ packages/erix-python-utils/erix-python-utils-0.0.2.tar.gz/erix-python-utils-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="erix-python-utils",
+    version="0.0.2",
+    url="https://github.com/EricStratigakis/erix-python-utils",
+    author="Eric Stratigakis",
+    author_email="enstrati@uwaterloo.ca",
+    description="All of my utilities and tools in one place",
+    py_modules=["helloworld"],
+    package_dir={"":"src"},
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["requests"],
+    extra_requires={
+        "dev":["pytest"]
+    }
+)
