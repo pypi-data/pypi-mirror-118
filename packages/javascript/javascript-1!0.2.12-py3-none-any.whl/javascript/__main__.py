2756805466fe514d316c901a21f933cc452aff6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:44 +0000

    Add package javascript/1!0.2.12/javascript-1!0.2.12-py3-none-any.whl

---
+++ packages/javascript/javascript-1!0.2.12-py3-none-any.whl/javascript/__main__.py
@@ -0,0 +1,37 @@
+import os, sys, argparse, shutil
+
+parser = argparse.ArgumentParser(
+    description="javascript (JSPyBridge) package manager. Use this to clear or update the internal package store."
+)
+parser.add_argument("--clean", default=False, action="store_true")
+parser.add_argument("--update", default=False, action="store_true")
+parser.add_argument("--install", default=False, action="store")
+args = parser.parse_args()
+
+if args.clean:
+    d = os.path.dirname(__file__)
+    nm = d + "/js/node_modules/"
+    nl = d + "/js/package-lock.json"
+    np = d + "/js/package.json"
+    print("Deleting", nm, nl, np)
+    try:
+        shutil.rmtree(nm)
+    except Exception:
+        pass
+    try:
+        os.remove(nl)
+    except Exception:
+        pass
+    try:
+        os.remove(np)
+    except Exception:
+        pass
+elif args.update:
+    print("Updating package store")
+    os.chdir(os.path.dirname(__file__) + "/js")
+    os.system("npm update")
+elif args.install:
+    os.chdir(os.path.dirname(__file__) + "/js")
+    os.system(f"npm install {args.install}")
+else:
+    parser.print_help(sys.stderr)
