ad98f74a1397465b7b64571d750db7ada2e29dbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:30 +0000

    Add package cicdtest/0.27/cicdtest-0.27-py3-none-any.whl

---
+++ packages/cicdtest/cicdtest-0.27-py3-none-any.whl/ci_command/version.py
@@ -0,0 +1,37 @@
+
+
+"""
+    Title: version.py
+    Author: Akash Dwivedi
+    Language: Python
+    Date Created: 26-07-2021
+    Date Modified: 14-08-2021
+    Description:
+        ###############################################################
+        ## Checks the version of the cli  ## 
+         ###############################################################
+ """
+import click
+import json
+import os
+@click.command()
+def version():
+    '''
+    Find the version of the buildpan
+    '''
+    """
+    Variables Used:
+    a : Carries the description of the cli
+    y : stores the instance of the json
+    version : stores the version of the cli  
+    
+    return: Nil
+   """
+    a = '{"version": "0.1", "languages": "Python"}'
+    y = json.loads(a)
+    version = y["version"]
+    # with open(file_path, 'r') as f:
+    #      data = json.load(f)
+    #      version = data["Version"]
+    
+    print(f'Current Buildspan version is {version}')
