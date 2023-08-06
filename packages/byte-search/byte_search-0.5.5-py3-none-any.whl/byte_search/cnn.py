e82de7e7b88475261e1e32d1f9344b25f9c7ffd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:31 +0000

    Add package byte-search/0.5.5/byte_search-0.5.5-py3-none-any.whl

---
+++ packages/byte-search/byte_search-0.5.5-py3-none-any.whl/byte_search/cnn.py
@@ -0,0 +1,40 @@
+import torch
+from torch import nn
+
+class CharCNN(nn.Module):
+    
+    def __init__(self, d_model, kernels=[2, 3, 4]):
+        """1DCNN layer with max pooling
+
+        Args:
+            d_model (int): embedding dimension
+            kernels (list, optional): kernel sizes for convolution. Defaults to [2, 3, 4].
+        """
+        
+        super().__init__()
+                
+        self.convs = nn.ModuleList()
+        
+        for k in kernels:
+            
+            cv = nn.Conv1d(d_model, d_model, kernel_size=k, bias=False)
+            
+            self.convs.append(cv)
+                                                
+    def forward(self, x):
+        
+        x = x.transpose(1, 2)
+        
+        convs = []
+        
+        for conv in self.convs:
+            
+            convolved = conv(x)
+            
+            convolved, _ = torch.max(convolved, dim=-1)
+                                    
+            convs.append(convolved)
+        
+        convs = torch.stack(convs, dim=0)
+                           
+        return convs.max(0).values
