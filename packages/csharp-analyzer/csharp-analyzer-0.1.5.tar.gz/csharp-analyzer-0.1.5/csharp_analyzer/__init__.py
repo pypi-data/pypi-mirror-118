6f7adc4418b1c8bad861268401538c0f3831ee9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:32 +0000

    Add package csharp-analyzer/0.1.5/csharp-analyzer-0.1.5.tar.gz

---
+++ packages/csharp-analyzer/csharp-analyzer-0.1.5.tar.gz/csharp-analyzer-0.1.5/csharp_analyzer/__init__.py
@@ -0,0 +1,5 @@
+from csharp_analyzer.attribute import Attribute
+from csharp_analyzer.class_ import Class
+from csharp_analyzer.method import Method
+from csharp_analyzer.namespace import Namespace
+from csharp_analyzer.property import Property
