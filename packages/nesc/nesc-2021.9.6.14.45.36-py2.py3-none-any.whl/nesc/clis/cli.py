f868f04b6db6cd248c1e6e73f5620fb43131b8a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:05 +0000

    Add package nesc/2021.9.6.14.45.36/nesc-2021.9.6.14.45.36-py2.py3-none-any.whl

---
+++ packages/nesc/nesc-2021.9.6.14.45.36-py2.py3-none-any.whl/nesc/clis/cli.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : MeUtils.
+# @File         : __init__.py
+# @Time         : 2021/1/31 10:20 下午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : python meutils/clis/__init__.py
+
+
+import typer
+
+from meutils.pipe import *
+
+cli = typer.Typer(name="东北证券 clis")
+
+
+def _run_cmd(cmd, nohup=0):
+    cmd = f"nohup {cmd} &" if nohup else cmd
+    logger.debug(cmd)
+    return os.system(cmd)
+
+
+@cli.command(help="help")  # help会覆盖docstring
+def clitest(name: str):
+    """
+
+    @param name: name
+    @return:
+    """
+    typer.echo(f"Hello {name}")
+
+
+@cli.command()
+def extract4ddl(ipath: str, opath=None):
+    """解析ddl"""
+    from nesc.extract import extract4ddl
+    extract4ddl.main(ipath, opath)
+
+
+cli.command()
+def extract4excel(ipath: str, opath=None):
+    """解析excel"""
+    from nesc.extract import extract4excel
+    extract4excel.main(ipath, opath)
+
+if __name__ == '__main__':
+    cli()
