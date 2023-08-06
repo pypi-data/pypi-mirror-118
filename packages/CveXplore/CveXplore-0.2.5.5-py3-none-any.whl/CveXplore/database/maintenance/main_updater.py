c32c142ccf661efb2ed48f14494895408154c6a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:47 +0000

    Add package CveXplore/0.2.5.5/CveXplore-0.2.5.5-py3-none-any.whl

---
+++ packages/CveXplore/CveXplore-0.2.5.5-py3-none-any.whl/CveXplore/database/maintenance/main_updater.py
@@ -0,0 +1,50 @@
+from CveXplore.database.maintenance.Sources_process import (
+    CPEDownloads,
+    CVEDownloads,
+    CWEDownloads,
+    CAPECDownloads,
+    VIADownloads,
+    DatabaseIndexer,
+)
+
+
+class MainUpdater(object):
+    def __init__(self, datasource, repopulate=False):
+
+        self.datasource = datasource
+
+        self.repopulate = repopulate
+
+        self.sources = [
+            {"name": "cpe", "updater": CPEDownloads},
+            {"name": "cve", "updater": CVEDownloads},
+            {"name": "cwe", "updater": CWEDownloads},
+            {"name": "capec", "updater": CAPECDownloads},
+            {"name": "via4", "updater": VIADownloads},
+        ]
+
+        self.posts = [{"name": "ensureindex", "updater": DatabaseIndexer}]
+
+    def update(self):
+
+        for source in self.sources:
+            up = source["updater"]()
+            up.update()
+
+        for post in self.posts:
+            indexer = post["updater"]()
+            indexer.create_indexes()
+
+        self.datasource.set_handlers_for_collections()
+
+    def populate(self):
+
+        for source in self.sources:
+            populator = source["updater"]()
+            populator.populate()
+
+        for post in self.posts:
+            indexer = post["updater"]()
+            indexer.create_indexes()
+
+        self.datasource.set_handlers_for_collections()
