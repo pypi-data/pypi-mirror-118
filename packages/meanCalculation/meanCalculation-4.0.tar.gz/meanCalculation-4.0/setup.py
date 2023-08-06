a175c1ea10ba89a9a34d1276bfb9627691cb0cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:53 +0000

    Add package meanCalculation/4.0/meanCalculation-4.0.tar.gz

---
+++ packages/meanCalculation/meanCalculation-4.0.tar.gz/meanCalculation-4.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='meanCalculation',
+      version='4.0',
+      description='calculation of mean and Std deviation',
+      packages=['meanCalculation'],
+      author_email='varshakt6@gmail.com',
+      zip_safe=False)
