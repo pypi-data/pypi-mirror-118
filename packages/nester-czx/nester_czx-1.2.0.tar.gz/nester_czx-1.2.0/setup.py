294046ad2d0166afd1d4db9b36d0f69986df5f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:42 +0000

    Add package nester-czx/1.2.0/nester_czx-1.2.0.tar.gz

---
+++ packages/nester-czx/nester_czx-1.2.0.tar.gz/nester_czx-1.2.0/setup.py
@@ -0,0 +1,12 @@
+# 发布文件
+from distutils.core import setup
+
+setup(
+    name='nester_czx',
+    version='1.2.0',
+    py_modules=['nester_czx'],
+    author='czx',
+    author_email='1554579148@qq.com',
+    url='https://pypi.org',
+    description='A simple printer of nested lists by czx'
+)
