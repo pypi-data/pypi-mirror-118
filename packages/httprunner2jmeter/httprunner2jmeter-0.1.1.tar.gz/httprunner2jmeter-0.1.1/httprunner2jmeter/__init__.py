ed1c53862e8edcde4de90aac7fd213664e31f4b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:17 +0000

    Add package httprunner2jmeter/0.1.1/httprunner2jmeter-0.1.1.tar.gz

---
+++ packages/httprunner2jmeter/httprunner2jmeter-0.1.1.tar.gz/httprunner2jmeter-0.1.1/httprunner2jmeter/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.1.0"
+__description__ = "Make httprunner scripts to jmeter scripts."
+
+# import firstly for monkey patch if needed
+
+__all__ = [
+    "__version__",
+    "__description__",
+]
