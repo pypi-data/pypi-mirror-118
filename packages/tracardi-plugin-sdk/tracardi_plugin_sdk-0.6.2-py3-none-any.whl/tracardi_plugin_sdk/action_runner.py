7a886f6c33657c335d6a79e5d26dc8f2b6b13a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:45 +0000

    Add package tracardi-plugin-sdk/0.6.2/tracardi_plugin_sdk-0.6.2-py3-none-any.whl

---
+++ packages/tracardi-plugin-sdk/tracardi_plugin_sdk-0.6.2-py3-none-any.whl/tracardi_plugin_sdk/action_runner.py
@@ -0,0 +1,15 @@
+class ActionRunner:
+    id = None
+    debug = False
+    event = None
+    session = None
+    profile = None
+    flow = None
+    flow_history = None
+    console = None
+
+    async def run(self, **kwargs):
+        pass
+
+    async def close(self):
+        pass
