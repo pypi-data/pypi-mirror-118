7070585528fbc0d1fecfa41389fdf5b0ff06bde7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:14 +0000

    Add package ludovico/0.0.2/ludovico-0.0.2.tar.gz

---
+++ packages/ludovico/ludovico-0.0.2.tar.gz/ludovico-0.0.2/ludovico/__init__.py
@@ -0,0 +1,10 @@
+"""
+ludovico.
+
+An opinionated DataFrame-to-TeX table generator.
+"""
+
+from .ludovico import *  # noqa: F401, F403
+
+__version__ = "0.0.2"
+__author__ = "Andrés Ignacio Torres"
