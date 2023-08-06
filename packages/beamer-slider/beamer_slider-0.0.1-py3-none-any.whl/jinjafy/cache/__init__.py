efa67b23bf5ee57f904d122bc421253c37a3d576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:03 +0000

    Add package beamer-slider/0.0.1/beamer_slider-0.0.1-py3-none-any.whl

---
+++ packages/beamer-slider/beamer_slider-0.0.1-py3-none-any.whl/jinjafy/cache/__init__.py
@@ -0,0 +1,8 @@
+from thtools.cache.simplecache import cache_update_str as cache_update_str
+from thtools.cache.simplecache import cache_contains_str as cache_contains_str
+
+from thtools.cache.simplecache import cache_update_file as cache_update_file
+from thtools.cache.simplecache import cache_contains_file as cache_contains_file
+
+from thtools.cache.simplecache import cache_update_dir as cache_update_dir
+from thtools.cache.simplecache import cache_contains_dir as cache_contains_dir
