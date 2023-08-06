ede544c8faff388e09823918c7130611e52493d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:31 +0000

    Add package adgmlclass/0.0.6/adgmlclass-0.0.6.tar.gz

---
+++ packages/adgmlclass/adgmlclass-0.0.6.tar.gz/adgmlclass-0.0.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="adgmlclass",
+    version="0.0.6",
+    author="Ashlin Darius Govindasamy",
+    author_email="adgrules@hotmail.com",
+    url="https://www.adgstudios.co.za",
+    description="A class used for finding the best machine learning algorithm",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license='MIT', 
+    install_requires=["click", "pytz","pandas","numpy","matplotlib","seaborn","scikit-learn"]
+)
