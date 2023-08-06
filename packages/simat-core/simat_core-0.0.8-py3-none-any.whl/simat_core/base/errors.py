3b8d8ad755cc909a6298b274619e98152c638d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:02 +0000

    Add package simat-core/0.0.8/simat_core-0.0.8-py3-none-any.whl

---
+++ packages/simat-core/simat_core-0.0.8-py3-none-any.whl/simat_core/base/errors.py
@@ -0,0 +1,25 @@
+
+
+class FieldNotFoundInResp(Exception):
+    pass
+
+class NotAttribute(Exception):
+    pass
+
+class TypeInvalidInGetData(Exception):
+    pass
+
+class ListEmpty(Exception):
+    pass
+
+class KeyNotFound(Exception):
+    pass
+
+class RetryExcceedError(Exception):
+    pass
+
+class SetPreStepError(Exception):
+    pass
+
+class StatusCodeInvalidException(Exception):
+    pass
