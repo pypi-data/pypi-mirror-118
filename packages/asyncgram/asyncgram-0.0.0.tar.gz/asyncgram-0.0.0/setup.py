8940bd239b5246640330bebcf4a104b842a332e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:37 +0000

    Add package asyncgram/0.0.0/asyncgram-0.0.0.tar.gz

---
+++ packages/asyncgram/asyncgram-0.0.0.tar.gz/asyncgram-0.0.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['asyncgram']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-dotenv', 'requests']
+
+setup_kwargs = {
+    'name': 'asyncgram',
+    'version': '0.0.0',
+    'description': 'A lightweight, asynchronous telegram logging client',
+    'long_description': None,
+    'author': 'sumermalhotra',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<3.8',
+}
+
+
+setup(**setup_kwargs)
