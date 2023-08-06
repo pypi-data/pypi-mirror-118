bef5f9116344b96cfa3e2cef00e86eebb960ec97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:22 +0000

    Add package seven-cloudapp-ppmt/1.0.27/seven_cloudapp_ppmt-1.0.27.tar.gz

---
+++ packages/seven-cloudapp-ppmt/seven_cloudapp_ppmt-1.0.27.tar.gz/seven_cloudapp_ppmt-1.0.27/seven_cloudapp_ppmt/handlers/server/theme_s.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+:Author: HuangJingCan
+:Date: 2020-05-28 18:03:59
+@LastEditTime: 2021-09-01 18:57:55
+@LastEditors: HuangJianYi
+:description: 主题皮肤
+"""
+from seven_cloudapp.handlers.seven_base import *
+
+from seven_cloudapp.models.db_models.theme.theme_info_model import *
+from seven_cloudapp.models.db_models.skin.skin_info_model import *
+from seven_cloudapp.models.db_models.act.act_info_model import *
+from seven_cloudapp.models.db_models.machine.machine_info_model import *
+
+from seven_cloudapp.handlers.server.theme_s import SkinListHandler
+from seven_cloudapp.handlers.server.theme_s import ThemeUpdate
+
+
+class ThemeListHandler(SevenBaseHandler):
+    """
+    :description: 主题列表
+    """
+    def get_async(self):
+        """
+        :description: 主题列表
+        :param 
+        :return: 列表
+        :last_editors: HuangJianYi
+        """
+        app_id = self.get_param("app_id")
+        where = "is_release=1"
+        params = []
+        if app_id:
+            where += " and app_id=%s"
+            params.append(app_id)
+        else:
+            where += " and app_id=''"
+        dict_list = ThemeInfoModel(context=self).get_dict_list(where, params=params)
+
+        for dict_info in dict_list:
+            dict_info["client_json"] = self.json_loads(dict_info["client_json"]) if dict_info["client_json"] else {}
+            dict_info["server_json"] = self.json_loads(dict_info["server_json"]) if dict_info["server_json"] else {}
+
+        self.reponse_json_success(dict_list)
