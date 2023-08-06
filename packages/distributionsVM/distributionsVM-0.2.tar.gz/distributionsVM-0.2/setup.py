7c4931a44cd77e61108653e0a9d1a41b52cce37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:19 +0000

    Add package distributionsVM/0.2/distributionsVM-0.2.tar.gz

---
+++ packages/distributionsVM/distributionsVM-0.2.tar.gz/distributionsVM-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributionsVM',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributionsVM'],
+      author = 'Victor Menoscal',
+      author_email = 'alfonso_menos@hotmail.com',
+      zip_safe=False)
