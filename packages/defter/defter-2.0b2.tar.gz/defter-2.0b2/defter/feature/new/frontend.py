cf5697f6acdeaa12a3de55c7f7876682b8f41ac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:33 +0000

    Add package defter/2.0b2/defter-2.0b2.tar.gz

---
+++ packages/defter/defter-2.0b2.tar.gz/defter-2.0b2/defter/feature/new/frontend.py
@@ -0,0 +1,12 @@
+# Defter Python front-end script
+# Compile using the command: defter-frontendc [Python file] [Output directory].
+# For example: defter-frontendc xxx.py res
+# xxx.py -> Compiler -> res/
+#                       |-- xxx.html                      (Front-end entry code)
+#                           build/
+#                           |-- xxx.js                    (Main JavaScript API)
+#                           |-- DefterVM.runtime.js       (DefterVM Runtime library)
+#                           |-- ~.js                      (More JavaScript APIs)
+
+
+# TODO Coding here :-)
