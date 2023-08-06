acfcdb52299c79b97ac8e0b475e5576c0a60922a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:57 +0000

    Add package whatsappchattingbot/0.0.1/whatsappchattingbot-0.0.1.tar.gz

---
+++ packages/whatsappchattingbot/whatsappchattingbot-0.0.1.tar.gz/whatsappchattingbot-0.0.1/README.md
@@ -0,0 +1,28 @@
+# WhatsappBOT
+It's an automated whatsapp bot which can serve as chatbot and can perform search operations on google, youtube, wikipedia 
+
+It can automatically fetch for new messages and reply to the user.
+
+Libraries used - 
+
+Chatterbot - For chatBot
+
+Selenium - for controling browser and scrapping and sending messages
+
+os - to run existing chrome session
+
+Important - 
+
+1- you just need to login to chrome using whatsapp web Once. This doesn't run on chromedriver and hence you 
+
+    you won't have to login again and again. 
+
+2 - create a whatsapp group with name "Standby".
+
+3 - To train the bot for personal use please refer to ::
+
+
+    please contact me at srivastavas08@gmail.com
+
+
+4 - Nothing else needed just enjoy
---
+++ packages/whatsappchattingbot/whatsappchattingbot-0.0.1.tar.gz/whatsappchattingbot-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="whatsappchattingbot",
+    version="0.0.1",
+    author="Kiran Chandra",
+    author_email="srivastavas08@gmail.com",
+    description="Its an whatsapp chatbot that can receive and send message to user",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/srivastavas08/WhatsappBOT",
+    project_urls={
+        "Bug Tracker": "https://github.com/srivastavas08/WhatsappBOT/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
