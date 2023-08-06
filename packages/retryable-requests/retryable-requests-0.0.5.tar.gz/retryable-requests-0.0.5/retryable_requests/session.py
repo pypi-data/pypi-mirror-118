19b1c5e4c5d5ccbe62ac15f8f66e7621cc73b542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:45 +0000

    Add package retryable-requests/0.0.5/retryable-requests-0.0.5.tar.gz

---
+++ packages/retryable-requests/retryable-requests-0.0.5.tar.gz/retryable-requests-0.0.5/retryable_requests/session.py
@@ -0,0 +1,35 @@
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+from requests.sessions import Session
+from requests_toolbelt.sessions import BaseUrlSession
+
+DEFAULT_RETRY_STRATEGY = Retry(
+    total=5,
+    status_forcelist=[429, 500, 502, 503, 504],
+    backoff_factor=0.1,
+)
+
+
+class RetryableMixin:
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('retry_strategy', DEFAULT_RETRY_STRATEGY)
+
+        adapter = HTTPAdapter(max_retries=kwargs['retry_strategy'])
+        kwargs.pop('retry_strategy')
+        super().__init__(*args, **kwargs)
+
+        self.mount('http://', adapter)
+        self.mount('https://', adapter)
+
+    def request(self, *args, **kwargs):
+        # See https://docs.python-requests.org/en/master/user/advanced/#timeouts
+        kwargs.setdefault('timeout', (3.05, 5))
+        return super().request(*args, **kwargs)
+
+
+class RetryableBaseUrlSession(RetryableMixin, BaseUrlSession):
+    pass
+
+
+class RetryableSession(RetryableMixin, Session):
+    pass
