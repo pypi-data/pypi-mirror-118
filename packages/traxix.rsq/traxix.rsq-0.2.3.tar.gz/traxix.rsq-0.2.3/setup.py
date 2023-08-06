3a4e76c1914b0d50d5478ae589f01ec5d27d4f6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:11 +0000

    Add package traxix.rsq/0.2.3/traxix.rsq-0.2.3.tar.gz

---
+++ packages/traxix.rsq/traxix.rsq-0.2.3.tar.gz/traxix.rsq-0.2.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_namespace_packages
+
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+    requirements_dev = open("requirements-dev.txt").read().splitlines()
+
+    setup(
+        name="traxix.rsq",
+        version="0.2.3",
+        url="https://gitlab.com/traxix/python/rsq",
+        packages=find_namespace_packages(include=["traxix"]),
+        scripts=["rsq"],
+        license="GPLv3",
+        author="trax Omar Givernaud",
+        author_email="o.givernaud@gmail.com",
+        install_requires=required,
+        extras_require={"dev": requirements_dev},
+    )
