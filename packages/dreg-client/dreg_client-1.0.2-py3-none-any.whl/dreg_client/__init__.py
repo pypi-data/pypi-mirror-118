7035b04fc73fbe874ea8f31f1c38f478cfd3aa30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:01 +0000

    Add package dreg-client/1.0.2/dreg_client-1.0.2-py3-none-any.whl

---
+++ packages/dreg-client/dreg_client-1.0.2-py3-none-any.whl/dreg_client/__init__.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from .auth_service import AuthService, AuthServiceFailure, DockerTokenAuthService
+from .client import Client
+from .image import Image, PlatformImage, UnavailableImagePlatformError, UnexpectedImageManifestError
+from .manifest import (
+    ImageConfig,
+    InvalidPlatformNameError,
+    LegacyManifest,
+    Manifest,
+    ManifestList,
+    Platform,
+    UnusableImageConfigBlobPayloadError,
+    UnusableImageConfigBlobResponseError,
+    UnusableManifestPayloadError,
+    UnusableManifestResponseError,
+)
+from .registry import Registry
+from .repository import LegacyImageRequestError, Repository
+
+
+__version__ = "1.0.2"
+
+
+__all__ = (
+    "AuthService",
+    "AuthServiceFailure",
+    "Client",
+    "DockerTokenAuthService",
+    "Image",
+    "ImageConfig",
+    "InvalidPlatformNameError",
+    "LegacyManifest",
+    "LegacyImageRequestError",
+    "Manifest",
+    "ManifestList",
+    "Platform",
+    "PlatformImage",
+    "Registry",
+    "Repository",
+    "UnavailableImagePlatformError",
+    "UnexpectedImageManifestError",
+    "UnusableImageConfigBlobResponseError",
+    "UnusableImageConfigBlobPayloadError",
+    "UnusableManifestResponseError",
+    "UnusableManifestPayloadError",
+)
