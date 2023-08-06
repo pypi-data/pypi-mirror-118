f4d6729fdb08610f59865007731739c70d36f0c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:35 +0000

    Add package udcuplpckg-distributions/0.1/udcuplpckg_distributions-0.1.tar.gz

---
+++ packages/udcuplpckg-distributions/udcuplpckg_distributions-0.1.tar.gz/udcuplpckg_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='udcuplpckg_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['udcuplpckg_distributions'],
+      zip_safe=False)
