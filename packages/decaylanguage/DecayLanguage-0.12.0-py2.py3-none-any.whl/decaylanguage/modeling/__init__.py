c04b8042accbe0f8a3157bbf34863f6e2e7f7e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:45 +0000

    Add package decaylanguage/0.12.0/DecayLanguage-0.12.0-py2.py3-none-any.whl

---
+++ packages/decaylanguage/DecayLanguage-0.12.0-py2.py3-none-any.whl/decaylanguage/modeling/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from .amplitudechain import LS, AmplitudeChain
+
+__all__ = ("LS", "AmplitudeChain")
---
+++ packages/decaylanguage/DecayLanguage-0.12.0-py2.py3-none-any.whl/decaylanguage/utils/errors.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018-2021, Eduardo Rodrigues and Henry Schreiner.
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://github.com/scikit-hep/decaylanguage for details.
+
+from __future__ import absolute_import, division, print_function
+
+
+class LineFailure(RuntimeError):
+    def __init__(self, line, message):
+        super(LineFailure, self).__init__("{}: {}".format(line, message))
