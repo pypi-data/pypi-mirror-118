f51cb35b114d00967880c4626e716fa2c580f7cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:49 +0000

    Add package ci-plumber/1.1.3/ci_plumber-1.1.3-py3-none-any.whl

---
+++ packages/ci-plumber/ci_plumber-1.1.3-py3-none-any.whl/ci_plumber/__init__.py
@@ -0,0 +1,8 @@
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata  # type: ignore
+
+__version__ = importlib_metadata.version(__name__)
+
+from ci_plumber.module_attributes import Module_attribute
---
+++ packages/ci-plumber/ci_plumber-1.1.3-py3-none-any.whl/ci_plumber/main.py
@@ -0,0 +1,54 @@
+import importlib
+import importlib.resources
+import pkgutil
+
+import typer
+from rich.console import Console
+from rich.markdown import Markdown
+
+from ci_plumber import docs
+
+# https://packaging.python.org/guides/creating-and-discovering-plugins/
+discovered_plugins = {
+    name: importlib.import_module(name)
+    for finder, name, ispkg in pkgutil.iter_modules()
+    if name.startswith("ci_plumber_")
+}
+
+
+app = typer.Typer()
+# app.add_typer(openshift_tools.app, name="openshift")
+# app.add_typer(gitlab_tools.app, name="gitlab")
+# app.add_typer(azure_tools.app, name="azure")
+
+
+for plugin in discovered_plugins:
+    # We're going a bit ambiguous here with types so I've just turned mypy off
+    try:
+        app.add_typer(
+            discovered_plugins[plugin].app,  # type: ignore
+            name=discovered_plugins[plugin].name,  # type: ignore
+        )
+    except AttributeError:
+        pass
+
+
+@app.callback()
+def callback() -> None:
+    """
+    CI Plumber
+    """
+
+
+@app.command()
+def readme() -> None:
+    """
+    Show README.md
+    """
+    console = Console()
+    readme = importlib.resources.read_text(docs, "README.md")
+    console.print(Markdown(readme))
+
+
+if __name__ == "__main__":
+    app()
