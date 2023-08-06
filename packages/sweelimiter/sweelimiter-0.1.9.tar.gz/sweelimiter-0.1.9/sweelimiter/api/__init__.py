e30ca940d133ffffebd3ece22b50b81ea30f7a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:38 +0000

    Add package sweelimiter/0.1.9/sweelimiter-0.1.9.tar.gz

---
+++ packages/sweelimiter/sweelimiter-0.1.9.tar.gz/sweelimiter-0.1.9/sweelimiter/api/__init__.py
@@ -0,0 +1,4 @@
+from .limit_config import Limit
+from .limiter import Limiter
+from .limiter_engine import create_limiter_engine
+from .interval import *
---
+++ packages/sweelimiter/sweelimiter-0.1.9.tar.gz/sweelimiter-0.1.9/sweelimiter/backends/redis_backend.py
@@ -0,0 +1,53 @@
+import functools
+from typing import Optional, Tuple, Callable
+
+from sweelimiter.api.backend import Backend
+
+try:
+    from redis import Redis
+except ImportError:
+    raise AssertionError('Please install redis to use redis_backend')
+
+
+class RedisBackend(Backend):
+
+    async def process(self, key: str, interval_ms: int, limit: int) -> Tuple[Optional[str], dict]:
+        redis_key = 'limiter_' + key
+        r = self._redis()
+
+        seconds, microseconds = r.time()
+        time_now = int(seconds * 1000 + microseconds / 1000)
+
+        separation = round(interval_ms / limit)
+
+        r.setnx(redis_key, 0)
+        tat = max(int(r.get(redis_key)), time_now)
+
+        wait_left = tat - time_now
+        est_interval = interval_ms - separation
+
+        if wait_left <= est_interval:
+            new_tat = max(tat, time_now) + separation
+            r.set(redis_key, new_tat)
+            return (None, {
+                'key': key,
+                'tat': new_tat,
+                'separation': separation,
+                'wait': wait_left,
+                'est_interval': est_interval
+            })
+
+        return (f'Hit limiter {key}: {limit} allowed per {interval_ms}ms, ', {
+            'key': key,
+            'tat': tat,
+            'separation': separation,
+            'wait': wait_left,
+            'est_interval': est_interval
+        })
+
+    def __init__(self, redis_factory: Callable[[], Redis]):
+        self.redis_factory = redis_factory
+
+    @functools.lru_cache(maxsize=None)
+    def _redis(self):
+        return self.redis_factory()
