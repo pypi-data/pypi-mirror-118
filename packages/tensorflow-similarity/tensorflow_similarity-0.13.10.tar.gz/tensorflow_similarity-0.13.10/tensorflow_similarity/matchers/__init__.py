585f2e574bbe306680904b3c528df6fad2f3b609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:54 +0000

    Add package tensorflow-similarity/0.13.10/tensorflow_similarity-0.13.10.tar.gz

---
+++ packages/tensorflow-similarity/tensorflow_similarity-0.13.10.tar.gz/tensorflow_similarity-0.13.10/tensorflow_similarity/matchers/__init__.py
@@ -0,0 +1,7 @@
+# TODO(ovallis): Explain what the calibration metrics are
+"""HERE in Markdown"""
+# Match Types
+from .classification_match import ClassificationMatch  # noqa
+from .match_majority_vote import MatchMajorityVote  # noqa
+from .match_nearest import MatchNearest  # noqa
+from .utils import make_classification_matcher  # noqa
