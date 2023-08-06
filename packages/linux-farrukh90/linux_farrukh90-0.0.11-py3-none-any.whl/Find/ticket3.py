e0d746f081315b8214314d751a5b8a14eba799be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:38 +0000

    Add package linux-farrukh90/0.0.11/linux_farrukh90-0.0.11-py3-none-any.whl

---
+++ packages/linux-farrukh90/linux_farrukh90-0.0.11-py3-none-any.whl/Find/ticket3.py
@@ -0,0 +1,33 @@
+import os 
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+def filestructure():
+    try:
+        FILETOWORK="/tmp/configurations"
+        # Checks if "/tmp/configurations" is created
+        if os.path.getsize(FILETOWORK) <= 0:
+            print(bcolors.FAIL + FILETOWORK + " " + "seems to be empty" + bcolors.ENDC  )
+        else:
+            print(bcolors.OKGREEN + FILETOWORK + " " + "is created" + bcolors.ENDC  )
+        try:
+            configurations_content = os.listdir(FILETOWORK)
+            searchstring = "resolv.conf"
+            for x in (x for x in configurations_content if x in searchstring):
+                print(bcolors.OKGREEN + FILETOWORK + " " + "has a proper content" + bcolors.ENDC  )
+        except FileNotFoundError:
+            print("message")
+    except FileNotFoundError:
+        print(bcolors.FAIL +  FILETOWORK + " is not created"+ bcolors.ENDC)
+
+filestructure()
