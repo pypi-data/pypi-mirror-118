10af957bd5e40d9c497fddaaea98e8fbcf382c31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:21 +0000

    Add package hius/0.1.1/hius-0.1.1-py3-none-any.whl

---
+++ packages/hius/hius-0.1.1-py3-none-any.whl/hius/routing/exceptions.py
@@ -0,0 +1,18 @@
+class MountError(Exception):
+    '''Routes Error'''
+
+
+class RoutedPathError(Exception):
+    '''Routes Error'''
+
+
+class RoutedMethodsError(Exception):
+    '''Routes Error'''
+
+
+class NoMatchFound(Exception):
+    '''Router Error'''
+
+
+class ProtocolError(Exception):
+    '''URLPath Error'''
