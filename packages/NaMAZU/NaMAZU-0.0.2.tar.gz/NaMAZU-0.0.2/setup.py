bb399399d6bed9f24813533ff2a50be73019ff19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:47 +0000

    Add package NaMAZU/0.0.2/NaMAZU-0.0.2.tar.gz

---
+++ packages/NaMAZU/NaMAZU-0.0.2.tar.gz/NaMAZU-0.0.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+import NaMAZU
+
+DESCRIPTION = "NaMAZU: Pretty Usefull Library"
+NAME = "NaMAZU"
+AUTHOR = "NaMAZU Team"
+AUTHOR_EMAIL = "bunbun@icloud.com"
+URL = "https://github.com/NMZ0429/NaMAZU"
+LICENSE = "MIT"
+DOWNLOAD_URL = "https://github.com/NMZ0429/NaMAZU"
+VERSION = NaMAZU.__version__
+PYTHON_REQUIRES = ">=3.6.0"
+
+INSTALL_REQUIRES = ["numpy", "pillow"]
+
+PACKAGES = ["NaMAZU"]
+
+with open("README.md", "r") as fp:
+    readme = fp.read()
+long_description = readme
+
+setup(
+    name=NAME,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    maintainer=AUTHOR,
+    maintainer_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
+    long_description=long_description,
+    license=LICENSE,
+    url=URL,
+    version=VERSION,
+    download_url=DOWNLOAD_URL,
+    python_requires=PYTHON_REQUIRES,
+    install_requires=INSTALL_REQUIRES,
+    # extras_require=EXTRAS_REQUIRE,
+    packages=PACKAGES,
+    # classifiers=CLASSIFIERS,
+)
+
