910b6486b3ee3e6862ee15221e94c5fe2097586e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:17 +0000

    Add package distributions2000-L/0.1/distributions2000_L-0.1.tar.gz

---
+++ packages/distributions2000-L/distributions2000_L-0.1.tar.gz/distributions2000_L-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions2000_L',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions2000_L'],
+      zip_safe=False)
