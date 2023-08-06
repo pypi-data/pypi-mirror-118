0238b594a67fcbf634ffd9668e117bbc75470b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:42 +0000

    Add package hub/2.0.9/hub-2.0.9.tar.gz

---
+++ packages/hub/hub-2.0.9.tar.gz/hub-2.0.9/hub/util/keys.py
@@ -0,0 +1,54 @@
+from hub.core.storage.provider import StorageProvider
+import posixpath
+
+from hub import constants
+
+
+def get_chunk_key(key: str, chunk_name: str) -> str:
+    return posixpath.join(key, constants.CHUNKS_FOLDER, f"{chunk_name}")
+
+
+def get_dataset_meta_key() -> str:
+    # dataset meta is always relative to the `StorageProvider`'s root
+    return constants.DATASET_META_FILENAME
+
+
+def get_dataset_info_key() -> str:
+    # dataset info is always relative to the `StorageProvider`'s root
+    return constants.DATASET_INFO_FILENAME
+
+
+def get_dataset_lock_key() -> str:
+    return constants.DATASET_LOCK_FILENAME
+
+
+def get_tensor_meta_key(key: str) -> str:
+    return posixpath.join(key, constants.TENSOR_META_FILENAME)
+
+
+def get_tensor_info_key(key: str) -> str:
+    return posixpath.join(key, constants.TENSOR_INFO_FILENAME)
+
+
+def get_chunk_id_encoder_key(key: str) -> str:
+    return posixpath.join(
+        key,
+        constants.ENCODED_CHUNK_NAMES_FOLDER,
+        constants.ENCODED_CHUNK_NAMES_FILENAME,
+    )
+
+
+def dataset_exists(storage: StorageProvider) -> bool:
+    try:
+        storage[get_dataset_meta_key()]
+        return True
+    except KeyError:
+        return False
+
+
+def tensor_exists(key: str, storage: StorageProvider) -> bool:
+    try:
+        storage[get_tensor_meta_key(key)]
+        return True
+    except KeyError:
+        return False
