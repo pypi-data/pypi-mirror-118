7ecdea700b6a24cc285b9a8eac28cfb99da207e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:42 +0000

    Add package django-koldar-utils/2.55.0/django_koldar_utils-2.55.0-py3-none-any.whl

---
+++ packages/django-koldar-utils/django_koldar_utils-2.55.0-py3-none-any.whl/django_koldar_utils/models/AttrDict.py
@@ -0,0 +1,29 @@
+from collections import UserDict
+from typing import Tuple, Iterable
+
+
+class AttrDict(object):
+    """
+    A dictionary whose key can be accessed using "." notation
+    """
+
+    def __init__(self):
+        self.d = {}
+
+    def __getattr__(self, item: str):
+        return self.d[item]
+
+    def __setattr__(self, key: str, value):
+        self.d[key] = value
+
+    def __len__(self) -> int:
+        return len(self.d)
+
+    def __iter__(self):
+        return iter(self.d)
+
+    def __del__(self, key: str):
+        del self.d[key]
+
+    def items(self) -> Iterable[Tuple[str, any]]:
+        yield from self.d.items()
