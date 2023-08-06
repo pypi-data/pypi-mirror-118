f8d7b8d9f212cfafa904401d92cc15688dcd4867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:05 +0000

    Add package guzzy/0.1.7/guzzy-0.1.7-py3-none-any.whl

---
+++ packages/guzzy/guzzy-0.1.7-py3-none-any.whl/guzzy/commands/log.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# help : list all commits with diff for changes fot each one
+
+CWD="$(dirname "$(readlink -f "$0")")"
+
+show_diff="git log --color=always -n 1 {1}; seq -s_ 150|tr -d '[:digit:]' && \
+  git diff --color=always {1}~ {1}"
+
+git log --color=always --format="%C(auto)%h %s" | fzf --ansi \
+  --preview="$show_diff" --preview-window=right:55% \
+  --bind "ctrl-t:execute($CWD/files.sh {1})"
