4dcfce4a58c444d5e6ed294b7a77b287e203fed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:17 +0000

    Add package guzzy/0.1.6/guzzy-0.1.6.tar.gz

---
+++ packages/guzzy/guzzy-0.1.6.tar.gz/guzzy-0.1.6/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['guzzy']
+
+package_data = \
+{'': ['*'], 'guzzy': ['commands/*']}
+
+entry_points = \
+{'console_scripts': ['guzzy = guzzy:main']}
+
+setup_kwargs = {
+    'name': 'guzzy',
+    'version': '0.1.6',
+    'description': 'Interactive CLI git client inside fzf.',
+    'long_description': None,
+    'author': 'samedamci',
+    'author_email': 'samedamci@disroot.org',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/samedamci/guzzy',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
