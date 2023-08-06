05abf2a18838cb87b4787cd15138ddf3bdd251bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:00 +0000

    Add package distributions-ev/0.1/distributions_ev-0.1.tar.gz

---
+++ packages/distributions-ev/distributions_ev-0.1.tar.gz/distributions_ev-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_ev',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_ev'],
+      zip_safe=False)
