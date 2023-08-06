f313ad9d02127c2a90ea81b3fc61e708897ab00b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:43 +0000

    Add package date-assistant/0.11.2/date_assistant-0.11.2.tar.gz

---
+++ packages/date-assistant/date_assistant-0.11.2.tar.gz/date_assistant-0.11.2/date_assistant/date_formats.py
@@ -0,0 +1,15 @@
+# Let's use christmas date as an example
+# Who doesn't like christmas? :)
+
+# 2021-12-25
+YYYY_MM_DD = "%Y-%m-%d"
+# 25-12-2021
+DD_MM_YYYY = "%d-%m-%Y"
+# 12-25-2021
+MM_DD_YYYY = "%m-%d-%Y"
+# 12-2021
+MM_YYYY = "%m-%Y"
+# 2021-12
+YYYY_MM = "%Y-%m"
+# December 25, 2021
+MONTH_SPACE_DD_COMA_SPACE_YYYY = "%B %d, %Y"
---
+++ packages/date-assistant/date_assistant-0.11.2.tar.gz/date_assistant-0.11.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['date_assistant']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pre-commit>=2.15.0,<3.0.0', 'python-dateutil>=2.8.2,<3.0.0']
+
+setup_kwargs = {
+    'name': 'date-assistant',
+    'version': '0.11.2',
+    'description': '🗓 Classes and functions with intuitive names for common dates operations',
+    'long_description': '<p align="center">\n<a href="https://github.com/jalvaradosegura/date-assistant/actions?query=workflow%3ATests" target="_blank">\n    <img src="https://github.com/jalvaradosegura/date-assistant/actions/workflows/tests.yml/badge.svg" alt="Test">\n</a>\n<a href="https://pycqa.github.io/isort/" target="_blank">\n    <img src="https://img.shields.io/badge/%20imports-isort-%231674b1?style=flat&labelColor=ef8336" alt="isort">\n</a>\n<a href="https://github.com/psf/black" target="_blank">\n    <img src="https://img.shields.io/badge/code%20style-black-000000.svg" alt="black">\n</a>\n<a href="https://pepy.tech/project/date-assistant" target="_blank">\n    <img src="https://pepy.tech/badge/date-assistant" alt="Downloads">\n</a>\n<a href="https://codecov.io/gh/jalvaradosegura/date-assistant" target="_blank">\n    <img src="https://img.shields.io/codecov/c/github/jalvaradosegura/date-assistant?color=%2334D058" alt="Coverage">\n</a>\n<a href="https://pypi.org/project/date-assistant" target="_blank">\n    <img src="https://img.shields.io/pypi/v/date-assistant?color=%2334D058&label=pypi%20package" alt="Package version">\n</a>\n<a href="https://github.com/jalvaradosegura/date_assistant/blob/main/LICENSE" target="_blank">\n    <img src="https://img.shields.io/github/license/jalvaradosegura/date_assistant" alt="License">\n</a>\n</p>\n\n# Date Assistant\nFor more details take a look at the [docs](https://date-assistant.readthedocs.io)\n\n## Installation\ndate-assistant is published on [PyPI](https://pypi.org/project/date-assistant/) and can be installed from there:\n```\npip install date-assistant\n```\n\n## Usage\n> 💡 Please consider that the default date format is \'%Y-%m-%d\'. Anyways, you can indicate the format of your date if you need to.\n\n### Functions approach\n\n#### Get the difference of days, months or years between 2 dates\n```py\nfrom date_assistant import (\n    get_days_diff_between,\n    get_months_diff_between,\n    get_years_diff_between,\n)\nfrom date_assistant.formats import DD_MM_YYYY, YYYY_MM\n\n\nget_days_diff_between(\'2021-01-01\', \'2021-01-11\')\n# 10\nget_days_diff_between(\'2021-01-01\', \'21-01-2021\', date2_format=DD_MM_YYYY)\n# 20\n\nget_months_diff_between(\'2021-01-01\', \'2022-01-11\')\n# 12\nget_months_diff_between(\'2021-01-05\', \'2021-02-01\')\n# 0\nget_months_diff_between(\'2021-01\', \'2021-02-21\', date1_format=YYYY_MM)\n# 1\n\nget_years_diff_between(\'2021-01-01\', \'2022-01-11\')\n# 1\nget_years_diff_between(\'2021-01-05\', \'2022-01-01\')\n# 0\nget_years_diff_between(\'2021-01\', \'2023-01-01\', date1_format=YYYY_MM)\n# 2\n```\n> 💡 See how months and years are only counted if a full year or month has passed.\n\n#### Get the amount of years or months started between 2 dates\n```py\nfrom date_assistant import (\n    get_months_started_between,\n    get_years_started_between,\n)\nfrom date_assistant.formats import YYYY_MM\n\n\nget_months_started_between(\'2021-01-05\', \'2021-02-01\')\n# 1\nget_months_started_between(\'2021-01-01\', \'2022-01-11\')\n# 12\nget_months_started_between(\'2021-01\', \'2021-02-21\', date1_format=YYYY_MM)\n# 1\n\nget_years_started_between(\'2021-01-01\', \'2020-12-31\')\n# 1\nget_years_started_between(\'2021-01-05\', \'2022-01-01\')\n# 1\nget_years_started_between(\'2021-01\', \'2023-01-01\', date1_format=YYYY_MM)\n# 2\n```\n\n> 💡 In contrast with the previous block example, here you don\'t need a full year or month between dates to count. If a new year or month started, it count.\n\n### Classes approach\n\n#### Get the difference of days, months or years between 2 dates\n```py\nfrom date_assistant import DateAssistant\n\nmy_birthday_2021 = DateAssistant(\'2021-07-13\')\ndate_assistant_birthday = \'2021-08-18\'\n\nmy_birthday_2021.days_diff_with(date_assistant_birthday)\n# 36\nmy_birthday_2021.months_diff_with(date_assistant_birthday)\n# 1\nmy_birthday_2021.years_diff_with(date_assistant_birthday)\n# 0\n```\n\n#### Get the amount of years or months started since or until some date\n```py\nfrom date_assistant import DateAssistant\n\nlast_day_of_2021 = DateAssistant(\'2021-12-31\')\nfirst_day_of_2022 = \'2022-01-01\'\nfirst_day_of_2023 = \'2023-01-01\'\ndate_assistant_birthday = \'2021-08-18\'\n\nlast_day_of_2021.years_started_until(first_day_of_2022)\n# 1\nlast_day_of_2021.years_started_until(first_day_of_2023)\n# 2\nlast_day_of_2021.months_started_until(first_day_of_2022)\n# 1\nlast_day_of_2021.months_started_since(date_assistant_birthday)\n# 4\n```\n',
+    'author': 'Jorge Alvarado',
+    'author_email': 'alvaradosegurajorge@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/jalvaradosegura/date-assistant',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
