778e5c4da1520ed57a7f4639ba1626d3c5642b3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:40 +0000

    Add package trojanzoo-sphinx-theme/0.0.26/trojanzoo_sphinx_theme-0.0.26-py2.py3-none-any.whl

---
+++ packages/trojanzoo-sphinx-theme/trojanzoo_sphinx_theme-0.0.26-py2.py3-none-any.whl/trojanzoo_sphinx_theme/__init__.py
@@ -0,0 +1,21 @@
+"""TrojanZoo Sphinx theme.
+
+From https://github.com/ain-soph/trojanzoo_sphinx_theme.
+
+"""
+from os import path
+
+__version__ = '0.0.26'
+__version_full__ = __version__
+
+
+def get_html_theme_path():
+    """Return list of HTML theme paths."""
+    cur_dir = path.abspath(path.dirname(path.dirname(__file__)))
+    return cur_dir
+
+
+def setup(app):
+    # See https://www.sphinx-doc.org/en/master/development/theming.html
+    app.add_html_theme('trojanzoo_sphinx_theme',
+                       path.abspath(path.dirname(__file__)))
