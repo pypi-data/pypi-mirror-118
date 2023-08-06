eb34c0226ab787f7121b4778272ed2794005cb5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:33 +0000

    Add package repro-eval/0.3.3/repro_eval-0.3.3.tar.gz

---
+++ packages/repro-eval/repro_eval-0.3.3.tar.gz/repro_eval-0.3.3/setup.py
@@ -0,0 +1,23 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(name='repro_eval',
+      version='0.3.3',
+      description='A tool to quantify the replicability and reproducibility of system-oriented IR experiments.',
+      long_description=README,
+      long_description_content_type="text/markdown",
+      url='http://github.com/irgroup/repro_eval',
+      author='Timo Breuer',
+      author_email='timo.breuer@th-koeln.de',
+      license='MIT',
+      packages=['repro_eval', 'repro_eval.measure', 'repro_eval.measure.external'],
+      install_requires=[
+          'pytrec_eval',
+          'numpy',
+          'scipy',
+          'tqdm'
+      ],
+      zip_safe=False)
