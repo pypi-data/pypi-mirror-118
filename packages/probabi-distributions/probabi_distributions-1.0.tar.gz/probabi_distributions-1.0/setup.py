55d8e3957d20f3541d95b0c2e2df896114c593a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:55 +0000

    Add package probabi-distributions/1.0/probabi_distributions-1.0.tar.gz

---
+++ packages/probabi-distributions/probabi_distributions-1.0.tar.gz/probabi_distributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='probabi_distributions',
+	version='1.0',
+	description='Gaussian and Binomial distributions',
+	packages=['probabi_distributions'],
+	author= 'Darshit Sharma',
+	zip_safe=False)
