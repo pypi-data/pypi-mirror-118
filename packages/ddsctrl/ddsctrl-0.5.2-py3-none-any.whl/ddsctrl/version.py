2f1a0a6ffd5c311aea32dd5dc91cd6247843ef11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:40 +0000

    Add package ddsctrl/0.5.2/ddsctrl-0.5.2-py3-none-any.whl

---
+++ packages/ddsctrl/ddsctrl-0.5.2-py3-none-any.whl/ddsctrl/version.py
@@ -0,0 +1,6 @@
+""" ddsctrl/version.py """
+__version__ = "0.5.2"
+
+# 0.5.2   (01/10/2021): Handle multiple DDS connected to computer.
+# 0.5.1   (27/06/2019): Update dds driver name package (dds -> ad9xdds).
+# 0.5.0   (19/06/2019): Move to PyQt5 library.
