b41abfad3dc4863d00edf5eebbdc6e25b452e1ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:31 +0000

    Add package secondtoclock/0.0.6/secondtoclock-0.0.6.tar.gz

---
+++ packages/secondtoclock/secondtoclock-0.0.6.tar.gz/secondtoclock-0.0.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+setup(
+    name='secondtoclock',
+    version='0.0.6',
+    author="LulubelleIII",
+    author_email="<uytudef@gmail.com>",
+    description='A package to convert seconds to clock',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    keywords=['python', 'time', 'clock',
+              'seconds to clock', 'time converter'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
