9e611e0734160d56ea82df704ef04c7b55d99f97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:17 +0000

    Add package quickpac/1.0.0/quickpac-1.0.0.tar.gz

---
+++ packages/quickpac/quickpac-1.0.0.tar.gz/quickpac-1.0.0/quickpac/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from quickpac.api.barcode_api import BarcodeApi
+from quickpac.api.pickup_migros_api import PickupMigrosApi
+from quickpac.api.zip_api import ZIPApi
