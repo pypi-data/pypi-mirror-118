afe59c2f6aabb917d90fab35615449728c2a4b32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:35 +0000

    Add package mmpose/0.18.0/mmpose-0.18.0-py2.py3-none-any.whl

---
+++ packages/mmpose/mmpose-0.18.0-py2.py3-none-any.whl/mmpose/datasets/datasets/face/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .face_300w_dataset import Face300WDataset
+from .face_aflw_dataset import FaceAFLWDataset
+from .face_cofw_dataset import FaceCOFWDataset
+from .face_wflw_dataset import FaceWFLWDataset
+
+__all__ = [
+    'Face300WDataset', 'FaceAFLWDataset', 'FaceWFLWDataset', 'FaceCOFWDataset'
+]
