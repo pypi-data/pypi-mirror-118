4a20676dae112e022e83e98f7e500c7f96897fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:46 +0000

    Add package u8darts/0.11.0/u8darts-0.11.0.tar.gz

---
+++ packages/u8darts/u8darts-0.11.0.tar.gz/u8darts-0.11.0/darts/models/__init__.py
@@ -0,0 +1,58 @@
+"""
+Models
+------
+"""
+from ..logging import get_logger
+
+logger = get_logger(__name__)
+
+# Forecasting
+from .baselines import NaiveMean, NaiveSeasonal, NaiveDrift
+from .exponential_smoothing import ExponentialSmoothing
+from .theta import Theta, FourTheta
+from .arima import ARIMA
+from .fft import FFT
+from .varima import VARIMA
+
+try:
+    from .auto_arima import AutoARIMA
+except ModuleNotFoundError:
+    logger.warning("Support for AutoARIMA is not available. To enable it, install u8darts[pmdarima] or u8darts[all].")
+
+try:
+    from .prophet import Prophet
+except ModuleNotFoundError:
+    logger.warning("Support Facebook Prophet is not available. "
+                   "To enable it, install u8darts[prophet] or u8darts[all].")
+
+try:
+    from .block_rnn_model import BlockRNNModel
+    from .rnn_model import RNNModel
+    from .tcn_model import TCNModel
+    from .nbeats import NBEATSModel
+    from .transformer_model import TransformerModel
+
+except ModuleNotFoundError:
+    logger.warning("Support Torch based models not available. To enable it, install u8darts[torch] or u8darts[all].")
+
+# Regression
+from .linear_regression_model import LinearRegressionModel
+from .random_forest import RandomForest
+from .regression_model import RegressionModel
+
+try:
+    from .gradient_boosted_model import LightGBMModel
+except:
+    logger.warning("Support for LightGBM not available. To enable LightGBM support in Darts, follow the detailed "
+                   "install instructions for LightGBM in the README: "
+                   "https://github.com/unit8co/darts/blob/master/README.md")
+
+# Ensembling
+from .ensemble_model import EnsembleModel
+from .baselines import NaiveEnsembleModel
+from .regression_ensemble_model import RegressionEnsembleModel
+
+# Filtering
+from .moving_average import MovingAverage
+from .kalman_filter import KalmanFilter
+from .gaussian_process_filter import GaussianProcessFilter
---
+++ packages/u8darts/u8darts-0.11.0.tar.gz/u8darts-0.11.0/darts/tests/test_ensemble_models.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+from .base_test_class import DartsBaseTestClass
+from ..utils import timeseries_generation as tg
+from ..models import NaiveDrift, NaiveSeasonal, Theta, ExponentialSmoothing
+from ..models import NaiveEnsembleModel
+
+
+class EnsembleModelsTestCase(DartsBaseTestClass):
+    series1 = tg.sine_timeseries(value_frequency=(1 / 5), value_y_offset=10, length=50)
+    series2 = tg.linear_timeseries(length=50)
+
+    def test_input_models(self):
+        with self.assertRaises(ValueError):
+            NaiveEnsembleModel([])
+        with self.assertRaises(ValueError):
+            NaiveEnsembleModel([NaiveDrift, NaiveSeasonal, Theta, ExponentialSmoothing])
+        with self.assertRaises(ValueError):
+            NaiveEnsembleModel([NaiveDrift(), NaiveSeasonal, Theta(), ExponentialSmoothing()])
+        NaiveEnsembleModel([NaiveDrift(), NaiveSeasonal(), Theta(), ExponentialSmoothing()])
+
+    def test_call_predict(self):
+        naive_ensemble = NaiveEnsembleModel([NaiveSeasonal(), Theta()])
+        with self.assertRaises(Exception):
+            naive_ensemble.predict(5)
+        naive_ensemble.fit(self.series1)
+        naive_ensemble.predict(5)
+
+    def test_predict_ensemble(self):
+        naive = NaiveSeasonal(K=5)
+        theta = Theta()
+        naive_ensemble = NaiveEnsembleModel([naive, theta])
+        naive_ensemble.fit(self.series1 + self.series2)
+        forecast_naive_ensemble = naive_ensemble.predict(5)
+        naive.fit(self.series1 + self.series2)
+        theta.fit(self.series1 + self.series2)
+        forecast_mean = 0.5 * naive.predict(5) + 0.5 * theta.predict(5)
+
+        self.assertTrue(np.array_equal(forecast_naive_ensemble.values(), forecast_mean.values()))
+
+
+if __name__ == '__main__':
+    import unittest
+
+    unittest.main()
