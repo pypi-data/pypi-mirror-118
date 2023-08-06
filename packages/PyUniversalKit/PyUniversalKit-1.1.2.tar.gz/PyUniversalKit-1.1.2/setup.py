12b2e9ad312af510a41ce9344ac0deb95eb842cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:50 +0000

    Add package PyUniversalKit/1.1.2/PyUniversalKit-1.1.2.tar.gz

---
+++ packages/PyUniversalKit/PyUniversalKit-1.1.2.tar.gz/PyUniversalKit-1.1.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+from setuptools import find_packages
+
+setuptools.setup(
+    name = "PyUniversalKit",
+    version = "1.1.2",
+    author = "MatthewYt",
+    author_email = "2158707744@qq.com",
+    description = "Hello,PyUniversalKit!",
+    packages = find_packages(),
+    url='https://github.com/yangtang-special/PyUniversalKit/tree/formal',
+    long_description_content_type = "text/markdown",
+    data_files = ["README.md"],
+    long_description = open('README.md',encoding="utf-8").read(),
+    install_requires = [
+        "requests",
+        "prettytable",
+        "lxml",
+        "colorama"
+    ]
+
+)
+
