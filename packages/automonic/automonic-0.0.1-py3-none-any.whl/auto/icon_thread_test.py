e767fc5bcf5b182247f60cd70a65652f03719556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:36 +0000

    Add package automonic/0.0.1/automonic-0.0.1-py3-none-any.whl

---
+++ packages/automonic/automonic-0.0.1-py3-none-any.whl/auto/icon_thread_test.py
@@ -0,0 +1,43 @@
+import auto.icon_thread
+import pytest
+from unittest.mock import MagicMock, patch
+
+IconThread = auto.icon_thread.IconThread
+
+@pytest.fixture(scope='function')
+def mock_icon():
+    with patch.object(auto.icon_thread.pystray, 'Icon') as icon:
+        yield icon
+
+def test_icon_thread_init(mock_icon):
+    t = IconThread(1, 2, a=3, b=4)
+    assert t._icon_args == (1, 2,)
+    assert t._icon_kwargs == {'a' : 3, 'b': 4}
+
+def test_icon_thread_run(mock_icon):
+    t = IconThread(1, 2, a=3, b=4)
+    t.icon = MagicMock()
+
+    t.run()
+    mock_icon.assert_called_once_with(1, 2, a=3, b=4)
+    t.icon.run.assert_called_once_with()
+
+def test_icon_thread_stop(mock_icon):
+    t = IconThread(1, 2, a=3, b=4)
+    t.stop() # does nothing since .icon is None
+
+    t.icon = MagicMock()
+    t.stop() # calls .icon.stop()
+    t.icon.stop.assert_called_once_with()
+
+def test_icon_thread_as_context_manager(mock_icon):
+    t = IconThread()
+    t.run = MagicMock()
+    t.stop = MagicMock()
+
+    with t:
+        t.run.assert_called_once_with()
+        t.stop.assert_not_called()
+
+    t.run.assert_called_once_with()
+    t.stop.assert_called_once_with()
