a00a9a75d2d2c00fda8fd43f2c673baa77d4dd30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:59 +0000

    Add package Grafico/0.1.0/Grafico-0.1.0.tar.gz

---
+++ packages/Grafico/Grafico-0.1.0.tar.gz/Grafico-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, version
+
+descri = "Graphics generation package."
+
+setup(name='Grafico',
+    version='0.1.0',
+    description=descri,
+    packages=['grafico'],
+    author='Guilherme Donizetti',
+    author_email = 'guilhermetecnologias@gmail.com',
+    zip_safe=False)
