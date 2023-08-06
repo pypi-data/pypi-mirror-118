8ad62737b0f7142e735b63bc173a855bea6f67f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:57 +0000

    Add package fastapi-datatables/0.4.0/fastapi_datatables-0.4.0-py3-none-any.whl

---
+++ packages/fastapi-datatables/fastapi_datatables-0.4.0-py3-none-any.whl/fastapi_datatables/__init__.py
@@ -0,0 +1,14 @@
+from .query import get_filtration
+from .filtration import FiltrationData, ParsingData
+from .table import DataTable
+from .operations import FiltrationOperator
+from .schemas import Filterable, Filter
+
+__all__ = [
+    "get_filtration",
+    "DataTable",
+    "FiltrationData",
+    "FiltrationOperator",
+    "Filterable",
+    "Filter",
+]
