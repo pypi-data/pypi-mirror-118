c51ab6adbb492dc4367e370e63365b4e5a140533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:56 +0000

    Add package cicdtest/0.66/cicdtest-0.66-py3-none-any.whl

---
+++ packages/cicdtest/cicdtest-0.66-py3-none-any.whl/ci_commands/encrypt.py
@@ -0,0 +1,61 @@
+"""
+    Title: encrypt.py
+    Author: Himshi C.
+    Modified By: Kushagra A.
+    Language: Python
+    Date Created: 02-09-2021
+    Date Modified: 03-09-2021
+    Description:
+        ###############################################################
+        ## Encrypt and decrypt a json file   ## 
+         ###############################################################
+ """
+
+from cryptography.fernet import Fernet
+import os
+
+
+class Encryptor():
+    
+    # generate a key for encrypting a file
+    def key_create(self):
+        key = Fernet.generate_key()
+        return key
+
+    # writing a key in a file
+    def key_write(self, key, key_name):
+        with open(key_name, 'wb') as mykey:
+            mykey.write(key)
+
+    # loading a key
+    def key_load(self, key_name):
+        with open(key_name, 'rb') as mykey:
+            key = mykey.read()
+        return key
+
+    # encrypting a file
+    def encrypt_file(self, key, file_name):
+        def get_file_data(filename:str) -> bytes:
+            with open(filename, 'r') as fo:
+                 data = fo.read()
+            return data.encode("utf-8")
+
+        plain_text:bytes = get_file_data(file_name)
+        f:"Fernet" = Fernet(key)
+        cipher_text = f.encrypt(plain_text)
+
+        with open (file_name + ".enc", 'wb') as fo:
+            fo.write(cipher_text)
+        os.remove(file_name)
+    
+    # decrypting a file
+    def decrypt_file(self, key, file_name):
+        
+        f:"Fernet" = Fernet(key)
+        with open(file_name, 'rb') as fo:
+            cipher_text = fo.read()
+        plain_text2 = f.decrypt(cipher_text)
+
+        with open(file_name[:-4], 'wb') as fo:               # opening file in binary mode
+            fo.write(plain_text2)
+        os.remove(file_name)
