f665e18597c907e976824e2fcbc1bf655f09b688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:12 +0000

    Add package fjgu-probability/0.2/fjgu_probability-0.2.tar.gz

---
+++ packages/fjgu-probability/fjgu_probability-0.2.tar.gz/fjgu_probability-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='fjgu_probability',
+      version='0.2',
+      description='Gaussian and Binomial Distributions',
+      packages=['fjgu_probability'],
+      author='franco Gutierrez',
+      zip_safe=False)
