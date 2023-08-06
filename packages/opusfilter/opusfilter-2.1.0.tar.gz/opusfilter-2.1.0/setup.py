fdf1d41e27a10a3a30c061dd12e1a3adae8f7374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:51 +0000

    Add package opusfilter/2.1.0/opusfilter-2.1.0.tar.gz

---
+++ packages/opusfilter/opusfilter-2.1.0.tar.gz/opusfilter-2.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="opusfilter",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    author="Mikko Aulamo, Sami Virpioja",
+    author_email="mikko.aulamo@helsinki.fi",
+    description="Toolbox for filtering parallel corpora",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Helsinki-NLP/OpusFilter",
+    packages=setuptools.find_packages(),
+    scripts=["bin/opusfilter", "bin/opusfilter-cmd", "bin/opusfilter-diagram", "bin/opusfilter-duplicates", "bin/opusfilter-scores", "bin/opusfilter-test"],
+    install_requires=["opustools", "beautifulsoup4", "fasttext", "graphviz", "langid", "matplotlib", "fast-mosestokenizer", "pandas>=0.24.0", "pycld2", "pyhash", "ruamel.yaml>=0.15.0", "regex", "requests", "scikit-learn", "tqdm"],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    python_requires=">=3.6",
+)
