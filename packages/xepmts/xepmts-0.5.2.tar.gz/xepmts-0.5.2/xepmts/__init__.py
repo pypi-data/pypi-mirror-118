8b06cf62f3ab468d52be877241cf6528181a9aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:18:32 +0000

    Add package xepmts/0.5.2/xepmts-0.5.2.tar.gz

---
+++ packages/xepmts/xepmts-0.5.2.tar.gz/xepmts-0.5.2/xepmts/__init__.py
@@ -0,0 +1,22 @@
+"""Top-level package for xepmts."""
+
+__author__ = """Yossi Mosbacher"""
+__email__ = 'joe.mosbacher@gmail.com'
+__version__ = '0.5.2'
+
+from xepmts.db.client import default_client, get_client, get_admin_client
+from . import streams
+
+def settings(**kwargs):
+    from eve_panel import settings as panel_settings
+    if not kwargs:
+        return dir(panel_settings)
+    else:
+        for k,v in kwargs.items():
+            setattr(panel_settings, k, v)
+
+def extension():
+    import eve_panel
+    eve_panel.extension()
+
+notebook = extension
