f3239edcfa2e8106cb859b2ba74587e1c7e26aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:22 +0000

    Add package RyStats/0.4.0/RyStats-0.4.0.tar.gz

---
+++ packages/RyStats/RyStats-0.4.0.tar.gz/RyStats-0.4.0/inferential/__init__.py
@@ -0,0 +1,7 @@
+from .ttests import *
+from .oneway_anova import *
+from .post_hoc import *
+from .twoway_anova import *
+from .linear_regression import *
+from .logistic_regression import *
+from .simple_mediation import *
