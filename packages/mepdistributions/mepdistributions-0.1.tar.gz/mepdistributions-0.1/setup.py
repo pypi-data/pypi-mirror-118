7e45034f5191218577b1154060e6a2f323d8c037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:30 +0000

    Add package mepdistributions/0.1/mepdistributions-0.1.tar.gz

---
+++ packages/mepdistributions/mepdistributions-0.1.tar.gz/mepdistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mepdistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['mepdistributions'],
+      zip_safe=False)
