d86822c98114e363c0c46e30a8915552aedaa7c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:35 +0000

    Add package ci-plumber-azure/0.2.0/ci_plumber_azure-0.2.0.tar.gz

---
+++ packages/ci-plumber-azure/ci_plumber_azure-0.2.0.tar.gz/ci_plumber_azure-0.2.0/ci_plumber_azure/__init__.py
@@ -0,0 +1,35 @@
+import typer
+
+from ci_plumber import Module_attribute
+from ci_plumber_azure.auth import (
+    list_subscriptions,
+    login,
+    set_default_subscription,
+)
+from ci_plumber_azure.create_app import create_app
+from ci_plumber_azure.create_registry import create_registry
+from ci_plumber_azure.database import create_database
+
+attributes = [Module_attribute.image_store, Module_attribute.consumer]
+
+name = "azure"
+
+app: typer.Typer = typer.Typer()
+
+app.command(name="create-registry")(create_registry)
+app.command(name="login")(login)
+app.command(name="create-app")(create_app)
+app.command(name="set-default-subscription")(set_default_subscription)
+app.command(name="list-subscriptions")(list_subscriptions)
+app.command(name="create-database")(create_database)
+
+
+@app.callback()
+def callback() -> None:
+    """
+    Tools to manage deploying to Azure
+    """
+
+
+if __name__ == "__main__":
+    app()
