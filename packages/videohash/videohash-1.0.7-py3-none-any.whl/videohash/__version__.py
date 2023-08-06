2a05697fa54e04dcff573193c8523ef4e824f15f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:06 +0000

    Add package videohash/1.0.7/videohash-1.0.7-py3-none-any.whl

---
+++ packages/videohash/videohash-1.0.7-py3-none-any.whl/videohash/__version__.py
@@ -0,0 +1,8 @@
+__title__ = "videohash"
+__description__ = "videohash is a video fingerprinting / video hashing tool written in Python"
+__url__ = "https://akamhy.github.io/videohash/"
+__version__ = "1.0.7"
+__author__ = "akamhy"
+__author_email__ = "akamhy@yahoo.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2021 Akash Mahanty et al."
