e935981febc7a64ec7144f1ccf5fdd147e44c284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:07 +0000

    Add package dash-table/5.0.0/dash_table-5.0.0.tar.gz

---
+++ packages/dash-table/dash_table-5.0.0.tar.gz/dash_table-5.0.0/setup.py
@@ -0,0 +1,15 @@
+import io
+from setuptools import setup
+
+setup(
+    name="dash_table",
+    version="5.0.0",
+    author="Chris Parmer <chris@plotly.com>",
+    packages=["dash_table"],
+    include_package_data=True,
+    license="MIT",
+    description="Dash table",
+    long_description=io.open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    install_requires=[],
+)
