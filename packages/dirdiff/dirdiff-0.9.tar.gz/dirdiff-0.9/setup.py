4df3b3ac28213a0cbb3d18b2258486e987f1d34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:23 +0000

    Add package dirdiff/0.9/dirdiff-0.9.tar.gz

---
+++ packages/dirdiff/dirdiff-0.9.tar.gz/dirdiff-0.9/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'dirdiff',        
+  packages = ['dirdiff'],   
+  version = '0.9',     
+  license='gpl-3.0',        
+  description = 'A utility to diff and patch entire directories, similiar to the GNU diff and patch for files.',  
+  author = 'Vishant Nambiar',                  
+  author_email = 'vishantnambiar@gmail.com',     
+  url = 'https://github.com/vishant-nambiar/dirdiff',  
+  download_url = 'https://github.com/vishant-nambiar/dirdiff/archive/refs/tags/0.9.tar.gz',   
+  keywords = ['diff', 'patch', 'directories', 'folders', 'bash'],   
+  install_requires=[            
+          'subprocess',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',  
+    'Programming Language :: Python :: 3',     
+  ],
+)
