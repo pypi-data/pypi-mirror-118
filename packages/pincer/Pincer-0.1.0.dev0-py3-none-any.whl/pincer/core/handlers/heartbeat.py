5d27f615cea03036d0454446f39dd1941a3bd9bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:24 +0000

    Add package pincer/0.1.0.dev0/Pincer-0.1.0.dev0-py3-none-any.whl

---
+++ packages/pincer/Pincer-0.1.0.dev0-py3-none-any.whl/pincer/core/handlers/heartbeat.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+# MIT License
+#
+# Copyright (c) 2021 Pincer
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+from __future__ import annotations
+
+import logging
+from asyncio import sleep
+from typing import Optional
+
+from websockets.legacy.client import WebSocketClientProtocol
+
+from pincer import __package__
+from pincer.core.dispatch import GatewayDispatch
+from pincer.exceptions import HeartbeatError
+
+heartbeat: float = 0
+sequence: Optional[int] = None
+
+log = logging.getLogger(__package__)
+
+
+def get_heartbeat() -> float:
+    """
+    Get the current heartbeat.
+
+    :return:
+        The current heartbeat of the client.
+        Default is 0 (client has not initialized the heartbeat yet.)
+
+    """
+    return heartbeat
+
+
+async def __send_heartbeat(socket: WebSocketClientProtocol):
+    # TODO: Fix docs
+
+    global sequence
+
+    log.debug(f"Sending heartbeat (seq: {sequence})")
+    await socket.send(str(GatewayDispatch(1, sequence)))
+
+
+async def handle_hello(socket: WebSocketClientProtocol,
+                       payload: GatewayDispatch):
+    """
+    Handshake between the discord API and the client.
+    Retrieve the heartbeat for maintaining a connection.
+    """
+    global heartbeat
+
+    log.debug("Handling initial discord hello websocket message.")
+    heartbeat = payload.data.get("heartbeat_interval")
+
+    if not heartbeat:
+        log.error(
+            "No `heartbeat_interval` is present. Has the API changed? "
+            f"(payload: {payload})"
+        )
+
+        raise HeartbeatError(
+            "Discord hello is missing `heartbeat_interval` in payload."
+            "Because of this the client can not maintain a connection. "
+            "Check logging for more information."
+        )
+
+    heartbeat /= 1000
+    log.debug(f"Maintaining a connection with heartbeat: {heartbeat}")
+
+    if sequence:
+        await socket.send(str(GatewayDispatch(6, sequence, seq=sequence)))
+    else:
+        await __send_heartbeat(socket)
+
+
+async def handle_heartbeat(socket: WebSocketClientProtocol, _):
+    # TODO: Fix docs
+    global heartbeat
+
+    logging.debug(f"Resting heart for {heartbeat}s")
+    await sleep(heartbeat)
+    await __send_heartbeat(socket)
+
+
+def update_sequence(seq: int):
+    """
+    Update the heartbeat sequence.
+
+    :param seq:
+        The new heartbeat sequence to be updated with.
+    """
+    global sequence
+    log.debug("Updating heartbeat sequence...")
+    sequence = seq
---
+++ packages/pincer/Pincer-0.1.0.dev0-py3-none-any.whl/pincer/core/http.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+# MIT License
+#
+# Copyright (c) 2021 Pincer
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import asyncio
+import logging
+from enum import Enum, auto
+from json import dumps
+from typing import Dict, Any, Optional, Protocol
+
+from aiohttp import ClientSession
+from aiohttp.client import _RequestContextManager
+from aiohttp.typedefs import StrOrURL
+
+from pincer import __package__
+from pincer.exceptions import (
+    NotFoundError, BadRequestError, NotModifiedError, UnauthorizedError,
+    ForbiddenError, MethodNotAllowedError, RateLimitError, ServerError,
+    HTTPError
+)
+
+log = logging.getLogger(__package__)
+
+
+class RequestMethod(Enum):
+    """HTTP Protocols supported by aiohttp"""
+
+    DELETE = auto()
+    GET = auto()
+    HEAD = auto()
+    OPTIONS = auto()
+    PATCH = auto()
+    POST = auto()
+    PUT = auto()
+
+
+class HttpCallable(Protocol):
+    """aiohttp HTTP method"""
+
+    def __call__(
+        self, url: StrOrURL, *,
+        allow_redirects: bool = True, json: Dict = None, **kwargs: Any
+    ) -> _RequestContextManager:
+        pass
+
+
+class HTTPClient:
+    """Interacts with Discord API through HTTP protocol"""
+
+    def __init__(self, token: str, version: int = 9, ttl: int = 5):
+        """
+        Instantiate a new HttpApi object.
+
+        :param token:
+            Discord API token
+
+        Keyword Arguments:
+
+        :param version:
+            The discord API version.
+            See `developers/docs/reference#api-versioning`.
+
+        :param ttl:
+            Max amount of attempts after error code 5xx
+        """
+        self.header: Dict[str, str] = {
+            "Authorization": f"Bot {token}"
+        }
+        self.endpoint: str = f"https://discord.com/api/v{version}"
+        self.max_ttl: int = ttl
+        self.__http_exceptions: Dict[int, HTTPError] = {
+            304: NotModifiedError(),
+            400: BadRequestError(),
+            401: UnauthorizedError(),
+            403: ForbiddenError(),
+            404: NotFoundError(),
+            405: MethodNotAllowedError(),
+            429: RateLimitError()
+        }
+
+    async def __send(
+        self, method: RequestMethod, route: str, *,
+        __ttl: int = None, data: Optional[Dict] = None
+    ) -> Dict:
+        # TODO: Fix docs
+        __ttl = __ttl or self.max_ttl
+
+        if __ttl == 0:
+            logging.error(
+                f"{method.value.name} {route} has reached the "
+                f"maximum retry count of  {self.max_ttl}."
+            )
+
+            raise ServerError(f"Maximum amount of retries for `{route}`.")
+
+        async with ClientSession() as session:
+            methods: Dict[RequestMethod, HttpCallable] = {
+                RequestMethod.DELETE: session.delete,
+                RequestMethod.GET: session.get,
+                RequestMethod.HEAD: session.head,
+                RequestMethod.OPTIONS: session.options,
+                RequestMethod.PATCH: session.patch,
+                RequestMethod.POST: session.post,
+                RequestMethod.PUT: session.put,
+            }
+
+            sender = methods.get(method)
+
+            if not sender:
+                log.debug(
+                    "Could not find provided RequestMethod "
+                    f"({method.value.name}) key in `methods` "
+                    f"[http.py>__send]."
+                )
+
+                raise RuntimeError("Unsupported RequestMethod has been passed.")
+
+            log.debug(f"new {method.value} {route} | {dumps(data)}")
+
+            async with sender(
+                f"{self.endpoint}/{route}",
+                headers=self.header, json=data
+            ) as res:
+                if res.ok:
+                    log.debug(
+                        "Request has been sent successfully. "
+                        "Returning json response."
+                    )
+
+                    return (
+                        await res.json()
+                        if res.content_type == "application/json"
+                        else {}
+                    )
+
+                exception = self.__http_exceptions.get(res.status)
+
+                if exception:
+                    log.error(
+                        f"An http exception occurred while trying to send "
+                        f"a request to {route}. ({res.status}, {res.reason})"
+                    )
+
+                    exception.__init__(res.reason)
+                    raise exception
+
+                # status code is guaranteed to be 5xx
+                retry_in = 1 + (self.max_ttl - __ttl) * 2
+                log.debug(
+                    "Server side error occurred with status code "
+                    f"{res.status}. Retrying in {retry_in}s."
+                )
+
+                await asyncio.sleep(retry_in)
+                await self.__send(method, route, __ttl=__ttl - 1, data=data)
+
+    async def delete(self, route: str) -> Dict:
+        """
+        :return: JSON response from the discord API.
+        """
+        return await self.__send(RequestMethod.DELETE, route)
+
+    async def get(self, route: str) -> Dict:
+        """
+        :return: JSON response from the discord API.
+        """
+        return await self.__send(RequestMethod.GET, route)
+
+    async def head(self, route: str) -> Dict:
+        """
+        :return: JSON response from the discord API.
+        """
+        return await self.__send(RequestMethod.HEAD, route)
+
+    async def options(self, route: str) -> Dict:
+        """
+        :return: JSON response from the discord API.
+        """
+        return await self.__send(RequestMethod.OPTIONS, route)
+
+    async def patch(self, route: str, data: Dict) -> Dict:
+        """
+        :return: JSON response from the discord API.
+        """
+        return await self.__send(RequestMethod.PATCH, route, data=data)
+
+    async def post(self, route: str, data: Dict) -> Dict:
+        """
+        :return: JSON response from the discord API.
+        """
+        return await self.__send(RequestMethod.POST, route, data=data)
+
+    async def put(self, route: str, data: Dict) -> Dict:
+        """
+        :return: JSON response from the discord API.
+        """
+        return await self.__send(RequestMethod.PUT, route, data=data)
