3305eb2cb7ceadeb63e3afa53d56a364515df37e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:31 +0000

    Add package bruin/0.1.2/bruin-0.1.2.tar.gz

---
+++ packages/bruin/bruin-0.1.2.tar.gz/bruin-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from io import open
+from os import path
+
+import pathlib
+
+CURDIRT = pathlib.Path(__file__).parent
+
+README = (CURDIRT / "README.md").read_text()
+
+with open(path.join(CURDIRT, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if ('git+' not in x) and (not x.startswith("#"))]
+
+setup(
+    name='bruin',
+    description="Command line tool made by python for various toolbox used in UCLA",
+    version="0.1.2",
+    packages=find_packages(),
+    python_requires='>=3.6',
+    entry_points='''
+        [console_scripts]
+        bruin=bruin.__main__:main
+    ''',
+    author='Oswald He',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OswaldHe/bruin-cli"
+)
