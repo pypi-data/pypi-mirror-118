1a12b61909813828990b3fba8b71a53a00edf279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:16 +0000

    Add package downloader-python/1.0.4/downloader-python-1.0.4.tar.gz

---
+++ packages/downloader-python/downloader-python-1.0.4.tar.gz/downloader-python-1.0.4/README.md
@@ -0,0 +1,5 @@
+
+this is a downloader that faster than P2P
+I will make it better.
+
+
---
+++ packages/downloader-python/downloader-python-1.0.4.tar.gz/downloader-python-1.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+with open('README.md','r',encoding='utf-8') as f:
+    long_description = f.read()
+setup(
+    name='downloader-python',
+    version='1.0.4',
+    url='https://github.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='mc_creater',
+    author_email='guoxiuchen20170402@163.com',
+    license='MIT',
+    packages=['downloader-python'],
+    classifiers=['Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9',],
+    install_requires=['PyQt5','requests']
+
+)
