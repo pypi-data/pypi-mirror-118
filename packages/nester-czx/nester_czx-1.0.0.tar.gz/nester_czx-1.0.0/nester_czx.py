f2e581bd8ce42388ea24d50e1890998da9fbdece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:34 +0000

    Add package nester-czx/1.0.0/nester_czx-1.0.0.tar.gz

---
+++ packages/nester-czx/nester_czx-1.0.0.tar.gz/nester_czx-1.0.0/nester_czx.py
@@ -0,0 +1,9 @@
+"""
+用来打印是否包含嵌套列表的列表
+"""
+def print_lol(the_list):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
