71edd37830c520e3fc6458cf34e7fe66145cf9e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:23 +0000

    Add package beanie/1.2.7/beanie-1.2.7.tar.gz

---
+++ packages/beanie/beanie-1.2.7.tar.gz/beanie-1.2.7/beanie/__init__.py
@@ -0,0 +1,17 @@
+from beanie.migrations.controllers.free_fall import free_fall_migration
+from beanie.migrations.controllers.iterative import iterative_migration
+from beanie.odm.fields import PydanticObjectId, Indexed
+from beanie.odm.utils.general import init_beanie
+from beanie.odm.documents import Document
+
+__version__ = "1.2.7"
+__all__ = [
+    # ODM
+    "Document",
+    "init_beanie",
+    "PydanticObjectId",
+    "Indexed",
+    # Migrations
+    "iterative_migration",
+    "free_fall_migration",
+]
