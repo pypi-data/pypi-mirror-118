62d74459e12e8697eda907379dd085934aa07b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:40 +0000

    Add package envoyUtils/0.0.2/envoyUtils-0.0.2.tar.gz

---
+++ packages/envoyUtils/envoyUtils-0.0.2.tar.gz/envoyUtils-0.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="envoyUtils",
+    version="0.0.2",
+    description="Python utilities for the Enphase Envoy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["zeroconf"],
+    python_requires=">=3.8",
+)
