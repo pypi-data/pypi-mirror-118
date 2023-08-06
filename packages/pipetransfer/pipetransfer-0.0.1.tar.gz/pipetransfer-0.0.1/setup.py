ffa9a1c5e754e9ebcc18b0292c0f2c33f7b93acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:17 +0000

    Add package pipetransfer/0.0.1/pipetransfer-0.0.1.tar.gz

---
+++ packages/pipetransfer/pipetransfer-0.0.1.tar.gz/pipetransfer-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages 
+import codecs
+import os 
+
+def read(*parts):
+    here = os.path.abspath(os.path.dirname(__file__))
+    return codecs.open(os.path.join(here, *parts), "r",encoding='utf-8').read()
+
+
+def get_version():
+    '''
+    version_file = 'pipetools/version.py'
+    with open(version_file, 'r', encoding='utf-8') as f:
+        exec(compile(f.read(), version_file, 'exec'))
+    return locals()['__version__']
+    '''
+    return '0.0.1'
+
+setup(
+    name='pipetransfer',
+    version=get_version(),
+    description="初始化",
+    author='zhys513',#作者
+    author_email="254851907@qq.com",
+    url="https://gitee.com/zhys513/pipetools",
+    python_requires='>=3.6', 
+)
+
