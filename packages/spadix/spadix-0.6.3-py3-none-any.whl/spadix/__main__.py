a676067d32ffefcd0150774434f4ba0cc9cc72c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:37 +0000

    Add package spadix/0.6.3/spadix-0.6.3-py3-none-any.whl

---
+++ packages/spadix/spadix-0.6.3-py3-none-any.whl/spadix/__main__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Copyright 2021 SafeAI, Inc.
+
+import sys
+
+import spadix
+
+
+def main():
+    return spadix.main()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
