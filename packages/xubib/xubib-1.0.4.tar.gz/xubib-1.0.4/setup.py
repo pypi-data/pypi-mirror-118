fb55a194b3e222a1d002985c6e06e28f262108b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:09 +0000

    Add package xubib/1.0.4/xubib-1.0.4.tar.gz

---
+++ packages/xubib/xubib-1.0.4.tar.gz/xubib-1.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+'''
+    必要元素，name，version，description，packages等。
+    常用元素，author，pymoudles,long_description,author_email,install_requires
+'''
+def markd(filename):
+    with open(filename) as rf:
+        return rf.read()
+
+
+
+#print (readme_file())
+setup(
+    name = "xubib",
+    version = "1.0.4",
+    packages = ["fb"],
+    description = "this is my first test",
+    author = "xushibo",
+    author_email = "937207118@qq.com",
+    #install_requires = ["bibtexparser"],#待测试
+    install_requires = ["bibtexparser","mysqlclient" ],#待测试
+    long_description = markd("README.md")
+    
+)
