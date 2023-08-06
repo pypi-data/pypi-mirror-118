b1852fcf32a50b6023c5fcc136414aa08997dd1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:53 +0000

    Add package wakepy/0.4.4/wakepy-0.4.4.tar.gz

---
+++ packages/wakepy/wakepy-0.4.4.tar.gz/wakepy-0.4.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="wakepy",
+    version="0.4.4",
+    author="Niko Pasanen",
+    author_email="niko@pasanen.me",
+    url="https://github.com/np-8/wakepy",
+    packages=find_packages(),
+    include_package_data=True,
+    license="MIT",
+    description="Simple wakelock / keep-awake / stay-awake",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Utilities",
+    ],
+)
