11576276b8134c89beb0d5d9ec838eae1323178c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:45 +0000

    Add package vcspack7/0.1/vcspack7-0.1.tar.gz

---
+++ packages/vcspack7/vcspack7-0.1.tar.gz/vcspack7-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+  name = 'vcspack7',         # How you named your package folder (MyLib)
+  packages = ['vcspack7'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'vcspack utilities',   # Give a short description about your library
+  author = 'vinay',
+  author_email = 'vinaygn@gmail.com',
+  keywords = ['PYTHON', 'UTILITIES'],   # Keywords that define your package best
+)
