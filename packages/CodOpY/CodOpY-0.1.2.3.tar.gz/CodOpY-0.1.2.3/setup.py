041f8b0ae8ca9021262741cd17c904acf0b2c6a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:54 +0000

    Add package CodOpY/0.1.2.3/CodOpY-0.1.2.3.tar.gz

---
+++ packages/CodOpY/CodOpY-0.1.2.3.tar.gz/CodOpY-0.1.2.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+print(find_packages())
+setup(
+    name = 'CodOpY',
+    version = '0.1.2.3',
+    author = 'Tobias von der Haar',
+    author_email = 'T.von-der-Haar@kent.ac.uk',
+    url="https://github.com/tobiasvonderhaar/CodOpY",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+    ],
+    description= 'Python Codon Optimisation tools',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules = ['CodOpY.optimise','CodOpY.misc','CodOpY.plot','CodOpY.Data'],
+    zip_safe=False,
+    package_dir = {'' : 'src'},
+    packages=find_packages(include=['CodOpY','CodOpY.*']),
+    package_data={'CodOpY':['Data/*.csv']},
+    include_package_data=True,
+    python_requires = ">=3.6",
+    install_requires=['pandas','numpy','matplotlib']
+)
