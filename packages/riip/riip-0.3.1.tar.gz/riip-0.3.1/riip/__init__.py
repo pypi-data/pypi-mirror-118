d94f4973ca7c318b7b013abeff6779e62eec080f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:47 +0000

    Add package riip/0.3.1/riip-0.3.1.tar.gz

---
+++ packages/riip/riip-0.3.1.tar.gz/riip-0.3.1/riip/__init__.py
@@ -0,0 +1,13 @@
+from riip import material
+from riip.dataframe_operations import RiiDataFrame
+from riip.material import Material
+
+__all__ = [
+    "Material",
+    "RiiDataFrame",
+    "material",
+]
+
+__author__ = "Munehiro Nishida"
+__version__ = "0.3.1"
+__license__ = "MIT"
