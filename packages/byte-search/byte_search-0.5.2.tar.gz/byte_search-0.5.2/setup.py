38c94244d403754a9f69e0b1f97b4754d4a1fd93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:37 +0000

    Add package byte-search/0.5.2/byte_search-0.5.2.tar.gz

---
+++ packages/byte-search/byte_search-0.5.2.tar.gz/byte_search-0.5.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='byte_search',
+    version='0.5.2',
+    author='Urchade Zaratiana',
+    author_email='urchade.zaratiana@gmail.com',
+    packages=['byte_search'],
+    url='https://github.com/urchade/byte_search',
+    license='LICENSE.txt',
+    description='Fast lexical search',
+    long_description=open('README.md').read(),
+    package_dir={'': 'src/'}
+)
