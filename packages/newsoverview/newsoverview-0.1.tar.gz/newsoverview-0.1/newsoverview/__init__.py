1fab0e6b03a491247436da0ff8927f125a970b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:01 +0000

    Add package newsoverview/0.1/newsoverview-0.1.tar.gz

---
+++ packages/newsoverview/newsoverview-0.1.tar.gz/newsoverview-0.1/newsoverview/__init__.py
@@ -0,0 +1,7 @@
+from .covid_news import CoronavirusNews
+from .en_world_news import EnglishWorldNews
+from .protests import Protests
+from .travel import TravelWarnings
+from .terror_monitor import TerrorNews
+from .tr_world_news import TranslatedWorldNews
+from .simple_search import SimpleSearch
