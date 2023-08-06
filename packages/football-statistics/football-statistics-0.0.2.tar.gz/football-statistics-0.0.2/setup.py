c6681a5cb39825d27110a0ac92a1dfadc50f1300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:26 +0000

    Add package football-statistics/0.0.2/football-statistics-0.0.2.tar.gz

---
+++ packages/football-statistics/football-statistics-0.0.2.tar.gz/football-statistics-0.0.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="football-statistics",
+    version="0.0.2",
+    author="Max Leonard",
+    author_email="maxhleonard@gmail.com",
+    description="Package for centralized football statistics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maxhleonard/football",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src","FootballStats":"src/FootballStats"},
+    packages=["FootballStats.odds","FootballStats.scraping","FootballStats.objects"],
+    package_data={"FootballStats":["data/*"]},
+    python_requires=">=3.6",
+    install_requires=[
+        "pandas",
+        "beautifulsoup4"
+    ]
+)
