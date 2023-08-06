3bcba8ea00799031a3ea4c60a0c533dd999bea34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:58 +0000

    Add package minos-microservice-networks/0.0.14/minos_microservice_networks-0.0.14.tar.gz

---
+++ packages/minos-microservice-networks/minos_microservice_networks-0.0.14.tar.gz/minos_microservice_networks-0.0.14/minos/networks/brokers/abc.py
@@ -0,0 +1,70 @@
+"""
+Copyright (C) 2021 Clariteia SL
+
+This file is part of minos framework.
+
+Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
+"""
+from abc import (
+    ABC,
+)
+from typing import (
+    NoReturn,
+)
+
+from psycopg2.sql import (
+    SQL,
+)
+
+from minos.common import (
+    MinosBroker,
+    PostgreSqlMinosDatabase,
+)
+
+
+class BrokerSetup(PostgreSqlMinosDatabase):
+    """Minos Broker Setup Class"""
+
+    async def _setup(self) -> NoReturn:
+        await self._create_broker_table()
+
+    async def _create_broker_table(self) -> NoReturn:
+        await self.submit_query(_CREATE_TABLE_QUERY, lock=hash("producer_queue"))
+
+
+class Broker(MinosBroker, BrokerSetup, ABC):
+    """Minos Broker Class."""
+
+    ACTION: str
+
+    async def enqueue(self, topic: str, raw: bytes) -> int:
+        """Send a sequence of bytes to the given topic.
+
+        :param topic: Topic in which the bytes will be send.
+        :param raw: Bytes sequence to be send.
+        :return: The identifier of the message in the queue.
+        """
+        params = (topic, raw, 0, self.ACTION)
+        raw = await self.submit_query_and_fetchone(_INSERT_ENTRY_QUERY, params)
+        await self.submit_query(_NOTIFY_QUERY)
+        return raw[0]
+
+
+_CREATE_TABLE_QUERY = SQL(
+    "CREATE TABLE IF NOT EXISTS producer_queue ("
+    "id BIGSERIAL NOT NULL PRIMARY KEY, "
+    "topic VARCHAR(255) NOT NULL, "
+    "model BYTEA NOT NULL, "
+    "retry INTEGER NOT NULL, "
+    "action VARCHAR(255) NOT NULL, "
+    "creation_date TIMESTAMP NOT NULL, "
+    "update_date TIMESTAMP NOT NULL)"
+)
+
+_INSERT_ENTRY_QUERY = SQL(
+    "INSERT INTO producer_queue (topic, model, retry, action, creation_date, update_date) "
+    "VALUES (%s, %s, %s, %s, NOW(), NOW()) "
+    "RETURNING id"
+)
+
+_NOTIFY_QUERY = SQL("NOTIFY producer_queue")
---
+++ packages/minos-microservice-networks/minos_microservice_networks-0.0.14.tar.gz/minos_microservice_networks-0.0.14/minos/networks/snapshots.py
@@ -0,0 +1,58 @@
+"""
+Copyright (C) 2021 Clariteia SL
+
+This file is part of minos framework.
+
+Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
+"""
+
+from aiomisc.service.periodic import (
+    PeriodicService,
+)
+from cached_property import (
+    cached_property,
+)
+
+from minos.common import (
+    PostgreSqlSnapshotBuilder,
+)
+
+
+class SnapshotService(PeriodicService):
+    """Minos Snapshot Service class."""
+
+    def __init__(self, interval: int = 60, **kwargs):
+        super().__init__(interval=interval, **kwargs)
+        self._init_kwargs = kwargs
+
+    async def start(self) -> None:
+        """Start the service execution.
+
+        :return: This method does not return anything.
+        """
+        await self.dispatcher.setup()
+        await super().start()
+
+    async def callback(self) -> None:
+        """Callback implementation to be executed periodically.
+
+        :return: This method does not return anything.
+        """
+        await self.dispatcher.dispatch()
+
+    async def stop(self, err: Exception = None) -> None:
+        """Stop the service execution.
+
+        :param err: Optional exception that stopped the execution.
+        :return: This method does not return anything.
+        """
+        await super().stop(err)
+        await self.dispatcher.destroy()
+
+    @cached_property
+    def dispatcher(self) -> PostgreSqlSnapshotBuilder:
+        """Get the service dispatcher.
+
+        :return: A ``PostgreSqlSnapshotBuilder`` instance.
+        """
+        return PostgreSqlSnapshotBuilder.from_config(**self._init_kwargs)
