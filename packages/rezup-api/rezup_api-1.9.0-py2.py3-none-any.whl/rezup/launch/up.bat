fb649809248cec23966dbbea34009acd0dbb6abd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:42 +0000

    Add package rezup-api/1.9.0/rezup_api-1.9.0-py2.py3-none-any.whl

---
+++ packages/rezup-api/rezup_api-1.9.0-py2.py3-none-any.whl/rezup/launch/up.bat
@@ -0,0 +1,16 @@
+@echo off
+
+if not defined PROMPT (
+    set "PROMPT=$P$G"
+)
+if not defined REZUP_DISABLE_PROMPT (
+    set "PROMPT=__REZUP_PROMPT__%PROMPT%"
+)
+
+if defined __REZUP_SCRIPT__ (
+    call %__REZUP_SCRIPT__%
+)
+if defined __REZUP_COMMAND__ (
+    %__REZUP_COMMAND__%
+    goto :eof
+)
