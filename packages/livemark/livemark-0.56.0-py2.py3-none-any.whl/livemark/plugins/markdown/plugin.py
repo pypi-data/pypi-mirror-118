146a351b0888505cf03f2e50f69e9da5bfcda110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:07 +0000

    Add package livemark/0.56.0/livemark-0.56.0-py2.py3-none-any.whl

---
+++ packages/livemark/livemark-0.56.0-py2.py3-none-any.whl/livemark/plugins/markdown/plugin.py
@@ -0,0 +1,19 @@
+import marko
+from .renderer import MarkdownRenderer
+from ...plugin import Plugin
+
+
+class MarkdownPlugin(Plugin):
+    identity = "markdown"
+
+    # Process
+
+    def process_document(self, document):
+        if document.format == "md":
+            markdown = marko.Markdown(renderer=MarkdownRenderer)
+            output = markdown.parse(document.content)
+            markdown.renderer.document = document
+            output = markdown.render(output)
+            if document.preface:
+                output = document.preface.join(["---"] * 2) + "\n\n" + output
+            document.output = output
