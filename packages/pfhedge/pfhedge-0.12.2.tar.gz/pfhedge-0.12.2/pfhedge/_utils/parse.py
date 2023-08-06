60052c4d2175cffad8d43ccfbd68774278678282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:47 +0000

    Add package pfhedge/0.12.2/pfhedge-0.12.2.tar.gz

---
+++ packages/pfhedge/pfhedge-0.12.2.tar.gz/pfhedge-0.12.2/pfhedge/_utils/parse.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+import torch
+from torch import Tensor
+
+
+def parse_spot(
+    *,
+    spot: Optional[Tensor] = None,
+    strike: Optional[Tensor] = None,
+    moneyness: Optional[Tensor] = None,
+    log_moneyness: Optional[Tensor] = None,
+    **kwargs
+) -> Tensor:
+    if spot is not None:
+        return spot
+    elif moneyness is not None and strike is not None:
+        return moneyness * strike
+    elif log_moneyness is not None and strike is not None:
+        return log_moneyness.exp() * strike
+    else:
+        raise ValueError("Insufficient parameters to parse `spot`")
