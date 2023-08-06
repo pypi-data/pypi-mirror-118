f00da97ac2c619ae47ce4e26a5a1dda5ca6c4abd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:51 +0000

    Add package ci-plumber/1.2.0/ci-plumber-1.2.0.tar.gz

---
+++ packages/ci-plumber/ci-plumber-1.2.0.tar.gz/ci-plumber-1.2.0/ci_plumber/helpers/run_command.py
@@ -0,0 +1,24 @@
+import subprocess
+from time import sleep
+
+from rich.console import Console
+from rich.traceback import install
+
+install(show_locals=True)
+
+
+def run_command(command: str) -> str:
+    """
+    Runs a command and returns the output.
+    """
+    sleep(2)  # Most commands seem to benefit from this delay
+    console = Console()
+    output = subprocess.run(command.split(), capture_output=True, text=True)
+
+    try:
+        output.check_returncode()
+    except subprocess.CalledProcessError:
+        console.log("[bold red]Command failed:[/bold red]")
+        console.log(output.stderr)
+        # console.print_exception(show_locals=True)
+    return output.stdout + output.stderr
