22810736c090c6b9bd1638b40741707c9f8f8453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:13 +0000

    Add package mlapiwrapper/1.1.5/mlapiwrapper-1.1.5.tar.gz

---
+++ packages/mlapiwrapper/mlapiwrapper-1.1.5.tar.gz/mlapiwrapper-1.1.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mlapiwrapper",
+    version="1.1.5",
+    author="Minh Le",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description="long_description",
+    long_description_content_type="text/markdown",
+    url="",
+    project_urls={},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["mlapiwrapper"],
+    package_dir={"mlapiwrapper":"mlapiwrapper"}
+
+)
