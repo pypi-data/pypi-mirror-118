99c8aa293cc5a3ef4205ce83da9a603a7a1a7d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:18 +0000

    Add package typewiseprediction/1.0.0/typewiseprediction-1.0.0.tar.gz

---
+++ packages/typewiseprediction/typewiseprediction-1.0.0.tar.gz/typewiseprediction-1.0.0/typewiseprediction/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from typewiseprediction.api.completion_api import CompletionApi
+from typewiseprediction.api.correction_api import CorrectionApi
+from typewiseprediction.api.default_api import DefaultApi
+from typewiseprediction.api.emoji_api import EmojiApi
+from typewiseprediction.api.language_detection_api import LanguageDetectionApi
