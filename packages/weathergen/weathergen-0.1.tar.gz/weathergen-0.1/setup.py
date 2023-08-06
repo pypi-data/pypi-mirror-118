ebfdbc78a183dcb48e02b489965c29179df03bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:45 +0000

    Add package weathergen/0.1/weathergen-0.1.tar.gz

---
+++ packages/weathergen/weathergen-0.1.tar.gz/weathergen-0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="weathergen",                     # This is the name of the package
+    version="0.1",                 # The initial release version
+    author="Thomas Morris",          # Full name of the author
+    description='Weather generation.',
+    url='https://github.com/tomachito/weathergen',
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
+    #packages=setuptools.find_packages(),    # List of all python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.7',                   # Minimum version requirement of the package
+    install_requires=['numpy'],                     # Install other dependencies if any
+    
+    #py_modules = ['weathergen',], 
+    include_package_data=True,
+    package_data={'weathergen': ['weathergen/weather_generator']},
+)
