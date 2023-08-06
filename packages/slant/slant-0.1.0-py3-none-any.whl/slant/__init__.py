009ef693b8530f69782d26e34878ee51abfa2782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:38 +0000

    Add package slant/0.1.0/slant-0.1.0-py3-none-any.whl

---
+++ packages/slant/slant-0.1.0-py3-none-any.whl/slant/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""Import functions."""
+from .bias import bias_words, get_bias_words, has_bias_words
+from .content import stop_words, top_words
+from .sentiment import Sentiment
