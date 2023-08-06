9dac9b374d5d708798051deda56a017ebe2d580c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:01 +0000

    Add package seriesdistancematrix/0.3.1/seriesdistancematrix-0.3.1.tar.gz

---
+++ packages/seriesdistancematrix/seriesdistancematrix-0.3.1.tar.gz/seriesdistancematrix-0.3.1/distancematrix/__init__.py
@@ -0,0 +1,6 @@
+import distancematrix.consumer
+import distancematrix.generator
+from distancematrix.calculator import AnytimeCalculator
+from distancematrix.calculator import StreamingCalculator
+
+__version__ = "0.3.1" # Also update ../setup.py!
