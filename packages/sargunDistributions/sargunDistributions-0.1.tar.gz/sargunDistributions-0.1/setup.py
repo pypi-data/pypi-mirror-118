adcf279d775b6afb1d2b49524b067dfaaad49355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:07 +0000

    Add package sargunDistributions/0.1/sargunDistributions-0.1.tar.gz

---
+++ packages/sargunDistributions/sargunDistributions-0.1.tar.gz/sargunDistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'sargunDistributions',
+      version = '0.1',
+      description = 'Test package',
+      packages = ['sargunDistributions'],
+      author ='Sargun Nagpal',
+      author_email = 'sargunnagpal@gmail.com',
+      zip_safe=False)
