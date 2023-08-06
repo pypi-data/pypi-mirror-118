6170244cefe57d41561fdcba2502241a9d5ee9d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:32 +0000

    Add package itembed/0.5.0/itembed-0.5.0.tar.gz

---
+++ packages/itembed/itembed-0.5.0.tar.gz/itembed-0.5.0/itembed/task.py
@@ -0,0 +1,257 @@
+import numpy as np
+
+from .optimization import do_unsupervised_batch, do_supervised_batch
+from .util import index_batch_stream
+
+
+class Task:
+    """Abstract training task."""
+
+    def __init__(self, learning_rate_scale):
+        self.learning_rate_scale = learning_rate_scale
+
+    def do_batch(self, learning_rate):
+        """Apply training step."""
+
+        raise NotImplementedError()
+
+
+class UnsupervisedTask(Task):
+    """Unsupervised training task.
+
+    See Also
+    --------
+    :meth:`do_unsupervised_steps`
+
+    Parameters
+    ----------
+    items: int32, num_item
+        Itemsets, concatenated.
+    offsets: int32, num_itemset + 1
+        Boundaries in packed items.
+    indices: int32, num_step
+        Subset of offsets to consider.
+    syn0: float32, num_label x num_dimension
+        First set of embeddings.
+    syn1: float32, num_label x num_dimension
+        Second set of embeddings.
+    weights: float32, num_item, optional
+        Item weights, concatenated.
+    num_negative: int32, optional
+        Number of negative samples.
+    learning_rate_scale: float32, optional
+        Learning rate multiplier.
+    batch_size: int32, optional
+        Batch size.
+
+    """
+
+    def __init__(
+        self,
+        items,
+        offsets,
+        syn0,
+        syn1,
+        *,
+        weights=None,
+        num_negative=5,
+        learning_rate_scale=1.0,
+        batch_size=64,
+    ):
+        super().__init__(learning_rate_scale)
+
+        # Make sure that there are no index overflow
+        assert syn0.shape == syn1.shape, "synsets shape mismatch"
+        assert items.min() >= 0, "negative item index"
+        assert items.max() < syn0.shape[0], "out-of-bound item index"
+        assert offsets.shape[0] > 1, "no itemset"
+        assert offsets.min() >= 0, "negative offset"
+        assert offsets.max() <= items.shape[0], "out-of-bound offset"
+        assert (offsets[1:] - offsets[:-1] >= 2).all(), "itemset size must be >= 2"
+
+        # Allocate unit weights, if needed
+        if weights is None:
+            weights = np.ones(items.shape[0], dtype=np.float32)
+        else:
+            assert weights.shape == items.shape, "weights shape mismatch"
+
+        # Store parameters
+        self.items = items
+        self.weights = weights
+        self.offsets = offsets
+        self.syn0 = syn0
+        self.syn1 = syn1
+        self.num_negative = num_negative
+        self.batch_size = batch_size
+
+        # Allocate internal buffer
+        size = syn0.shape[1]
+        self._tmp_syn = np.empty(size, dtype=np.float32)
+
+        # Instanciate index generator
+        num_itemset = offsets.shape[0] - 1
+        self.batch_iterator = index_batch_stream(num_itemset, batch_size)
+
+    def __len__(self):
+        (num_itemset,) = self.offsets.shape
+        return (num_itemset - 1) // self.batch_size + 1
+
+    def do_batch(self, learning_rate):
+        indices = next(self.batch_iterator)
+        do_unsupervised_batch(
+            self.items,
+            self.weights,
+            self.offsets,
+            indices,
+            self.syn0,
+            self.syn1,
+            self._tmp_syn,
+            self.num_negative,
+            learning_rate * self.learning_rate_scale,
+        )
+
+
+class SupervisedTask(Task):
+    """Supervised training task.
+
+    See Also
+    --------
+    :meth:`do_supervised_steps`
+
+    Parameters
+    ----------
+    left_items: int32, num_left_item
+        Itemsets, concatenated.
+    left_offsets: int32, num_itemset + 1
+        Boundaries in packed items.
+    right_items: int32, num_right_item
+        Itemsets, concatenated.
+    right_offsets: int32, num_itemset + 1
+        Boundaries in packed items.
+    left_syn: float32, num_left_label x num_dimension
+        Feature embeddings.
+    right_syn: float32, num_right_label x num_dimension
+        Label embeddings.
+    left_weights: float32, num_left_item, optional
+        Item weights, concatenated.
+    right_weights: float32, num_right_item, optional
+        Item weights, concatenated.
+    num_negative: int32, optional
+        Number of negative samples.
+    learning_rate_scale: float32, optional
+        Learning rate multiplier.
+    batch_size: int32, optional
+        Batch size.
+
+    """
+
+    def __init__(
+        self,
+        left_items,
+        left_offsets,
+        right_items,
+        right_offsets,
+        left_syn,
+        right_syn,
+        *,
+        left_weights=None,
+        right_weights=None,
+        num_negative=5,
+        learning_rate_scale=1.0,
+        batch_size=64,
+    ):
+        super().__init__(learning_rate_scale)
+
+        # Make sure that there are no index overflow
+        assert left_syn.shape[1] == right_syn.shape[1], "embedding size mismatch"
+        assert left_items.min() >= 0, "negative item index"
+        assert right_items.min() >= 0, "negative item index"
+        assert left_items.max() < left_syn.shape[0], "out-of-bound item index"
+        assert right_items.max() < right_syn.shape[0], "out-of-bound item index"
+        assert left_offsets.shape == right_offsets.shape, "offsets shape mismatch"
+        assert left_offsets.shape[0] > 1, "no itemset"
+        assert right_offsets.shape[0] > 1, "no itemset"
+        assert left_offsets.min() >= 0, "negative offset"
+        assert right_offsets.min() >= 0, "negative offset"
+        assert left_offsets.max() <= left_items.shape[0], "out-of-bound offset"
+        assert right_offsets.max() <= right_items.shape[0], "out-of-bound offset"
+        assert (left_offsets[1:] - left_offsets[:-1] >= 1).all(), "itemset size must be >= 1"
+        assert (right_offsets[1:] - right_offsets[:-1] >= 1).all(), "itemset size must be >= 1"
+
+        # Allocate unit weights, if needed
+        if left_weights is None:
+            left_weights = np.ones(left_items.shape[0], dtype=np.float32)
+        else:
+            assert left_weights.shape == left_items.shape, "weights shape mismatch"
+        if right_weights is None:
+            right_weights = np.ones(right_items.shape[0], dtype=np.float32)
+        else:
+            assert right_weights.shape == right_items.shape, "weights shape mismatch"
+
+        # Store parameters
+        self.left_items = left_items
+        self.left_offsets = left_offsets
+        self.left_weights = left_weights
+        self.right_items = right_items
+        self.right_offsets = right_offsets
+        self.right_weights = right_weights
+        self.left_syn = left_syn
+        self.right_syn = right_syn
+        self.num_negative = num_negative
+        self.batch_size = batch_size
+
+        # Allocate internal buffer
+        size = left_syn.shape[1]
+        self._tmp_syn = np.empty(size, dtype=np.float32)
+
+        # Instanciate index generator
+        num_itemset = left_offsets.shape[0] - 1
+        self.batch_iterator = index_batch_stream(num_itemset, batch_size)
+
+    def __len__(self):
+        (num_itemset,) = self.left_offsets.shape
+        return (num_itemset - 1) // self.batch_size + 1
+
+    def do_batch(self, learning_rate):
+        indices = next(self.batch_iterator)
+        do_supervised_batch(
+            self.left_items,
+            self.left_weights,
+            self.left_offsets,
+            indices,
+            self.right_items,
+            self.right_weights,
+            self.right_offsets,
+            indices,
+            self.left_syn,
+            self.right_syn,
+            self._tmp_syn,
+            self.num_negative,
+            learning_rate * self.learning_rate_scale,
+        )
+
+
+class CompoundTask(Task):
+    """Group multiple sub-tasks together.
+
+    Parameters
+    ----------
+    *tasks: list of Task
+        Collection of tasks to train jointly.
+    learning_rate_scale: float32, optional
+        Learning rate multiplier.
+
+    """
+
+    def __init__(self, *tasks, learning_rate_scale=1.0):
+        super().__init__(learning_rate_scale)
+        assert len(tasks) > 0
+        self.tasks = tasks
+
+    def __len__(self):
+        return max(len(task) for task in self.tasks)
+
+    def do_batch(self, learning_rate):
+        learning_rate = learning_rate * self.learning_rate_scale
+        for task in self.tasks:
+            task.do_batch(learning_rate)
---
+++ packages/itembed/itembed-0.5.0.tar.gz/itembed-0.5.0/itembed/util.py
@@ -0,0 +1,263 @@
+from collections import Counter
+
+import numpy as np
+
+from tqdm import tqdm
+
+
+def index_batch_stream(num_index, batch_size):
+    """Indices generator."""
+
+    assert num_index > 0
+    assert batch_size > 0
+
+    indices = np.arange(num_index, dtype=np.int32)
+    # TODO improve this algorithm
+
+    # Replicate indices, to cover at least one batch
+    if num_index < batch_size:
+        repeat = batch_size // num_index + 1
+        indices = np.tile(indices, repeat)
+        num_index *= repeat
+
+    # Loop forever
+    while True:
+        np.random.shuffle(indices)
+        i = 0
+        while i + batch_size <= num_index:
+            yield indices[i : i + batch_size]
+            i += batch_size
+
+
+def pack_itemsets(itemsets, *, min_count=1, min_length=1):
+    """Convert itemset collection to packed indices.
+
+    Parameters
+    ----------
+    itemsets: list of list of object
+        List of sets of hashable objects.
+    min_count: int, optional
+        Minimal frequency count to be kept.
+    min_length: int, optional
+        Minimal itemset length.
+
+    Returns
+    -------
+    labels: list of object
+        Mapping from indices to labels.
+    indices: int32, num_item
+        Packed index array.
+    offsets: int32, num_itemset + 1
+        Itemsets offsets in packed array.
+
+    Example
+    -------
+    >>> itemsets = [
+    ...     ["apple"],
+    ...     ["apple", "sugar", "flour"],
+    ...     ["pear", "sugar", "flour", "butter"],
+    ...     ["apple", "pear", "sugar", "butter", "cinnamon"],
+    ...     ["salt", "flour", "oil"],
+    ... ]
+    >>> pack_itemsets(itemsets, min_length=2)
+    (['apple', 'sugar', 'flour', 'pear', 'butter', 'cinnamon', 'salt', 'oil'],
+     array([0, 1, 2, 3, 1, 2, 4, 0, 3, 1, 4, 5, 6, 2, 7]),
+     array([ 0,  3,  7, 12, 15]))
+
+    """
+
+    # Count labels
+    counter = Counter()
+    for itemset in itemsets:
+        counter.update(itemset)
+    if None in counter:
+        del counter[None]
+
+    # Define label list
+    labels = [l for l, c in counter.most_common() if c >= min_count]
+    label_map = {l: i for i, l in enumerate(labels)}
+
+    # Generate indices
+    indices = []
+    offsets = [0]
+    for itemset in itemsets:
+        itemset_indices = []
+        for label in itemset:
+            index = label_map.get(label)
+            if index is not None:
+                itemset_indices.append(index)
+        if len(itemset_indices) >= min_length:
+            indices.extend(itemset_indices)
+            offsets.append(len(indices))
+
+    # Convert to arrays
+    indices = np.array(indices, dtype=np.int32)
+    offsets = np.array(offsets, dtype=np.int32)
+    return labels, indices, offsets
+
+
+def prune_itemsets(indices, offsets, *, mask=None, min_length=None):
+    """Filter packed indices.
+
+    Either an explicit mask or a length threshold must be defined.
+
+    Parameters
+    ----------
+    indices: int32, num_item
+        Packed index array.
+    offsets: int32, num_itemset + 1
+        Itemsets offsets in packed array.
+    mask: bool, num_itemset
+        Boolean mask.
+    min_length: int
+        Minimum length, inclusive.
+
+    Returns
+    -------
+    indices: int32, num_item
+        Packed index array.
+    offsets: int32, num_itemset + 1
+        Itemsets offsets in packed array.
+
+    Example
+    -------
+    >>> indices = np.array([0, 0, 1, 0, 1, 2, 0, 1, 2, 3])
+    >>> offsets = np.array([0, 1, 3, 6, 10])
+    >>> mask = np.array([True, True, False, True])
+    >>> prune_itemsets(indices, offsets, mask=mask, min_length=2)
+    (array([0, 1, 0, 1, 2, 3]), array([0, 2, 6]))
+
+    """
+
+    # Build mask from length limit, if needed
+    lengths = offsets[1:] - offsets[:-1]
+    if min_length is not None:
+        length_mask = lengths >= min_length
+        if mask is None:
+            mask = length_mask
+        else:
+            mask = np.logical_and(mask, length_mask)
+    assert lengths.shape == mask.shape
+
+    # Allocate buffers
+    out_indices = np.zeros(lengths[mask].sum(), dtype=np.int32)
+    out_offsets = np.zeros(mask.sum() + 1, dtype=np.int32)
+
+    # Build new itemsets
+    offset = 0
+    j = 1
+    for i in range(len(mask)):
+        keep = mask[i]
+        if keep:
+            length = lengths[i]
+            out_indices[offset : offset + length] = indices[offsets[i] : offsets[i + 1]]
+            offset += length
+            out_offsets[j] = offset
+            j += 1
+    return out_indices, out_offsets
+
+
+def initialize_syn(num_label, num_dimension, method="uniform"):
+    """Allocate and initialize embedding set.
+
+    Parameters
+    ----------
+    num_label: int32
+        Number of labels.
+    num_dimension: int32
+        Size of embeddings.
+    method: {"uniform", "zero"}, optional
+        Initialization method.
+
+    Returns
+    -------
+    syn: float32, num_label x num_dimension
+        Embedding set.
+
+    """
+
+    if method == "zero":
+        syn = np.zeros((num_label, num_dimension), dtype=np.float32)
+    elif method == "uniform":
+        syn = np.random.rand(num_label, num_dimension).astype(np.float32)
+        syn -= 0.5
+        syn /= num_dimension
+    else:
+        raise KeyError(method)
+    return syn
+
+
+def train(
+    task,
+    *,
+    num_epoch=10,
+    initial_learning_rate=0.025,
+    final_learning_rate=0.0,
+):
+    """Train loop.
+
+    Learning rate decreases linearly, down to zero.
+
+    Keyboard interruptions are silently captured, which interrupt the training
+    process.
+
+    A progress bar is shown, using ``tqdm``.
+
+    Parameters
+    ----------
+    task: Task
+        Top-level task to train.
+    num_epoch: int
+        Number of passes across the whole task.
+    initial_learning_rate: float
+        Maximum learning rate (inclusive).
+    final_learning_rate: float
+        Minimum learning rate (exclusive).
+
+    """
+
+    try:
+
+        # Iterate over epochs and batches
+        num_batch = len(task)
+        num_step = num_epoch * num_batch
+        delta_learning_rate = final_learning_rate - initial_learning_rate
+        step = 0
+        with tqdm(total=num_step) as progress:
+            for epoch in range(num_epoch):
+                for batch in range(num_batch):
+
+                    # Learning rate decreases linearly
+                    learning_rate = (
+                        delta_learning_rate * step / num_step + initial_learning_rate
+                    )
+
+                    # Apply step
+                    task.do_batch(learning_rate)
+
+                    # Update progress
+                    step += 1
+                    progress.update(1)
+
+    # Allow soft interruption
+    except KeyboardInterrupt:
+        pass
+
+
+def softmax(x):
+    """Compute softmax."""
+
+    e = np.exp(x)
+    return e / e.sum(axis=-1)[..., None]
+
+
+def norm(x):
+    """L\\ :sub:`2` norm."""
+
+    return np.sqrt((x ** 2).sum(axis=-1))
+
+
+def normalize(x):
+    """L\\ :sub:`2` normalization."""
+
+    return x / norm(x)[..., None]
