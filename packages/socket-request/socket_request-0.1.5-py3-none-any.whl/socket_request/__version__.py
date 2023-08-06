7c6cd0a962d8ba0b534a3d850b5fbc06f933d0bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:03 +0000

    Add package socket-request/0.1.5/socket_request-0.1.5-py3-none-any.whl

---
+++ packages/socket-request/socket_request-0.1.5-py3-none-any.whl/socket_request/__version__.py
@@ -0,0 +1,9 @@
+####
+__title__ = 'socket_request'
+__description__ = 'socket_request is parse and build HTTP requests using unix domain socket.'
+__url__ = 'https://github.com/jinwoo-j/socket_request'
+__version__ = '0.1.5'
+__author__ = 'Jinwoo Jeong'
+__author_email__ = 'jinwoo@iconloop.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020 JINWOO'
