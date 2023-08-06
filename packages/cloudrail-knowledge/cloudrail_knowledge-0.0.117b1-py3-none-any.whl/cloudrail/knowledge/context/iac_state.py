323ce0c761c748d9ddc323333d8c0a192df085f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:17 +0000

    Add package cloudrail-knowledge/0.0.117b1/cloudrail_knowledge-0.0.117b1-py3-none-any.whl

---
+++ packages/cloudrail-knowledge/cloudrail_knowledge-0.0.117b1-py3-none-any.whl/cloudrail/knowledge/context/iac_state.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from cloudrail.knowledge.context.iac_type import IacType
+from cloudrail.knowledge.context.terraform_action_type import TerraformActionType
+from cloudrail.knowledge.context.iac_resource_metadata import IacResourceMetadata
+
+
+@dataclass
+class IacState:
+    address: str
+    action: TerraformActionType
+    resource_metadata: Optional[IacResourceMetadata]
+    is_new: bool
+    iac_resource_url: Optional[str] = None
+    iac_type: IacType = None
