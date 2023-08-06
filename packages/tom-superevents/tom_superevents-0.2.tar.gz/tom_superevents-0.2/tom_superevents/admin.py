eae5f1e0a6b9a23924a0583bfef6884e3bdae833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:46 +0000

    Add package tom-superevents/0.2/tom_superevents-0.2.tar.gz

---
+++ packages/tom-superevents/tom_superevents-0.2.tar.gz/tom_superevents-0.2/tom_superevents/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import EventLocalization, Superevent
+
+# Register your models here.
+admin.site.register(EventLocalization)
+admin.site.register(Superevent)
