1171f4f59bc956dee96018d0c352de7cb77b477a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:47 +0000

    Add package py-Ultroid/60.9b0/py-Ultroid-60.9b0.tar.gz

---
+++ packages/py-Ultroid/py-Ultroid-60.9b0.tar.gz/py-Ultroid-60.9b0/pyUltroid/functions/pmpermit_db.py
@@ -0,0 +1,38 @@
+# Ultroid - UserBot
+# Copyright (C) 2021 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+from .. import udB
+
+try:
+    eval(udB.get("PMPERMIT"))
+except BaseException:
+    udB.set("PMPERMIT", "[]")
+
+
+def get_approved():
+    return eval(udB.get("PMPERMIT"))
+
+
+def approve_user(id):
+    ok = get_approved()
+    if id not in ok:
+        ok.append(id)
+        udB.set("PMPERMIT", str(ok))
+
+
+def disapprove_user(id):
+    ok = get_approved()
+    if id in ok:
+        ok.remove(id)
+        udB.set("PMPERMIT", str(ok))
+
+
+def is_approved(id):
+    ok = get_approved()
+    if id in ok:
+        return True
+    return False
---
+++ packages/py-Ultroid/py-Ultroid-60.9b0.tar.gz/py-Ultroid-60.9b0/pyUltroid/version.py
@@ -0,0 +1,2 @@
+__version__ = "60.9-beta"
+ultroid_version = "0.1"
