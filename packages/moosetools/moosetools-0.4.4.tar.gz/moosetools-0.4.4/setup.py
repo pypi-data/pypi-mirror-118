8b36885d688bafdcec1b4730718d5e0a76bb4851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:06 +0000

    Add package moosetools/0.4.4/moosetools-0.4.4.tar.gz

---
+++ packages/moosetools/moosetools-0.4.4.tar.gz/moosetools-0.4.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['moosetools', 'moosetools.test']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests-toolbelt>=0.9,<0.10', 'requests>=2,<3']
+
+setup_kwargs = {
+    'name': 'moosetools',
+    'version': '0.4.4',
+    'description': 'Moose Tools',
+    'long_description': '',
+    'author': 'Chuck Mo',
+    'author_email': 'chuck@moosejudge.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://moose-tools.moosejudge.com',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
