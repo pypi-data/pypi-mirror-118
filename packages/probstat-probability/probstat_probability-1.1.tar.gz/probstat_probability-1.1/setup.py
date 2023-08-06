560a02eb9b46df3357f74be7c41db5eb4f93c07f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:21 +0000

    Add package probstat-probability/1.1/probstat_probability-1.1.tar.gz

---
+++ packages/probstat-probability/probstat_probability-1.1.tar.gz/probstat_probability-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probstat_probability',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['probstat_probability'],
+      author= 'Tayyab Khan',
+      author_email= 'tayyabkhan298@gmail.com',
+      zip_safe=False)
