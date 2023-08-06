7822668895ce652cc2c1e8ea38d731e414a09fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:33 +0000

    Add package udacity-probability-distributions/0.1/udacity_probability_distributions-0.1.tar.gz

---
+++ packages/udacity-probability-distributions/udacity_probability_distributions-0.1.tar.gz/udacity_probability_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='udacity_probability_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['udacity_probability_distributions'],
+	  author='Ravi Bansal',
+      zip_safe=False)
