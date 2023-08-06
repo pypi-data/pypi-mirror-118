05997fdf5b551d61f05c95d7f6a3e56a52d7e5a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:27 +0000

    Add package moad/0.3.0/moad-0.3.0.tar.gz

---
+++ packages/moad/moad-0.3.0.tar.gz/moad-0.3.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='moad',
+    version='0.3.0',
+    packages=['moad'],
+    url='https://github.com/claasd/moad',
+    license='MIT',
+    author='Claas Diederichs',
+    author_email='',
+    description='Mic open API docs',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "PyYaml"
+    ]
+)
