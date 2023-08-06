6340f3fd767ed03e8e5d69cfe5a331fdfe84fa99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:23 +0000

    Add package nesterPenn/1.0.0/nesterPenn-1.0.0.tar.gz

---
+++ packages/nesterPenn/nesterPenn-1.0.0.tar.gz/nesterPenn-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='nesterPenn',
+    version='1.0.0',
+    py_modules=['nesterPenn'],
+    author='pennli',
+    author_email='lipeng1a@gmail.com',
+    url='https://pypi.org/',
+    description="Print item in list (of - possibly - nested lists)",
+    long_description="Print item in list (of - possibly - nested lists)",
+)
+
