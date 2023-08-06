78b3cbbafccc32c30fc776f3e85d0a95ba5e558c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:44 +0000

    Add package nbgitpuller/1.0.0/nbgitpuller-1.0.0.tar.gz

---
+++ packages/nbgitpuller/nbgitpuller-1.0.0.tar.gz/nbgitpuller-1.0.0/README.md
@@ -0,0 +1,28 @@
+# [nbgitpuller](https://github.com/jupyterhub/nbgitpuller)
+
+
+[![GitHub Workflow Status - Test](https://img.shields.io/github/workflow/status/jupyterhub/nbgitpuller/Tests?logo=github&label=tests)](https://github.com/jupyterhub/nbgitpuller/actions)
+[![CircleCI build status](https://img.shields.io/circleci/build/github/jupyterhub/nbgitpuller?logo=circleci&label=docs)](https://circleci.com/gh/jupyterhub/nbgitpuller)
+[![](https://img.shields.io/pypi/v/nbgitpuller.svg?logo=pypi)](https://pypi.python.org/pypi/nbgitpuller)
+[![GitHub](https://img.shields.io/badge/issue_tracking-github-blue?logo=github)](https://github.com/jupyterhub/nbgitpuller/issues)
+[![Discourse](https://img.shields.io/badge/help_forum-discourse-blue?logo=discourse)](https://discourse.jupyter.org/c/jupyterhub)
+[![Gitter](https://img.shields.io/badge/social_chat-gitter-blue?logo=gitter)](https://gitter.im/jupyterhub/jupyterhub)
+
+`nbgitpuller` lets you distribute content in a git repository to your students
+by having them click a simple link. [Automatic
+merging](https://jupyterhub.github.io/nbgitpuller/topic/automatic-merging.html)
+ensures that your students are never exposed to `git` directly. It is primarily
+used with a JupyterHub, but can also work on students' local computers.
+
+See [the documentation](https://jupyterhub.github.io/nbgitpuller) for more
+information.
+
+## Installation
+
+```shell
+pip install nbgitpuller
+```
+
+## Example
+
+![](https://raw.githubusercontent.com/jupyterhub/nbgitpuller/v0.8.0/docs/_static/nbpuller.gif)
---
+++ packages/nbgitpuller/nbgitpuller-1.0.0.tar.gz/nbgitpuller-1.0.0/setup.py
@@ -0,0 +1,48 @@
+from setuptools import find_packages, setup
+from distutils.util import convert_path
+import subprocess
+
+# Imports __version__, reference: https://stackoverflow.com/a/24517154/2220152
+ns = {}
+ver_path = convert_path('nbgitpuller/version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), ns)
+__version__ = ns['__version__']
+
+subprocess.check_call(['npm', 'install'])
+subprocess.check_call(['npm', 'run', 'webpack'])
+
+setup(
+    name='nbgitpuller',
+    version=__version__,
+    url='https://github.com/jupyterhub/nbgitpuller',
+    license='3-clause BSD',
+    author='Peter Veerman, YuviPanda',
+    author_email='peterkangveerman@gmail.com',
+    description='Notebook Extension to do one-way synchronization of git repositories',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=['notebook>=5.5.0', 'jupyter_server>=1.10.1', 'tornado'],
+    data_files=[
+        ('etc/jupyter/jupyter_server_config.d', ['nbgitpuller/etc/jupyter_server_config.d/nbgitpuller.json']),
+        ('etc/jupyter/jupyter_notebook_config.d', ['nbgitpuller/etc/jupyter_notebook_config.d/nbgitpuller.json'])
+    ],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'gitpuller = nbgitpuller.pull:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
