e0090f3d9353dc3a5f6fdd0558a7fc58beef9a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:20 +0000

    Add package apysc/0.41.11/apysc-0.41.11-py3-none-any.whl

---
+++ packages/apysc/apysc-0.41.11-py3-none-any.whl/apysc/_animation/animation_interface_base.py
@@ -0,0 +1,9 @@
+"""Base class for each animation interface.
+"""
+
+from apysc._animation.animation_pause_interface import AnimationPauseInterface
+
+
+class AnimationInterfaceBase(AnimationPauseInterface):
+    """Base class for each animation interface.
+    """
